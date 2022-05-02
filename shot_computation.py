e7cab944730205277b214295a91667043e132616
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-05-02 18:10:35 +0200

    Adjusted shot descriptor script

--- shot_computation.py
+++ shot_computation.py
@@ -1,27 +1,41 @@
 from tqdm import tqdm
 
 import os
-import open3d as o3d
+import trimesh
 import pyshot
 import numpy as np
+import shutil
 
 
-def compute_descriptors(directory, target_directory):
+def compute_descriptors(directory, target_directory, down_sample=None):
     file_list = os.listdir(directory)
     file_list.sort()
     file_list = [f for f in file_list if f[-4:] != ".png"]
+    descriptor_files = []
     for f in tqdm(file_list):
-        mesh = o3d.io.read_triangle_mesh(f"{directory}/{f}")
+        if down_sample is not None:
+            mesh = trimesh.load_mesh(f"{directory}/{f}")
+            points, face_indices = trimesh.sample.sample_surface_even(mesh, down_sample)
+            mesh = mesh.submesh([face_indices])[0]
+        else:
+            mesh = trimesh.load_mesh(f"{directory}/{f}")
         descr = pyshot.get_descriptors(
             np.array(mesh.vertices),
-            np.array(mesh.triangles, dtype=np.int64),
+            np.array(mesh.faces, dtype=np.int64),
             radius=100,
             local_rf_radius=.0,
             min_neighbors=0,
             n_bins=32
         )
-        np.save(f"{target_directory}/{f[:-4]}.npy", descr)
 
+        descriptor_filename = f"{target_directory}/{f[:-4]}.npy"
+        descriptor_files.append(descriptor_filename)
+        np.save(descriptor_filename, descr)
+        os.remove(f"{directory}/{f}")
+
+    shutil.make_archive(target_directory, "zip", target_directory)
+    for f in descriptor_files:
+        os.remove(f)
 
 if __name__ == "__main__":
 
@@ -31,11 +45,11 @@ if __name__ == "__main__":
     os.makedirs("SHOT-DESCRIPTORS/test/scans", exist_ok=True)
 
     # Compute descriptors
+    # print("Currently working on: 'MPI-FAUST/training/registrations'")
+    # compute_descriptors(
+    #     "MPI-FAUST/training/registrations", "SHOT-DESCRIPTORS/training/registrations"
+    # )
     print("Currently working on: 'MPI-FAUST/training/scans'")
-    compute_descriptors("MPI-FAUST/training/scans", "SHOT-DESCRIPTORS/training/scans")
-    print("Currently working on: 'MPI-FAUST/training/registrations'")
-    compute_descriptors(
-        "MPI-FAUST/training/registrations", "SHOT-DESCRIPTORS/training/registrations"
-    )
+    compute_descriptors("MPI-FAUST/training/scans", "SHOT-DESCRIPTORS/training/scans", 20_000)
     print("Currently working on: 'MPI-FAUST/test/scans'")
-    compute_descriptors("MPI-FAUST/test/scans", "SHOT-DESCRIPTORS/test/scans")
+    compute_descriptors("MPI-FAUST/test/scans", "SHOT-DESCRIPTORS/test/scans", 20_000)
