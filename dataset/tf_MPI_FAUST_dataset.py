96a09e95aaae9a01dbf4f5410c49a04bab9028d7
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-07-04 12:06:39 +0200

    Wrote tensorflow dataset for preprocessed MPI-FAUST dataset

---
+++ dataset/tf_MPI_FAUST_dataset.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+
+
+def faust_generator(path_to_zip):
+    """Reads one element of preprocessed FAUST-dataset into memory per 'next'-call."""
+
+    dataset = np.load(path_to_zip)
+    file_names = dataset.files
+    SHOT = [file_name for file_name in file_names if file_name.startswith("SHOT")]
+    GPC = [file_name for file_name in file_names if file_name.startswith("GPC")]
+    BC = [file_name for file_name in file_names if file_name.startswith("BC")]
+    SHOT.sort(), GPC.sort(), BC.sort()
+    for idx in range(100):
+        shot = dataset[SHOT[idx]]
+        gpc = dataset[GPC[idx]]
+        bc = dataset[BC[idx]]
+        yield shot, gpc, bc
+
+
+def load_preprocessed_faust(path_to_zip):
+    """Returns a 'tf.data.Dataset' of the preprocessed MPI-FAUST dataset.
+
+    Requires that preprocessing already happened. This function operates directly on the resulting 'zip'-file.
+
+    **Input**
+
+    - The path to the 'zip'-file obtained through preprocessing.
+
+    **Output**
+
+    - A tf.data.Dataset of the preprocessed MPI-FAUST dataset.
+
+    """
+
+    gen = faust_generator(path_to_zip)
+    return tf.data.Dataset.from_generator(
+        lambda: gen,  # "gen must be callable"
+        output_signature=(
+            tf.TensorSpec(shape=(6890, 1056), dtype=tf.float64),
+            tf.TensorSpec(shape=(6890, 6890, 2), dtype=tf.float64),
+            tf.TensorSpec(shape=(6890, 8, 8), dtype=tf.float32)
+        )
+    )
+
+
+if __name__ == "__main__":
+    tf_faust_dataset = load_preprocessed_faust("/home/andreas/Uni/Masterarbeit/MPI-FAUST/preprocessed_faust.zip")
