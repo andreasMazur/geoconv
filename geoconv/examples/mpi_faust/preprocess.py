8e236e142de0abc9490639c7813241a4390a37e7
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-08-13 07:39:29 +0200

    Increased target resolution

--- geoconv/examples/mpi_faust/preprocess.py
+++ geoconv/examples/mpi_faust/preprocess.py
@@ -1,7 +1,6 @@
 from geoconv.preprocessing.barycentric_coords import create_kernel_matrix, barycentric_coordinates
 from geoconv.preprocessing.discrete_gpc import discrete_gpc
 
-import open3d as o3d
 import os
 import tqdm
 import numpy as np
@@ -11,21 +10,18 @@ import trimesh
 import pyshot
 
 
-def preprocess(directory, target_dir, sub_sample_amount, reference_mesh):
+def preprocess(directory, target_dir, reference_mesh):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
     file_list = os.listdir(directory)
     file_list.sort()
-    file_list = [f for f in file_list if f[-4:] != ".png"]
+    file_list = [f for f in file_list if f[-4:] != ".png"][:1]
 
-    ############################
-    # Sub-sample reference mesh
-    ############################
-    reference_mesh = o3d.io.read_triangle_mesh(reference_mesh)
-    reference_mesh.compute_vertex_normals()
-    reference_mesh = reference_mesh.sample_points_poisson_disk(sub_sample_amount)
-    ref_mesh_kd_tree = scipy.spatial.KDTree(reference_mesh.points)
+    ######################
+    # Load reference mesh
+    ######################
+    reference_mesh = trimesh.load_mesh(reference_mesh)
 
     with tqdm.tqdm(total=len(file_list)) as pbar:
         for file_no, file in enumerate(file_list):
@@ -36,15 +32,28 @@ def preprocess(directory, target_dir, sub_sample_amount, reference_mesh):
             mesh = trimesh.load_mesh(f"{directory}/{file}")
 
             pbar.set_postfix({"Step": "Ground-truth computation"})
-            ################################################
-            # Compute ground-truth for shape correspondence
-            ################################################
+            ################
+            # Shuffle nodes
+            ################
+            # Otherwise ground-truth matrix is unit-matrix all the time
+            shuffled_node_indices = np.arange(mesh.vertices.shape[0])
+            np.random.shuffle(shuffled_node_indices)
+            object_mesh_vertices = np.copy(mesh.vertices)[shuffled_node_indices]
+            object_mesh_faces = np.copy(mesh.faces)
+            for face in object_mesh_faces:
+                face[0] = np.where(shuffled_node_indices == face[0])[0]
+                face[1] = np.where(shuffled_node_indices == face[1])[0]
+                face[2] = np.where(shuffled_node_indices == face[2])[0]
+            mesh = trimesh.Trimesh(vertices=object_mesh_vertices, faces=object_mesh_faces)
+
+            ##########################
+            # Set ground-truth labels
+            ##########################
             label_matrix = np.zeros(
-                shape=(np.array(mesh.vertices).shape[0], np.array(reference_mesh.points).shape[0]), dtype=np.int8
+                shape=(np.array(mesh.vertices).shape[0], np.array(reference_mesh.vertices).shape[0]), dtype=np.int8
             )
-            for vertex_idx, vertex in enumerate(mesh.vertices):
-                _, gt_idx = ref_mesh_kd_tree.query(vertex)
-                label_matrix[vertex_idx, gt_idx] = 1.
+            # For vertex mesh.vertices[i] ground truth is given by shuffled_node_indices[i]
+            label_matrix[(np.arange(label_matrix.shape[0]), shuffled_node_indices)] = 1
             label_matrix = scipy.sparse.csc_array(label_matrix)
             np.save(f"{target_dir}/GT_{file[:-4]}.npy", label_matrix)
 
