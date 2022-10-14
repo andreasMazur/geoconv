0e0d002c3d76bc837150e699bfc1d012deddfdb1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-10-14 19:30:46 +0200

    Adjusted gradient computation

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -7,12 +7,11 @@ BATCH_SIZE = 689
 
 class PointCorrespondenceGeoCNN(Model):
 
-    # @tf.function
+    @tf.function
     def train_step(self, data):
         (signal, barycentric), ground_truth = data
 
-        idx = tf.constant(0)
-        losses = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
+        trainable_vars = self.trainable_variables
         with tf.GradientTape(persistent=True) as tape:
             y_pred = self([signal, barycentric], training=True)
             all_losses = self.compiled_loss(ground_truth, y_pred) + tf.math.reduce_sum(self.losses)
@@ -20,14 +19,8 @@ class PointCorrespondenceGeoCNN(Model):
             for i in tf.range(start=0, limit=amt_losses, delta=BATCH_SIZE):
                 losses_batch = tf.reduce_sum(all_losses[i:i+BATCH_SIZE])
                 tape.watch(losses_batch)
-                losses = losses.write(idx, losses_batch)
-                idx = idx + tf.constant(1)
-
-        trainable_vars = self.trainable_variables
-        for idx in tf.range(losses.size()):
-            gradients = tape.gradient(losses.read(idx), trainable_vars)
-            self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        # tape.reset()
+                gradients = tape.gradient(losses_batch, trainable_vars)
+                self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
         self.compiled_metrics.update_state(ground_truth, y_pred)
         return {m.name: m.result() for m in self.metrics}
