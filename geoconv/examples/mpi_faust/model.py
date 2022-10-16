efb050968dae0599c81e48cdf4b2b2668cb47639
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-10-16 10:29:04 +0200

    Re-introduced jacobians

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -3,7 +3,6 @@ from tensorflow.keras import Model
 import tensorflow as tf
 
 BATCH_SIZE = 130
-SKIP_SIZE = 10
 
 
 class PointCorrespondenceGeoCNN(Model):
@@ -12,14 +11,17 @@ class PointCorrespondenceGeoCNN(Model):
     def train_step(self, data):
         (signal, barycentric), ground_truth = data
 
-        trainable_vars = self.trainable_variables
         with tf.GradientTape(persistent=True) as tape:
             y_pred = self([signal, barycentric], training=True)
             loss = self.compiled_loss(ground_truth, y_pred)
-            for batch_losses in tf.stack(tf.split(loss, BATCH_SIZE))[::SKIP_SIZE]:
-                with tape.stop_recording():
-                    gradients = tape.gradient(batch_losses, trainable_vars)
-                    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+            # Batch losses
+            loss = tf.stack(tf.split(loss, BATCH_SIZE))
+            loss = tf.math.reduce_sum(loss, axis=1) / tf.cast(tf.shape(loss)[1], tf.float32)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.jacobian(loss, trainable_vars, experimental_use_pfor=False)
+        for i in tf.range(BATCH_SIZE):
+            self.optimizer.apply_gradients(zip([g[i] for g in gradients], trainable_vars))
 
         self.compiled_metrics.update_state(ground_truth, y_pred)
         return {m.name: m.result() for m in self.metrics}
