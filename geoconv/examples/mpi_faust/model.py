19b898b1adae9cf13c8d5c4b767a0c7b93e21bf1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2022-10-16 14:51:21 +0200

    Made gradient computation faster

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -2,7 +2,7 @@ from tensorflow.keras import Model
 
 import tensorflow as tf
 
-BATCH_SIZE = 130
+AMT_BATCHES = 130
 
 
 class PointCorrespondenceGeoCNN(Model):
@@ -15,11 +15,10 @@ class PointCorrespondenceGeoCNN(Model):
         with tf.GradientTape(persistent=True) as tape:
             y_pred = self([signal, barycentric], training=True)
             loss = self.compiled_loss(ground_truth, y_pred)
-            loss = tf.math.reduce_mean(tf.stack(tf.split(loss, BATCH_SIZE)), axis=1)
-            for batch_losses in loss:
-                with tape.stop_recording():
-                    gradients = tape.gradient(batch_losses, trainable_vars)
-                    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+            loss = tf.math.reduce_mean(tf.stack(tf.split(loss, AMT_BATCHES)), axis=1)
+
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
         self.compiled_metrics.update_state(ground_truth, y_pred)
         return {m.name: m.result() for m in self.metrics}
