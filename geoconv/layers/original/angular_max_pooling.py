970bde9120a92e6a340bfe68052d13485b653474
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2023-10-25 10:25:57 +0200

    Adjusted Documentation

--- geoconv/layers/original/angular_max_pooling.py
+++ geoconv/layers/original/angular_max_pooling.py
@@ -16,6 +16,12 @@ class AngularMaxPooling(keras.layers.Layer):
             The result tensor of a geodesic convolution. It has a size of: (n_vertices, n_rotations, feature_dim),
             where 'n_vertices' references to the total amount of vertices in the triangle mesh, 'n_rotations' to the
             amount of rotations conducted in the geodesic convolution and 'feature_dim' to the feature dimensionality.
+
+        Returns
+        -------
+        tf.Tensor:
+            A two-dimensional tensor of size (n_vertices, feature_dim), that contains the convolution result of the
+            rotation that has the largest Euclidean norm.
         """
         maximal_response = tf.norm(inputs, ord="euclidean", axis=-1)
         maximal_response = tf.cast(tf.argmax(maximal_response, axis=1), dtype=tf.int32)
