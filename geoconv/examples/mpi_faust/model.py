82e44bd4d30d8ae5579ecd522d92e58bacc442df
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-01-08 10:47:05 +0100

    Added device context to call method

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -10,9 +10,9 @@ class Imcnn(tf.keras.Model):
     def __init__(self, template_radius, splits):
         super().__init__()
         self.amp = AngularMaxPooling()
-        gpus = [device_name for device_name in device_lib.list_local_devices() if "GPU" in device_name.name]
+        self.gpus = [device_name for device_name in device_lib.list_local_devices() if "GPU" in device_name.name]
 
-        with tf.device(gpus[0].name):
+        with tf.device(self.gpus[0].name):
             self.conv1 = ConvDirac(
                 amt_templates=96,
                 template_radius=template_radius,
@@ -35,7 +35,7 @@ class Imcnn(tf.keras.Model):
                 splits=splits,
             )
 
-        with tf.device(gpus[1].name):
+        with tf.device(self.gpus[1].name):
             self.conv4 = ConvDirac(
                 amt_templates=384,
                 template_radius=template_radius,
@@ -53,15 +53,17 @@ class Imcnn(tf.keras.Model):
             self.output_layer = tf.keras.layers.Dense(6890)
 
     def call(self, inputs, training=None, mask=None):
-        signal, bc = inputs
-        signal = self.conv1([signal, bc])
-        signal = self.amp(signal)
-        signal = self.conv2([signal, bc])
-        signal = self.amp(signal)
-        signal = self.conv3([signal, bc])
-        signal = self.amp(signal)
-        signal = self.conv4([signal, bc])
-        signal = self.amp(signal)
-        signal = self.conv5([signal, bc])
-        signal = self.amp(signal)
-        return self.output_layer(signal)
+        with tf.device(self.gpus[0].name):
+            signal, bc = inputs
+            signal = self.conv1([signal, bc])
+            signal = self.amp(signal)
+            signal = self.conv2([signal, bc])
+            signal = self.amp(signal)
+            signal = self.conv3([signal, bc])
+            signal = self.amp(signal)
+        with tf.device(self.gpus[0].name):
+            signal = self.conv4([signal, bc])
+            signal = self.amp(signal)
+            signal = self.conv5([signal, bc])
+            signal = self.amp(signal)
+            return self.output_layer(signal)
