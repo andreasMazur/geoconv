3c432f8bf50edc69035b1353c1674e6d5b723028
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-01-18 16:05:29 +0100

    Adjusted model

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -13,16 +13,22 @@ class Imcnn(tf.keras.Model):
         self.template_radius = template_radius
         self.splits = splits
         self.rotation_delta = rotation_delta
-        self.output_dims = [300, 200]  # 96, 256, 384, 384, 256
 
-        self.amp = AngularMaxPooling()
+        #################
+        # Handling Input
+        #################
+        self.normalize = keras.layers.Normalization(axis=-1)
         self.downsize_dense = keras.layers.Dense(64, activation="relu", name="downsize")
         self.downsize_bn = keras.layers.BatchNormalization(axis=-1, name="BN_downsize")
-        self.normalize = keras.layers.Normalization(axis=-1)
 
+        ##################
+        # Global Features
+        ##################
+        self.output_dims = [20, 20, 20, 20, 20]
         self.isc_layers = []
         self.bn_layers = []
         self.do_layers = []
+        self.amp_layers = []
         for idx in range(len(self.output_dims)):
             self.isc_layers.append(
                 ConvDirac(
@@ -36,16 +42,61 @@ class Imcnn(tf.keras.Model):
             )
             self.bn_layers.append(keras.layers.BatchNormalization(axis=-1, name=f"BN_layer_{idx}"))
             self.do_layers.append(keras.layers.Dropout(rate=0.2, name=f"DO_layer_{idx}"))
+            if idx < len(self.output_dims) - 1:
+                self.amp_layers.append(AngularMaxPooling())
+
+        ##################
+        # Local Features
+        ##################
+        self.local_isc = ConvDirac(
+            amt_templates=400,
+            template_radius=self.template_radius,
+            activation="relu",
+            name=f"local_ISC_layer",
+            splits=self.splits,
+            rotation_delta=self.rotation_delta
+        )
+
+        #################
+        # Global + Local
+        #################
+        self.concat_layer = keras.layers.Concatenate(axis=1)
+        self.concat_amp = AngularMaxPooling()
+
+        #########
+        # Output
+        #########
         self.output_dense = keras.layers.Dense(6890, name="output")
 
     def call(self, inputs, orientations=None, training=None, mask=None):
+        #################
+        # Handling Input
+        #################
         signal, bc = inputs
         signal = self.normalize(signal)
         signal = self.downsize_dense(signal)
         signal = self.downsize_bn(signal)
-        for idx in range(len(self.output_dims)):
-            signal = self.isc_layers[idx]([signal, bc])
-            signal = self.amp(signal)
-            signal = self.bn_layers[idx](signal)
-            signal = self.do_layers[idx](signal)
-        return self.output_dense(signal)
+
+        ##################
+        # Global Features
+        ##################
+        global_signal = self.isc_layers[0]([signal, bc])
+        for idx in range(1, len(self.output_dims)):
+            global_signal = self.isc_layers[idx]([global_signal, bc])
+            if idx < len(self.output_dims) - 1:
+                global_signal = self.amp_layers[idx](global_signal)
+                global_signal = self.bn_layers[idx](global_signal)
+                global_signal = self.do_layers[idx](global_signal)
+
+        ##################
+        # Local Features
+        ##################
+        local_signal = self.local_isc([signal, bc])
+
+        #################
+        # Global + Local
+        #################
+        combined_signal = self.concat_layer([global_signal, local_signal])
+        combined_signal = self.concat_amp(combined_signal)
+
+        return self.output_dense(combined_signal)
