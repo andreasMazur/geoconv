c5e7f044d21f5968d2380f4b1ccf1ae904275214
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-01-19 13:42:39 +0100

    Adjusted model architecture

--- geoconv/examples/mpi_faust/model.py
+++ geoconv/examples/mpi_faust/model.py
@@ -13,6 +13,7 @@ class Imcnn(tf.keras.Model):
         self.template_radius = template_radius
         self.splits = splits
         self.rotation_delta = rotation_delta
+        self.output_dims = [100 for _ in range(3)]
 
         #################
         # Handling Input
@@ -24,7 +25,6 @@ class Imcnn(tf.keras.Model):
         ##################
         # Global Features
         ##################
-        self.output_dims = [100 for _ in range(5)]
         self.isc_layers = []
         self.bn_layers = []
         self.do_layers = []
@@ -44,23 +44,9 @@ class Imcnn(tf.keras.Model):
             self.do_layers.append(keras.layers.Dropout(rate=0.2, name=f"DO_layer_{idx}"))
             self.amp_layers.append(AngularMaxPooling())
 
-        ##################
-        # Local Features
-        ##################
-        self.local_isc = ConvDirac(
-            amt_templates=500,
-            template_radius=self.template_radius,
-            activation="relu",
-            name=f"local_ISC_layer",
-            splits=self.splits,
-            rotation_delta=self.rotation_delta
-        )
-        self.local_amp = AngularMaxPooling()
-
-        ##########################
-        # Output = Global + Local
-        ##########################
-        self.concat_layer = keras.layers.Concatenate(axis=1)
+        #########
+        # Output
+        #########
         self.output_dense = keras.layers.Dense(6890, name="output")
 
     def call(self, inputs, orientations=None, training=None, mask=None):
@@ -75,24 +61,13 @@ class Imcnn(tf.keras.Model):
         ##################
         # Global Features
         ##################
-        global_signal = self.isc_layers[0]([signal, bc])
-        global_signal = self.amp_layers[0](global_signal)
-        global_signal = self.bn_layers[0](global_signal)
-        global_signal = self.do_layers[0](global_signal)
-        for idx in range(1, len(self.output_dims)):
-            global_signal = self.isc_layers[idx]([global_signal, bc])
-            global_signal = self.amp_layers[idx](global_signal)
-            global_signal = self.bn_layers[idx](global_signal)
-            global_signal = self.do_layers[idx](global_signal)
+        for idx in range(len(self.output_dims)):
+            signal = self.isc_layers[idx]([signal, bc])
+            signal = self.amp_layers[idx](signal)
+            signal = self.bn_layers[idx](signal)
+            signal = self.do_layers[idx](signal)
 
-        ##################
-        # Local Features
-        ##################
-        local_signal = self.local_isc([signal, bc])
-        local_signal = self.local_amp(local_signal)
-
-        ##########################
-        # Output = Global + Local
-        ##########################
-        combined_signal = self.concat_layer([global_signal, local_signal])
-        return self.output_dense(combined_signal)
+        #########
+        # Output
+        #########
+        return self.output_dense(signal)
