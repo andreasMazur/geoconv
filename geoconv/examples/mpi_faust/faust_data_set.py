1c2fe9514b68184b9a990892afb5de20e814450f
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-01-19 16:14:15 +0100

    Removed bug

--- geoconv/examples/mpi_faust/faust_data_set.py
+++ geoconv/examples/mpi_faust/faust_data_set.py
@@ -75,7 +75,7 @@ def faust_generator(path_to_zip, set_type=0, only_signal=False):
         if only_signal:
             yield signal
         else:
-            yield (signal, bc, tf.constant([idx])), gt
+            yield (signal, bc), gt
 
 
 def load_preprocessed_faust(path_to_zip,
@@ -118,8 +118,7 @@ def load_preprocessed_faust(path_to_zip,
         output_signature = (
             (
                 tf.TensorSpec(shape=(None, signal_dim,), dtype=tf.float32),  # Signal
-                tf.TensorSpec(shape=(None,) + kernel_size + (3, 2), dtype=tf.float32),  # Barycentric Coordinates
-                tf.TensorSpec(shape=(None,), dtype=tf.int32)
+                tf.TensorSpec(shape=(None,) + kernel_size + (3, 2), dtype=tf.float32)  # Barycentric Coordinates
             ),
             tf.TensorSpec(shape=(None,), dtype=tf.float32)
         )
