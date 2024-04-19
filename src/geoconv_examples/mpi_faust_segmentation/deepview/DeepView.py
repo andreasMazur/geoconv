49dd829ebb26d882b337220d09b75c98296904db
robertsi20 <isaacroberts689@gmail.com>
2024-04-19 18:02:07 +0200

    Updates to Deepview for Mesh viz
    ability to select multiple points and visualize them in the mesh

--- src/geoconv_examples/mpi_faust_segmentation/deepview/DeepView.py
+++ src/geoconv_examples/mpi_faust_segmentation/deepview/DeepView.py
@@ -1,10 +1,16 @@
 from deepview.DeepView import DeepView
-
+from .viz_imcnn import SelectFromCollection
 import numpy as np
-
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+import warnings
 
 class DeepViewSubClass(DeepView):
 
+    def __init__(self,*args, selector=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.selector = selector
     @property
     def distances(self):
         '''
@@ -21,16 +27,161 @@ class DeepViewSubClass(DeepView):
         stacked = np.dstack((fisher, eucl))
         return stacked.sum(-1)
 
-    def get_artist_sample(self, point):
-        """Maps the location of an embedded point to it's image.
+    def get_artist_sample(self, sample_ids):
+        """Maps the location of embedded points to their image.
 
-		# TODO: Pass proper data_viz function as argument to DeepView instantitation
 
         Parameters
         ----------
         point: tuple
         	Coorinates of click in image
         """
-        sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
-        yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
-        return sample_id, yp, yt
+        # sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
+        yps = []
+        yts = []
+        for sample_id in sample_ids:
+            yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
+            yps.append(yp)
+            yts.append(yt)
+        return sample_ids, yps, yts
+
+    def show(self):
+        '''
+        Shows the current plot.
+        '''
+        if not hasattr(self, 'fig'):
+            self._init_plots()
+
+        x_min, y_min, x_max, y_max = self._get_plot_measures()
+
+        self.cls_plot.set_data(self.classifier_view)
+        self.cls_plot.set_extent((x_min, x_max, y_max, y_min))
+        self.ax.set_xlim((x_min, x_max))
+        self.ax.set_ylim((y_min, y_max))
+
+        params_str = 'batch size: %d - n: %d - $\lambda$: %.2f - res: %d'
+        desc = params_str % (self.batch_size, self.n, self.lam, self.resolution)
+        self.desc.set_text(desc)
+
+        for c in range(self.n_classes):
+            data = self.embedded[self.y_true == c]
+            self.sample_plots[c].set_data(data.transpose())
+
+        for c in range(self.n_classes):
+            data = self.embedded[np.logical_and(self.y_pred == c, self.background_at != c)]
+            self.sample_plots[self.n_classes + c].set_data(data.transpose())
+
+        if os.name == 'posix':
+            self.fig.canvas.manager.window.raise_()
+
+        self.selector = SelectFromCollection(self.ax, self.embedded)
+
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        plt.show()
+
+    def _init_plots(self):
+        '''
+        Initialises matplotlib artists and plots.
+        '''
+        if self.interactive:
+            plt.ion()
+        self.fig, self.ax = plt.subplots(1, 1, figsize=(8, 8))
+        self.ax.set_title(self.title)
+        self.desc = self.fig.text(0.5, 0.02, '', fontsize=8, ha='center')
+        self.cls_plot = self.ax.imshow(np.zeros([5, 5, 3]),
+                                       interpolation='gaussian', zorder=0, vmin=0, vmax=1)
+
+        self.sample_plots = []
+
+        for c in range(self.n_classes):
+            color = self.cmap(c / (self.n_classes - 1))
+            plot = self.ax.plot([], [], 'o', label=self.classes[c],
+                                color=color, zorder=2, picker=mpl.rcParams['lines.markersize'])
+            self.sample_plots.append(plot[0])
+
+        for c in range(self.n_classes):
+            color = self.cmap(c / (self.n_classes - 1))
+            plot = self.ax.plot([], [], 'o', markeredgecolor=color,
+                                fillstyle='none', ms=12, mew=2.5, zorder=1)
+            self.sample_plots.append(plot[0])
+
+        # set the mouse-event listeners
+        # self.fig.canvas.mpl_connect("key_press_event", self.show_sample)
+
+        # self.fig.canvas.mpl_connect('pick_event', self.show_sample)
+        self.fig.canvas.mpl_connect('key_press_event', self.show_sample)
+        self.disable_synth = False
+        self.ax.legend()
+
+    # def accept(self, event, selector):
+    #     if event.key == "enter":
+    #         print("Selected points:")
+    #         print(selector.xys[selector.ind])
+    #         selector.disconnect()
+    #         # seldax.set_title("")
+    #         self.fig.canvas.draw()
+
+    def show_sample(self, event):
+        '''
+        Invoked when the user clicks on the plot. Determines the
+        embedded or synthesised sample at the click location and
+        passes it to the data_viz method, together with the prediction,
+        if present a groun truth label and the 2D click location.
+        '''
+
+        # when there is an artist attribute, a
+        # concrete sample was clicked, otherwise
+        # show the according synthesised image
+
+
+        # print(event)
+        # print(event.ind)
+
+        # if hasattr(event, 'artist'):
+        if event.key == "enter":
+            indices = self.selector.ind
+            sample, p, t = self.get_artist_sample(indices)
+            title = '%s <-> %s' if p != t else '%s --- %s'
+            title = title % (self.classes[p], self.classes[t])
+            self.disable_synth = True
+        elif not self.disable_synth:
+            # workaraound: inverse embedding needs more points
+            # otherwise it doens't work --> [point]*5
+            point = np.array([[event.xdata, event.ydata]] * 5)
+
+            # if the outside of the plot was clicked, points are None
+            if None in point[0]:
+                return
+
+            sample = self.inverse(point)[0]
+            sample += abs(sample.min())
+            sample /= sample.max()
+            title = 'Synthesised at [%.1f, %.1f]' % tuple(point[0])
+            p, t = self.get_mesh_prediction_at(*point[0]), None
+        else:
+            self.disable_synth = False
+            return
+
+        is_image = self.is_image(sample)
+        rank_perm = np.roll(range(len(sample.shape)), -1)
+        sample_T = sample.transpose(rank_perm)
+        is_transformed_image = self.is_image(sample_T)
+
+        if self.data_viz is not None:
+            self.data_viz(sample, p, t, self.cmap)
+            return
+        # try to show the image, if the shape allows it
+        elif is_image:
+            img = sample - sample.min()
+        elif is_transformed_image:
+            img = sample_T - sample_T.min()
+        else:
+            warnings.warn("Data visualization not possible, as the data points have"
+                          "no image shape. Pass a function in the data_viz argument,"
+                          "to enable custom data visualization.")
+            return
+
+        f, a = plt.subplots(1, 1)
+        a.imshow(img / img.max())
+        a.set_title(title)
--- src/geoconv_examples/mpi_faust_segmentation/deepview/viz_imcnn.py
+++ src/geoconv_examples/mpi_faust_segmentation/deepview/viz_imcnn.py
@@ -1,7 +1,65 @@
 import tensorflow as tf
 import numpy as np
 import trimesh
+from matplotlib.path import Path
+from matplotlib.widgets import LassoSelector
 
+class SelectFromCollection:
+    """
+    Select indices from a matplotlib collection using `LassoSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        Axes to interact with.
+    collection : `matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to *alpha_other*.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        # self.fc = collection.get_facecolors()
+        # if len(self.fc) == 0:
+        #     raise ValueError('Collection must have a facecolor')
+        # elif len(self.fc) == 1:
+        #     self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.lasso = LassoSelector(ax, onselect=self.onselect)
+        self.ind = []
+
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        # print(self.ind)
+        # self.fc[:, -1] = self.alpha_other
+        # self.fc[self.ind, -1] = 1
+        # self.collection.set_facecolors(self.fc)
+        # self.canvas.draw_idle()
+        return self.ind
+
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        # self.fc[:, -1] = 1
+        # self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
 
 def embed(model, inputs):
     #################
@@ -50,7 +108,8 @@ def pred_wrapper(x, model):
     output = tf.nn.softmax(logits)
     return output
 
-def interactive_seg_correction(shape_idx, coordinates, all_segments, ground_truth, query_idx, file_name="corrected_labels.csv"):
+def interactive_seg_correction(shape_idx, coordinates, all_segments, ground_truth, query_idxs,
+                               idxs_preds, idxs_labels,cmap, file_name="corrected_labels.csv"):
     """Interactively correct the segmentation label.
 
     Parameters
@@ -80,43 +139,41 @@ def interactive_seg_correction(shape_idx, coordinates, all_segments, ground_trut
         9: "right_hand"
     }
 
-    query_coordinates = coordinates[query_idx]
+    query_coordinates = coordinates[query_idxs]
 
-    # Set default value for query-found
-    query_found = False
+    # Assign vertex colors
+    unique_segment_labels = np.unique(ground_truth)
+    unique_segment_colors = [cmap(c / (10 - 1)) for c in range(10)]  # amount classes + 10
+    segments = np.concatenate([all_segments[x] for x in unique_segment_labels], axis=0)
+    colors = np.full(shape=(segments.shape[0], 4), fill_value=[1, 1, 1., 1.])
+    for idx, qc in enumerate(query_coordinates):
+        colors[np.where((qc == segments).all(axis=-1))[0][0]] = unique_segment_colors[ground_truth[idx]]
 
-    for segment in all_segments:
-        # Check whether query coordinates are in segment
-        query_found = query_coordinates in segment
-        if query_found:
-            # Colorize segment and assign unique color to query vertex
-            colors = np.full(shape=(segment.shape[0], 4), fill_value=[0, 0, 255, 255])
-            colors[np.where((query_coordinates == segment).all(axis=-1))[0][0]] = [0, 255, 0, 255]
 
-            # Show mesh
-            mesh = trimesh.PointCloud(vertices=segment, colors=colors)
-            print(f"Current class name: {class_dict[ground_truth]} / {ground_truth}")
-            mesh.show()
+    # Show mesh
+    mesh = trimesh.PointCloud(vertices=segments, colors=colors)
+    # print(f"Current class name: {class_dict[ground_truth]} / {ground_truth}")
+    mesh.show()
 
-            # User interactions
-            prompt_1 = input("Write this to the output file? [y/n]:").lower()
-            if prompt_1 == "y":
-                # Ask for class label
-                prompt_2 = input("What is the correct class? Give a number 0-9:").lower()
-                real_class = class_dict[int(prompt_2)]
-
-                # Write class label given by user to file
-                with open(file_name, "a") as f:
-                    f.write(str(shape_idx) + "," + str(query_idx) + "," + real_class + "\n")
-            elif prompt_1 == "n":
-                print("okay :(")
-
-            # End loop if query was found
-            break
-
-    # Raise error if query vertex is in no segment
-    if not query_found:
-        raise RuntimeError("Query vertex could not be found.")
+    #         # User interactions
+    #     prompt_1 = input("Write this to the output file? [y/n]:").lower()
+    #     if prompt_1 == "y":
+    #         # Ask for class label
+    #         prompt_2 = input("What is the correct class? Give a number 0-9:").lower()
+    #         real_class = class_dict[int(prompt_2)]
+    #
+    #             # Write class label given by user to file
+    #         with open(file_name, "a") as f:
+    #             f.write(str(shape_idx) + "," + str(query_idx) + "," + real_class + "\n")
+    #     elif prompt_1 == "n":
+    #         print("okay :(")
+    #
+    #     # End loop if query was found
+    #     break
+    #
+    # # Raise error if query vertex is in no segment
+    # if not query_found:
+    #     raise RuntimeError("Query vertex could not be found.")
 
 
 def load_model(model_path):
