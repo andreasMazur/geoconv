f2a72776676182788b0dcd458a4aafd866e2195c
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-04-22 10:41:49 +0200

    Added csv visualization

--- src/geoconv_examples/mpi_faust_segmentation/tensorflow/experiment.py
+++ src/geoconv_examples/mpi_faust_segmentation/tensorflow/experiment.py
@@ -2,6 +2,32 @@ from geoconv_examples.mpi_faust.tensorflow.training_demo import train_model
 from geoconv_examples.mpi_faust_segmentation.tensorflow.convert_dataset import convert_dataset
 
 from pathlib import Path
+from matplotlib import pyplot as plt
+
+import pandas as pd
+
+
+def visualize_csv(csv_path, figure_name="training_statistics"):
+    csv = pd.read_csv(csv_path)
+
+    # Configure plot
+    n_rows, n_cols, cm = 2, 1, 1/2.54
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(12*cm, 12.2*cm), sharex=True)
+    for j in range(n_rows):
+        axs[j].grid()
+
+    # Configure axes
+    axs[0].plot(csv["loss"], label="loss")
+    axs[0].plot(csv["val_loss"], label="val_loss")
+    axs[0].set_ylabel("Loss")
+
+    axs[1].plot(csv["sparse_categorical_accuracy"])
+    axs[1].plot(csv["val_sparse_categorical_accuracy"])
+    axs[1].set_xlabel("Epoch")
+    axs[1].set_ylabel("Sparse Categorical Accuracy")
+
+    plt.savefig(f"{figure_name}.svg", bbox_inches="tight")
+    plt.show()
 
 
 def run_experiment(registration_path, old_dataset_path, new_dataset_path, logging_dir):
@@ -39,6 +65,9 @@ def run_experiment(registration_path, old_dataset_path, new_dataset_path, loggin
         epochs=10,
         seeds=[10]
     )
+
+    # Visualize training results
+    visualize_csv(f"{logging_dir}/training_0.log", figure_name=f"{logging_dir}/training_statistics")
 
     ########################################################
     # 2.) Run experiment with corrected segmentation labels
