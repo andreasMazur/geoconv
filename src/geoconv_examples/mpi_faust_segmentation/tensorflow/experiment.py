0a788fb97f50630110bb027b2de7be1950e0fa4b
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-04-22 10:06:25 +0200

    Added logging directory paramter

--- src/geoconv_examples/mpi_faust_segmentation/tensorflow/experiment.py
+++ src/geoconv_examples/mpi_faust_segmentation/tensorflow/experiment.py
@@ -4,7 +4,7 @@ from geoconv_examples.mpi_faust_segmentation.tensorflow.convert_dataset import c
 from pathlib import Path
 
 
-def run_experiment(registration_path, old_dataset_path, new_dataset_path):
+def run_experiment(registration_path, old_dataset_path, new_dataset_path, logging_dir):
     #######################################################
     # 1.) Run experiment w/o corrected segmentation labels
     #######################################################
@@ -23,16 +23,13 @@ def run_experiment(registration_path, old_dataset_path, new_dataset_path):
     train_model(
         reference_mesh_path=f"{registration_path}/tr_reg_000.ply",
         preprocessed_data=new_dataset_path,
-        ### PRE-PROCESSING ###
         n_radial=5,
         n_angular=8,
         registration_path=registration_path,
-        # Make sure you have installed: https://github.com/uhlmanngroup/pyshot (do not use `pip install pyshot`!)
         compute_shot=True,
         precomputed_gpc_radius=R,
-        ### GENERAL ###
         template_radius=R * 0.75,
-        logging_dir="./tensorflow_seg_demo_logging",
+        logging_dir=logging_dir,
         processes=1,
         layer_conf=[(96, 1)],
         init_lr=0.00165,
@@ -43,4 +40,7 @@ def run_experiment(registration_path, old_dataset_path, new_dataset_path):
         seeds=[10]
     )
 
+    ########################################################
     # 2.) Run experiment with corrected segmentation labels
+    ########################################################
+    # Load corrected segmentation labels
