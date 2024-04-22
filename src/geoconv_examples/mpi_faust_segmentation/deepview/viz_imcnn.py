a082d1cdc1f597814be7774720361ee8c6df7d44
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-04-22 11:23:39 +0200

    Added correction pipeline function

--- src/geoconv_examples/mpi_faust_segmentation/deepview/viz_imcnn.py
+++ src/geoconv_examples/mpi_faust_segmentation/deepview/viz_imcnn.py
@@ -1,8 +1,13 @@
+from geoconv_examples.mpi_faust.tensorflow.faust_data_set import faust_generator
+from geoconv_examples.mpi_faust_segmentation.deepview.DeepView import DeepViewSubClass
+
+from matplotlib.path import Path
+from matplotlib.widgets import LassoSelector
+
 import tensorflow as tf
 import numpy as np
 import trimesh
-from matplotlib.path import Path
-from matplotlib.widgets import LassoSelector
+
 
 class SelectFromCollection:
     """
@@ -61,6 +66,7 @@ class SelectFromCollection:
         # self.collection.set_facecolors(self.fc)
         self.canvas.draw_idle()
 
+
 def embed(model, inputs):
     #################
     # Handling Input
@@ -82,21 +88,8 @@ def embed(model, inputs):
     #########
     # Output
     #########
-    return signal
+    return np.array(signal)
 
-def get_embeddings(dataset,model, shape):
-    embeddings = []
-    coords = []
-    gts = []
-    i = 0
-    for (signal, bc, coord), gt in dataset:
-        if i == shape:
-            embeddings.append(embed(model, [signal, bc]))
-            coords.append(coord)
-            gts.append(gt)
-            return embeddings, gt, coords
-        i += 1
-    return None
 
 def pred_wrapper(x, model):
     """
@@ -108,81 +101,118 @@ def pred_wrapper(x, model):
     output = tf.nn.softmax(logits)
     return output
 
-def interactive_seg_correction(shape_idx, coordinates, all_segments, ground_truth, query_idxs,
-                               idxs_preds, idxs_labels,cmap, file_name="corrected_labels.csv"):
-    """Interactively correct the segmentation label.
 
-    Parameters
-    ----------
-    shape_idx: int
-        The index of the current shape in the FAUST dataset.
-    coordinates: np.ndarray
-        The coordinates of the current shape.
-    ground_truth: np.ndarray
-        The current ground truth segmentation labels for the given shape.
-    query_idx: int
-        The index of the query vertex.
-    file_name: str
-        The file name in which to write corrections.
-    """
-    # Define class names
-    class_dict = {
-        0: "right_arm",
-        1: "left_arm",
-        2: "torso",
-        3: "head",
-        4: "left_foot",
-        5: "right_foot",
-        6: "left_leg",
-        7: "right_leg",
-        8: "left_hand",
-        9: "right_hand"
-    }
+def interactive_seg_correction(coordinates,
+                               all_segments,
+                               ground_truth,
+                               query_idxs,
+                               cmap,
+                               amount_classes,
+                               file_name="corrected_labels.csv"):
+    """Interactively correct the segmentation label."""
 
-    query_coordinates = coordinates[query_idxs]
+    ##########
+    # 3D Plot
+    ##########
+    # Define colors
+    segments = np.concatenate([all_segments[x] for x in np.unique(ground_truth)], axis=0)
+    colors = np.full(shape=(segments.shape[0], 4), fill_value=[1., 1., 1., 1.])  # default color
 
-    # Assign vertex colors
-    unique_segment_labels = np.unique(ground_truth)
-    unique_segment_colors = [cmap(c / (10 - 1)) for c in range(10)]  # amount classes + 10
-    segments = np.concatenate([all_segments[x] for x in unique_segment_labels], axis=0)
-    colors = np.full(shape=(segments.shape[0], 4), fill_value=[1, 1, 1., 1.])
-    for idx, qc in enumerate(query_coordinates):
+    unique_segment_colors = [cmap(c / (amount_classes - 1)) for c in range(amount_classes)]
+    for idx, qc in enumerate(coordinates[query_idxs]):
         colors[np.where((qc == segments).all(axis=-1))[0][0]] = unique_segment_colors[ground_truth[idx]]
-
 
     # Show mesh
     mesh = trimesh.PointCloud(vertices=segments, colors=colors)
-    # print(f"Current class name: {class_dict[ground_truth]} / {ground_truth}")
     mesh.show()
 
-    #         # User interactions
+    # TODO:
+    #   Define class names
+    #   class_dict = {
+    #         0: "right_arm",
+    #         1: "left_arm",
+    #         2: "torso",
+    #         3: "head",
+    #         4: "left_foot",
+    #         5: "right_foot",
+    #         6: "left_leg",
+    #         7: "right_leg",
+    #         8: "left_hand",
+    #         9: "right_hand"
+    #   }
+    #     # User interactions
     #     prompt_1 = input("Write this to the output file? [y/n]:").lower()
     #     if prompt_1 == "y":
     #         # Ask for class label
     #         prompt_2 = input("What is the correct class? Give a number 0-9:").lower()
     #         real_class = class_dict[int(prompt_2)]
-    #
     #             # Write class label given by user to file
     #         with open(file_name, "a") as f:
     #             f.write(str(shape_idx) + "," + str(query_idx) + "," + real_class + "\n")
     #     elif prompt_1 == "n":
     #         print("okay :(")
-    #
     #     # End loop if query was found
     #     break
-    #
-    # # Raise error if query vertex is in no segment
-    # if not query_found:
-    #     raise RuntimeError("Query vertex could not be found.")
+    #   if not query_found:
+    #        raise RuntimeError("Query vertex could not be found.")
 
 
-def load_model(model_path):
-    """
-    loads the model from path
-    :param model_path: path to model
-    :return: tf.model
-    """
+def correction_pipeline(model_path, dataset_path):
+    # Load model and dataset
     model = tf.keras.models.load_model(model_path)
-    return model
+    dataset = faust_generator(dataset_path, set_type=3, only_signal=False, return_coordinates=True)
 
+    for (signal, bc, coordinates), labels in dataset:
+        coordinates = np.array(coordinates)
+        embeddings = embed(model, [signal, bc])
 
+        # Determine segments by binning the vertices by their class labels
+        all_segments = [coordinates[np.where(labels == x)[0]] for x in range(10)]
+
+        # loading the values needed for visualizing the mesh segment
+        def dv_show_seg(vertex_idx, point, pred, gt):
+            interactive_seg_correction(
+                coordinates=coordinates,
+                all_segments=all_segments,
+                ground_truth=gt,
+                query_idxs=vertex_idx,
+                cmap=cmap,
+                file_name="corrected_labels.csv"
+            )
+
+        # --- Deep View Parameters ----
+        classes = np.arange(np.unique(labels).shape[0])
+        batch_size = 64
+        max_samples = 7000
+        data_shape = (96,)
+        resolution = 100
+        N = 10
+        lam = 1
+        cmap = 'tab10'
+        metric = 'euclidean'
+        disc_dist = False
+
+        # to make sure deepview.show is blocking,
+        # disable interactive mode
+        interactive = False
+        title = 'movie-reviews BERT'
+
+        imcnn_deepview = DeepViewSubClass(
+            lambda x: pred_wrapper(x, model),
+            classes,
+            max_samples,
+            batch_size,
+            data_shape,
+            N,
+            lam,
+            resolution,
+            cmap,
+            interactive,
+            title,
+            data_viz=dv_show_seg,
+            metric=metric,
+            disc_dist=disc_dist
+        )
+
+        imcnn_deepview.add_samples(embeddings, labels)
+        imcnn_deepview.show()
