Fixed bug in computation order

src/geoconv_examples/graspable/data/preprocess.py
@@ -41,20 +41,24 @@ def preprocess_data(data_path, target_dir, temp_dir=None, processes=1, n_radial=
             for edge_f in edge_faces:
                 update_mask = np.logical_not((edge_f == mesh.faces).all(axis=-1))
                 face_mask = np.logical_and(face_mask, update_mask)
-        mesh = trimesh.Trimesh(mesh.vertices, mesh.faces[face_mask])
+
+        # Save label mask for ground truth correction later
+        np.save(label_mask_name, np.unique(mesh.faces[face_mask]))
 
         # Get geodesic diameter
         gd = GEODESIC_DIAMETERS[(GEODESIC_DIAMETERS == float(file_name[:-4])).any(axis=-1)][0, 0]
+
+        # Normalize mesh
+        mesh = trimesh.Trimesh(mesh.vertices, mesh.faces[face_mask])
         normed_mesh, geodesic_diameter = normalize_mesh(mesh, geodesic_diameter=gd)
 
         # Log geodesic diameter
         with open(f"{target_dir}/geodesic_diameters.txt", "a") as diameters_file:
             diameters_file.write(f"{geodesic_diameter}\n")
 
-        # Save normalized mesh and label mask
+        # Save normalized mesh
         np.save(normalized_v_name, np.asarray(normed_mesh.vertices))
         np.save(normalized_f_name, np.asarray(normed_mesh.faces))
-        np.save(label_mask_name, np.unique(mesh.faces[face_mask]))
 
     # Find GPC-system radius
     amount_meshes, gpc_radius = mesh_idx + 1, .0
@@ -86,7 +90,7 @@ def preprocess_data(data_path, target_dir, temp_dir=None, processes=1, n_radial=
         bc_name = f"{target_dir}/BC_{file_name}.npy"
         gt_name = f"{target_dir}/GT_{file_name}.npy"
         signal_name = f"{target_dir}/SIGNAL_{file_name}.npy"
-        label_mask_name = f"{temp_dir}/mask_{mesh_idx}"
+        label_mask_name = f"{temp_dir}/mask_{mesh_idx}.npy"
 
         # Load normalized mesh
         vertices = np.load(f"{temp_dir}/vertices_{mesh_idx}.npy")