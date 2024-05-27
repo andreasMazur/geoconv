10b809dd22277234f27e1d4eb9a5349eae0ea132
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-05-27 10:17:11 +0200

    Added common utilities for shape pre-processing

---
+++ src/geoconv/utils/common.py
@@ -0,0 +1,44 @@
+from geoconv.preprocessing.gpc_system_group import GPCSystemGroup
+from geoconv.utils.misc import normalize_mesh, find_largest_one_hop_dist
+
+import json
+import os
+import numpy as np
+
+
+def compute_gpc_systems(shape, output_dir, processes=1):
+    """Wrapper function that computes all GPC systems for one given shape.
+
+    Parameters
+    ----------
+    shape: trimesh.Trimesh
+        A manifold mesh.
+    output_dir: str
+        The directory where the GPC-systems shall be stored.
+    processes: int
+        The amount of processes to be used concurrently.
+    """
+    # 0.) Create output dir if not existent
+    os.makedirs(output_dir, exist_ok=True)
+
+    # 1.) Normalize shape
+    shape, geodesic_diameter = normalize_mesh(shape)
+
+    # 2.) Compute GPC-systems
+    gpc_systems = GPCSystemGroup(shape, processes=processes)
+    gpc_system_radius = find_largest_one_hop_dist(shape)
+    gpc_systems.compute(u_max=gpc_system_radius)
+    gpc_systems.save(f"{output_dir}/gpc_systems")
+
+    # 3.) Log preprocess properties
+    properties_file_path = f"{output_dir}/preprocess_properties.json"
+    with open(properties_file_path, "w") as properties_file:
+        json.dump(
+            {
+                "non_manifold_edges": np.asarray(shape.as_open3d.get_non_manifold_edges()).shape[0],
+                "gpc_system_radius": gpc_system_radius,
+                "geodesic_diameter": geodesic_diameter
+            },
+            properties_file,
+            indent=4
+        )
---
+++ src/geoconv/utils/data_generator.py
@@ -0,0 +1,114 @@
+from geoconv.utils.misc import get_faces_of_edge
+
+from io import BytesIO
+
+import zipfile
+import trimesh
+import numpy as np
+import os
+
+
+def remove_nme(mesh):
+    """Removes non-manifold edges by removing all their faces.
+
+    Parameters
+    ----------
+    mesh: trimesh.Trimesh
+        The manifold mesh.
+
+    Returns
+    -------
+    trimesh.Trimesh:
+        The mesh without non-manifold edges.
+    """
+    # Check if non-manifold edges exist
+    non_manifold_edges = np.asarray(mesh.as_open3d.get_non_manifold_edges())
+    if non_manifold_edges.shape[0] > 0:
+        # Compute mask that removes non-manifold edges and all their faces
+        face_mask = np.full(mesh.faces.shape[0], True)
+        for edge in non_manifold_edges:
+            sorted_edge, edge_faces = get_faces_of_edge(edge, mesh)
+            for edge_f in edge_faces:
+                update_mask = np.logical_not((edge_f == mesh.faces).all(axis=-1))
+                face_mask = np.logical_and(face_mask, update_mask)
+        # Remove non-manifold edges and faces with mask
+        mesh = trimesh.Trimesh(mesh.vertices, mesh.faces[face_mask])
+    return mesh
+
+
+def down_sample_mesh(mesh, target_number_of_triangles):
+    """Down-samples the mesh to (roughly) the given target number of triangles.
+
+    Parameters
+    ----------
+    mesh: trimesh.Trimesh
+        The mesh to be down-sampled.
+    target_number_of_triangles: int
+        The target amount of triangles. Cannot be ensured.
+
+    Returns
+    -------
+    trimesh.Trimesh:
+        The down-sampled mesh.
+    """
+    mesh = mesh.as_open3d.simplify_quadric_decimation(target_number_of_triangles=target_number_of_triangles)
+    mesh = trimesh.Trimesh(vertices=mesh.vertices, faces=mesh.triangles)
+    trimesh.repair.fill_holes(mesh)
+    return mesh
+
+
+def zip_file_generator(zipfile_path, file_type, manifold_plus_executable=None, down_sample=None):
+    """Loads shapes from a given zip-file and removes non-manifold edges.
+
+    Parameters
+    ----------
+    zipfile_path: str
+        The path to the zip-file.
+    file_type: str
+        The file type of the CAD models.
+    manifold_plus_executable: str
+        The path to the manifold+ algorithm.
+    down_sample: int
+        The target amount of triangles the mesh shall be down-sampled to.
+
+    Returns
+    -------
+    trimesh.Trimesh:
+        A manifold-mesh.
+    """
+    modelnet_40_zip = zipfile.ZipFile(zipfile_path, "r")
+    zip_content = [fn for fn in modelnet_40_zip.namelist() if fn[-3:] == file_type]
+    zip_content.sort()
+
+    for shape_path in zip_content:
+        shape = trimesh.load_mesh(BytesIO(modelnet_40_zip.read(shape_path)), file_type=file_type)
+
+        # Concat meshes if a scene was loaded
+        if type(shape) == trimesh.scene.Scene:
+            shape = trimesh.util.concatenate([y for y in shape.geometry.values()])
+
+        if manifold_plus_executable is not None:
+            # Create temporary file for manifold+ algorithm
+            in_file = f"./in_shape_temp.obj"
+            shape.export(in_file)
+
+            # Create output file
+            out_file = f"./out_shape_temp.obj"
+
+            # Manifold plus algorithm
+            if np.asarray(shape.as_open3d.get_non_manifold_edges()).shape[0] > 0:
+                os.system(f"{manifold_plus_executable} --input {in_file} --output {out_file} --depth 8")
+                shape = trimesh.load_mesh(out_file)
+                # Remove temp files
+                os.remove(out_file)
+
+            # Remove temp files
+            os.remove(in_file)
+
+        if down_sample is not None and shape.faces.shape[0] > down_sample:
+            shape = down_sample_mesh(shape, down_sample)
+
+        # Remove non-manifold meshes
+        shape = remove_nme(shape)
+
+        yield shape
