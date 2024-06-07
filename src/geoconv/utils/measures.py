a29b1b02be0bd62c4f9c3f3b18196b32f240b0fa
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-07 16:39:46 +0200

    Removed unused import

--- src/geoconv/utils/measures.py
+++ src/geoconv/utils/measures.py
@@ -1,4 +1,4 @@
-from geoconv.utils.misc import get_included_faces, normalize_mesh
+from geoconv.utils.misc import normalize_mesh
 
 from matplotlib import pyplot as plt
 from multiprocessing import Pool
