74f16a82df4893404dce4e47f954415be5fe2f54
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-07 16:51:32 +0200

    Removed unused code

--- src/geoconv/utils/misc.py
+++ src/geoconv/utils/misc.py
@@ -239,53 +239,6 @@ def shuffle_mesh_vertices(mesh, given_shuffle=None):
     return shuffled_mesh, shuffle_map, ground_truth
 
 
-def get_included_faces(object_mesh, gpc_system):
-    """Retrieves face indices from GPC-system
-
-    Parameters
-    ----------
-    object_mesh: trimesh.Trimesh
-        The object mesh
-    gpc_system: np.ndarray
-        The considered GPC-system
-
-    Returns
-    -------
-    list:
-        The list of face IDs which are included in the GPC-system
-    """
-    included_face_ids = []
-
-    # Determine vertex IDs that are included in the GPC-system
-    gpc_vertex_ids = np.arange(gpc_system.shape[0])[gpc_system[:, 0] != np.inf]
-
-    # Determine what faces are entirely contained within the GPC-system
-    for face_id, face in enumerate(object_mesh.faces):
-        counter = 0
-        for vertex_id in face:
-            counter = counter + 1 if vertex_id in gpc_vertex_ids else counter
-        if counter == 3:
-            included_face_ids.append(face_id)
-
-    return included_face_ids
-
-
-def get_points_from_polygons(polygons):
-    """Returns the unique set of points given in a set of polygons
-
-    Parameters
-    ----------
-    polygons: np.ndarray
-        Set of polygons from which the set of unique points will be returned
-
-    Returns
-    -------
-    np.ndarray
-        The set of unique points
-    """
-    return np.unique(polygons.reshape((-1, 2)), axis=0)
-
-
 def find_largest_one_hop_dist(object_mesh):
     """Finds the largest Euclidean distance from center vertex to a one-hop neighbor in a triangle mesh
 
