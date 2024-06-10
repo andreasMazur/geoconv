a65aa073327e22e52f3415159cb8d0d18d55cbb4
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-10 11:13:01 +0200

    Added batching over shapes

--- src/geoconv/tensorflow/layers/pooling/angular_max_pooling.py
+++ src/geoconv/tensorflow/layers/pooling/angular_max_pooling.py
@@ -25,8 +25,5 @@ class AngularMaxPooling(keras.layers.Layer):
             all rotations.
         """
         maximal_response = tf.norm(inputs, ord="euclidean", axis=-1)
-        maximal_response = tf.cast(tf.argmax(maximal_response, axis=1), dtype=tf.int32)
-        return tf.gather_nd(
-            indices=tf.stack([tf.range(tf.shape(inputs)[0]), maximal_response], axis=-1),
-            params=inputs
-        )
+        maximal_response = tf.cast(tf.argmax(maximal_response, axis=-1), dtype=tf.int32)
+        return tf.gather(inputs, maximal_response, batch_dims=2)
