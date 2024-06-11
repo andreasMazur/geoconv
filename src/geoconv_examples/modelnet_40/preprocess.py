5ae4b37f2fac779943efb03c0fa2bf50d579a371
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-11 13:07:32 +0200

    Replaced individual bc computation with general wrapper function

--- src/geoconv_examples/modelnet_40/preprocess.py
+++ src/geoconv_examples/modelnet_40/preprocess.py
@@ -1,15 +1,8 @@
-from geoconv.preprocessing.barycentric_coordinates import compute_barycentric_coordinates
-from geoconv.preprocessing.gpc_system_group import GPCSystemGroup
-from geoconv.preprocessing.wrapper import compute_gpc_systems_wrapper
+from geoconv.preprocessing.wrapper import compute_gpc_systems_wrapper, compute_bc_wrapper
 from geoconv.utils.data_generator import zip_file_generator
 
-from tqdm import tqdm
-
-import trimesh
 import shutil
 import json
-import numpy as np
-import os
 
 
 def preprocess(modelnet_path,
@@ -42,7 +35,13 @@ def preprocess(modelnet_path,
         preprocessed_shapes = preprocessed_shapes + 1 if was_successful else preprocessed_shapes
 
     # Compute barycentric coordinates
-    template_configurations, most_gpc_systems = compute_bc(output_path)
+    template_configurations, most_gpc_systems = compute_bc_wrapper(
+        preprocess_dir=output_path,
+        template_sizes=[(3, 6), (5, 8)],
+        scales=[0.75, 1.0, 1.25],
+        load_compressed_gpc_systems=True,
+        processes=processes
+    )
 
     # Add preprocess information to dataset
     with open(f"{output_path}/dataset_properties.json", "a") as properties_file:
@@ -60,58 +59,3 @@ def preprocess(modelnet_path,
         shutil.make_archive(base_name=output_path, format="zip", root_dir=output_path)
         shutil.rmtree(output_path)
         print("Done.")
-
-
-def compute_bc(preprocess_dir, inverse_order=False, shape_classes=None):
-    # Get average template radius and most gpc-systems among shapes
-    gpc_system_radii, most_gpc_systems = [], 0
-    preprocess_dir_temp = f"{preprocess_dir}/ModelNet40"
-    for shape_class in tqdm(os.listdir(preprocess_dir_temp), postfix="Computing template radius for BC.."):
-        for split in ["test", "train"]:
-            shape_folders = os.listdir(f"{preprocess_dir_temp}/{shape_class}/{split}/")
-            shape_folders.sort()
-            for shape_folder in shape_folders:
-                shape_path = f"{preprocess_dir_temp}/{shape_class}/{split}/{shape_folder}"
-                with open(f"{shape_path}/preprocess_properties.json") as properties_file:
-                    properties = json.load(properties_file)
-                    gpc_system_radii.append(properties["gpc_system_radius"])
-                    n_gpc_systems = properties["amount_gpc_systems"]
-                    most_gpc_systems = n_gpc_systems if n_gpc_systems > most_gpc_systems else most_gpc_systems
-    avg_gpc_system_radius = np.array(gpc_system_radii).mean()
-
-    # Define template configurations
-    template_configurations = [
-        (3, 6, avg_gpc_system_radius * .75),
-        (3, 6, avg_gpc_system_radius),
-        (3, 6, avg_gpc_system_radius * 1.25),
-        (5, 8, avg_gpc_system_radius * .75),
-        (5, 8, avg_gpc_system_radius),
-        (5, 8, avg_gpc_system_radius * 1.25)
-    ]
-
-    # Compute BC
-    if shape_classes is None:
-        shape_classes = os.listdir(preprocess_dir_temp)
-    shape_classes.sort()
-    step = -1 if inverse_order else 1
-    for shape_class in shape_classes[::step]:
-        for split in ["test", "train"]:
-            shape_folders = os.listdir(f"{preprocess_dir_temp}/{shape_class}/{split}/")
-            shape_folders.sort()
-            for shape_folder in shape_folders:
-                shape_path = f"{preprocess_dir_temp}/{shape_class}/{split}/{shape_folder}"
-
-                # Load GPC-systems for current mesh
-                gpc_systems = GPCSystemGroup(object_mesh=trimesh.load_mesh(f"{shape_path}/normalized_mesh.stl"))
-                gpc_systems.load(f"{shape_path}/gpc_systems")
-
-                # Compute barycentric coordinates
-                for (n_radial, n_angular, template_radius) in template_configurations:
-                    bc_file_name = f"{shape_path}/BC_{n_radial}_{n_angular}_{template_radius}.npy"
-                    if not os.path.isfile(bc_file_name):
-                        bc = compute_barycentric_coordinates(
-                            gpc_systems, n_radial=n_radial, n_angular=n_angular, radius=template_radius
-                        )
-                        np.save(bc_file_name, bc)
-    print(f"Barycentric coordinates done.")
-    return template_configurations, most_gpc_systems
