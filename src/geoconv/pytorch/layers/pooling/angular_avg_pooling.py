1967ffa4d048b3d232eb0dc3667a6afe3e06d1f0
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-13 10:01:08 +0200

    Simplified pooling

--- src/geoconv/pytorch/layers/pooling/angular_avg_pooling.py
+++ src/geoconv/pytorch/layers/pooling/angular_avg_pooling.py
@@ -12,16 +12,13 @@ class AngularAvgPooling(nn.Module):
         Parameters
         ----------
         inputs: torch.Tensor
-            The result tensor of an intrinsic surface convolution.
-            It has a size of: (n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the total amount
-            of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during the intrinsic
-            surface convolution and 'feature_dim' to the feature dimensionality.
+            A tensor of size: (batch_shapes, n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the
+            total amount of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during
+            the intrinsic surface convolution and 'feature_dim' to the feature dimensionality.
 
         Returns
         -------
         torch.Tensor:
-            A two-dimensional tensor of size (n_vertices, feature_dim), that contains a convolution results for each
-            vertex. Thereby, the convolution result is the average results among the convolution results for
-            all rotations.
+            A three-dimensional tensor of size (batch_shapes, n_vertices, feature_dim).
         """
         return torch.mean(inputs, dim=-2)
--- src/geoconv/pytorch/layers/pooling/angular_min_pooling.py
+++ src/geoconv/pytorch/layers/pooling/angular_min_pooling.py
@@ -12,21 +12,13 @@ class AngularMinPooling(nn.Module):
         Parameters
         ----------
         inputs: torch.Tensor
-            The result tensor of an intrinsic surface convolution.
-            It has a size of: (n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the total amount
-            of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during the intrinsic
-            surface convolution and 'feature_dim' to the feature dimensionality.
+            A tensor of size: (batch_shapes, n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the
+            total amount of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during
+            the intrinsic surface convolution and 'feature_dim' to the feature dimensionality.
 
         Returns
         -------
         torch.Tensor:
-            A two-dimensional tensor of size (n_vertices, feature_dim), that contains a convolution results for each
-            vertex. Thereby, the convolution result has the smallest Euclidean norm among the convolution results for
-            all rotations.
+            A three-dimensional tensor of size (batch_shapes, n_vertices, feature_dim).
         """
-        minimal_response = torch.linalg.vector_norm(inputs, ord=2, dim=-1)
-        minimal_response = torch.argmin(minimal_response, dim=-1).int()
-        pooled_signals = []
-        for signal, indices in zip(inputs, minimal_response):
-            pooled_signals.append(signal[torch.arange(0, inputs.shape[1]), indices])
-        return torch.stack(pooled_signals)
+        return torch.min(inputs, dim=-2)[0]
