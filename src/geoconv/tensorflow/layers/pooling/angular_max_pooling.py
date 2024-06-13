4333c26d9434bd95ec93853fcfb77106d15702c5
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-13 10:01:28 +0200

    Adjusted documentation

--- src/geoconv/tensorflow/layers/pooling/angular_max_pooling.py
+++ src/geoconv/tensorflow/layers/pooling/angular_max_pooling.py
@@ -12,14 +12,13 @@ class AngularMaxPooling(keras.layers.Layer):
         Parameters
         ----------
         inputs: tensorflow.Tensor
-            The result tensor of an intrinsic surface convolution.
-            It has a size of: (batch_shapes, n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the
+            A tensor of size: (batch_shapes, n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the
             total amount of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during
             the intrinsic surface convolution and 'feature_dim' to the feature dimensionality.
 
         Returns
         -------
         tensorflow.Tensor:
-            A two-dimensional tensor of size (batch_shapes, n_vertices, feature_dim).
+            A three-dimensional tensor of size (batch_shapes, n_vertices, feature_dim).
         """
         return tf.reduce_max(inputs, axis=-2)
