6857a8cc9b7d54f26e153a8820a16a9e829a0414
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-06-22 13:08:15 +0200

    Added adapation of normalization layer

--- src/geoconv_examples/modelnet_40/training.py
+++ src/geoconv_examples/modelnet_40/training.py
@@ -58,6 +58,13 @@ def training(dataset_path, logging_dir, template_configurations=None, variant=No
         imcnn.build(
             input_shape=[tf.TensorShape([None, 541, 3]), tf.TensorShape([None, 541, n_radial, n_angular, 3, 2])]
         )
+        print("Adapt normalization layer on training data..")
+        imcnn.backbone.normalize.adapt(
+            load_preprocessed_modelnet(
+                dataset_path, n_radial, n_angular, template_radius, is_train=True, only_signal=True
+            )
+        )
+        print("Done.")
         imcnn.summary()
 
         # Define callbacks
