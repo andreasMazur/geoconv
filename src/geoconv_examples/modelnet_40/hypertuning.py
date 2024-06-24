Fixed normalization bug

src/geoconv_examples/modelnet_40/hypertuning.py
@@ -14,13 +14,21 @@ class HyperModel(kt.HyperModel):
         self.template_radius = template_radius
         self.adapt_data = adapt_data
 
+        self.normalize = tf.keras.layers.Normalization(axis=-1, name="input_normalization")
+        print("Adapt normalization layer on training data..")
+        self.normalize.adapt(adapt_data)
+        print("Done.")
+
     def build(self, hp):
         # Define model input
         signal_input = tf.keras.layers.Input(shape=(3,), name="Signal")
         bc_input = tf.keras.layers.Input(shape=(541, self.n_radial, self.n_angular, 3, 2), name="BC")
 
+        # Normalize input
+        signal = self.normalize(signal_input)
+
         # Define classifier
-        clf = ModelnetClassifier(
+        signal_output = ModelnetClassifier(
             self.n_radial,
             self.n_angular,
             self.template_radius,
@@ -29,10 +37,9 @@ class HyperModel(kt.HyperModel):
                 hp.Int(name="ISC_2", min_value=4, max_value=256),
                 hp.Int(name="ISC_3", min_value=4, max_value=256),
                 hp.Int(name="ISC_4", min_value=4, max_value=256),
-            ]
-        )
-        clf.backbone.normalize.adapt(self.adapt_data)
-        signal_output = clf([signal_input, bc_input])
+            ],
+            normalize=False
+        )([signal, bc_input])
 
         # Compile model
         imcnn = tf.keras.Model(inputs=[signal_input, bc_input], outputs=signal_output)

src/geoconv_examples/modelnet_40/training.py
@@ -9,7 +9,7 @@ import tensorflow_probability as tfp
 
 
 class ModelnetClassifier(keras.Model):
-    def __init__(self, n_radial, n_angular, template_radius, isc_layer_dims=None, variant=None):
+    def __init__(self, n_radial, n_angular, template_radius, isc_layer_dims=None, variant=None, normalize=True):
         super().__init__()
         isc_layer_dims = [128, 64, 8] if isc_layer_dims is None else isc_layer_dims
         self.backbone = ImcnnBackbone(
@@ -17,7 +17,8 @@ class ModelnetClassifier(keras.Model):
             n_radial=n_radial,
             n_angular=n_angular,
             template_radius=template_radius,
-            variant=variant
+            variant=variant,
+            normalize=normalize
         )
         self.flatten = tf.keras.layers.Flatten()
         self.output_layer = tf.keras.layers.Dense(40)