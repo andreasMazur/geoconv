69cbced73f611962a9caf942072a08c549f2d6b5
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-07-26 14:52:05 +0200

    Decreased model size

--- src/geoconv_examples/modelnet_40_projections/hypertuning.py
+++ src/geoconv_examples/modelnet_40_projections/hypertuning.py
@@ -50,9 +50,9 @@ class HyperModel(kt.HyperModel):
         # Embed
         signal = ImcnnBackbone(
             isc_layer_dims=[
-                hp.Int(name="ISC_1", min_value=96, max_value=400),
-                hp.Int(name="ISC_2", min_value=96, max_value=400),
-                hp.Int(name="ISC_3", min_value=96, max_value=400)
+                hp.Int(name="ISC_1", min_value=96, max_value=200),
+                hp.Int(name="ISC_2", min_value=96, max_value=200),
+                hp.Int(name="ISC_3", min_value=96, max_value=200)
             ],
             n_radial=self.n_radial,
             n_angular=self.n_angular,
