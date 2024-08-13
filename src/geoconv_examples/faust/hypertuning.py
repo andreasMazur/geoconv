4dd9ee3859b8dcd2fb536b103e8b26f24d1f2b02
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-08-13 10:08:40 +0200

    Adjusted hypertuning to latest training script

--- src/geoconv_examples/faust/hypertuning.py
+++ src/geoconv_examples/faust/hypertuning.py
@@ -1,5 +1,5 @@
 from geoconv_examples.faust.dataset import load_preprocessed_faust
-from geoconv_examples.faust.training import SIG_DIM, FaustVertexClassifier, reconstruction_loss
+from geoconv_examples.faust.training import SIG_DIM, AMOUNT_VERTICES, FaustVertexClassifier
 
 import tensorflow as tf
 import keras_tuner as kt
@@ -7,75 +7,79 @@ import os
 
 
 class HyperModel(kt.HyperModel):
-    def __init__(self, n_radial, n_angular, template_radius, adapt_data):
+    def __init__(self, n_radial, n_angular, template_radius):
         super().__init__()
         self.n_radial = n_radial
         self.n_angular = n_angular
         self.template_radius = template_radius
 
-        self.normalize = tf.keras.layers.Normalization(axis=-1, name="input_normalization")
-        print("Adapt normalization layer on training data..")
-        self.normalize.adapt(adapt_data)
-        print("Done.")
-
     def build(self, hp):
         # Define model input
-        signal_input = tf.keras.layers.Input(shape=(SIG_DIM,), name="Signal")
-        bc_input = tf.keras.layers.Input(shape=(6890, self.n_radial, self.n_angular, 3, 2), name="BC")
+        signal_input = tf.keras.layers.Input(shape=(AMOUNT_VERTICES, SIG_DIM), name="Signal")
+        bc_input = tf.keras.layers.Input(shape=(AMOUNT_VERTICES, self.n_radial, self.n_angular, 3, 2), name="BC")
 
-        # Normalize input
-        signal = self.normalize(signal_input)
-
-        vertex_predictions, reconstructions = FaustVertexClassifier(
-            self.n_radial,
-            self.n_angular,
+        vertex_predictions = FaustVertexClassifier(
             self.template_radius,
             isc_layer_dims=[
-                hp.Int(name="ISC_1", min_value=201, max_value=300),
-                hp.Int(name="ISC_2", min_value=101, max_value=200),
-                hp.Int(name="ISC_3", min_value=1, max_value=100)
+                hp.Int(name="ISC_1", min_value=50, max_value=400),
+                hp.Int(name="ISC_2", min_value=50, max_value=400),
+                hp.Int(name="ISC_3", min_value=50, max_value=400),
+                hp.Int(name="ISC_4", min_value=50, max_value=400)
             ],
-            normalize=False
-        )([signal, bc_input])
+            variant="dirac"
+        )([signal_input, bc_input])
 
         # Compile model
-        imcnn = tf.keras.Model(inputs=[signal_input, bc_input], outputs=[vertex_predictions, reconstructions])
-        losses = [
-            tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            reconstruction_loss
-        ]
+        imcnn = tf.keras.Model(inputs=[signal_input, bc_input], outputs=vertex_predictions)
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         opt = tf.keras.optimizers.AdamW(
             learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=hp.Float("initial_learning_rate", min_value=0.001, max_value=0.005),
+                initial_learning_rate=hp.Float("initial_learning_rate", min_value=0.01, max_value=0.0001),
                 decay_steps=500,
                 decay_rate=0.99
             ),
             weight_decay=0.005
         )
-        imcnn.compile(optimizer=opt, loss=losses, metrics=["accuracy"])
+        imcnn.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
         imcnn.build(
             input_shape=[
-                tf.TensorShape([None, 6890, 3]), tf.TensorShape([None, 6890, self.n_radial, self.n_angular, 3, 2])
+                tf.TensorShape([None, AMOUNT_VERTICES, 3]),
+                tf.TensorShape([None, AMOUNT_VERTICES, self.n_radial, self.n_angular, 3, 2])
             ]
         )
 
         return imcnn
 
 
-def hyper_tuning(dataset_path, logging_dir, template_configuration):
+def hyper_tuning(dataset_path, logging_dir, template_configuration, gen_info_file=None):
     # Create logging dir
     os.makedirs(logging_dir, exist_ok=True)
 
+    # Set filename for generator
+    if gen_info_file is None:
+        gen_info_file = "generator_info.json"
+
     # Run hyper-tuning
     n_radial, n_angular, template_radius = template_configuration
-    train_data = load_preprocessed_faust(dataset_path, n_radial, n_angular, template_radius, is_train=True)
-    test_data = load_preprocessed_faust(dataset_path, n_radial, n_angular, template_radius, is_train=False)
-    adapt_data = load_preprocessed_faust(
-        dataset_path, n_radial, n_angular, template_radius, is_train=True, only_signal=True
+    train_data = load_preprocessed_faust(
+        dataset_path,
+        n_radial,
+        n_angular,
+        template_radius,
+        is_train=True,
+        gen_info_file=f"{logging_dir}/{gen_info_file}"
+    )
+    test_data = load_preprocessed_faust(
+        dataset_path,
+        n_radial,
+        n_angular,
+        template_radius,
+        is_train=False,
+        gen_info_file=f"{logging_dir}/test_{gen_info_file}"
     )
 
     tuner = kt.Hyperband(
-        hypermodel=HyperModel(n_radial, n_angular, template_radius, adapt_data),
+        hypermodel=HyperModel(n_radial, n_angular, template_radius),
         objective=kt.Objective("val_faust_vertex_classifier_accuracy", direction="max"),
         max_epochs=200,
         factor=3,
