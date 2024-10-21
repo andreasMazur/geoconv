82bb782e28d92228000ceaf9b794c98d849951d3
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-10-21 09:51:46 +0200

    Fixed bug that appeared due to missing dimension

--- src/geoconv_examples/modelnet_40/classifier.py
+++ src/geoconv_examples/modelnet_40/classifier.py
@@ -10,7 +10,7 @@ import tensorflow_probability as tfp
 class ShiftPointCloud(tf.keras.layers.Layer):
     @tf.function(jit_compile=True)
     def call(self, inputs):
-        return inputs - tf.reduce_mean(inputs, axis=1)
+        return inputs - tf.reduce_mean(inputs, axis=1, keepdims=True)
 
 
 class Covariance(tf.keras.layers.Layer):
