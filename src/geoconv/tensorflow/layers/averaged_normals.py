Fixed a bug where nan-values are returned due to averaging over empty neighborhoods

src/geoconv/tensorflow/layers/averaged_normals.py
@@ -16,5 +16,10 @@ class AveragedNormals(tf.keras.layers.Layer):
     @tf.function(jit_compile=True)
     def call_helper(self, vertices):
         lrfs, _, neighborhoods_indices = knn_shot_lrf(self.neighbors_for_lrf, vertices)
-        avg_normal = tf.reduce_mean(tf.gather(lrfs[:, 0, :], neighborhoods_indices[:, :self.neighbors_for_avg]), axis=1)
-        return avg_normal / tf.expand_dims(tf.linalg.norm(avg_normal, axis=-1), axis=-1)
+        if self.neighbors_for_avg > 1:
+            avg_normal = tf.reduce_mean(
+                tf.gather(lrfs[:, 0, :], neighborhoods_indices[:, :self.neighbors_for_avg]), axis=1
+            )
+            return avg_normal / tf.expand_dims(tf.linalg.norm(avg_normal, axis=-1), axis=-1)
+        else:
+            return lrfs[:, 0, :]