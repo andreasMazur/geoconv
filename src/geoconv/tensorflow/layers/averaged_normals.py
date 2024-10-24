fadec1171158b9bc3ce1350b5458718b34b70ebc
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-10-24 10:11:53 +0200

    Refactored layer

--- src/geoconv/tensorflow/layers/averaged_normals.py
+++ src/geoconv/tensorflow/layers/averaged_normals.py
@@ -4,10 +4,9 @@ import tensorflow as tf
 
 
 class AveragedNormals(tf.keras.layers.Layer):
-    def __init__(self, neighbors_for_lrf=128, neighbors_for_avg=128):
+    def __init__(self, neighbors_for_lrf=32):
         super().__init__()
         self.neighbors_for_lrf = neighbors_for_lrf
-        self.neighbors_for_avg = neighbors_for_avg
 
     @tf.function(jit_compile=True)
     def call(self, vertices):
@@ -16,10 +15,4 @@ class AveragedNormals(tf.keras.layers.Layer):
     @tf.function(jit_compile=True)
     def call_helper(self, vertices):
         lrfs, _, neighborhoods_indices = knn_shot_lrf(self.neighbors_for_lrf, vertices)
-        if self.neighbors_for_avg > 1:
-            avg_normal = tf.reduce_mean(
-                tf.gather(lrfs[:, 0, :], neighborhoods_indices[:, :self.neighbors_for_avg]), axis=1
-            )
-            return avg_normal / tf.expand_dims(tf.linalg.norm(avg_normal, axis=-1), axis=-1)
-        else:
-            return lrfs[:, 0, :]
+        return lrfs[:, :, 0]
