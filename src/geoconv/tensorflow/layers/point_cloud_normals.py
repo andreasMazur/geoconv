2b5672ed73a53fe58a31482bdc43715b106c241e
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2024-10-25 17:26:32 +0200

    Fixed bug caused by tfp

--- src/geoconv/tensorflow/layers/point_cloud_normals.py
+++ src/geoconv/tensorflow/layers/point_cloud_normals.py
@@ -1,7 +1,6 @@
 from geoconv.tensorflow.utils.compute_shot_lrf import knn_shot_lrf
 
 import tensorflow as tf
-import tensorflow_probability as tfp
 
 
 class PointCloudNormals(tf.keras.layers.Layer):
@@ -11,12 +10,14 @@ class PointCloudNormals(tf.keras.layers.Layer):
 
     @tf.function(jit_compile=True)
     def call(self, vertices):
+        self.call_helper(vertices[0])
         return tf.map_fn(self.call_helper, vertices)
 
     @tf.function(jit_compile=True)
     def call_helper(self, vertices):
         lrfs, _, neighborhoods_indices = knn_shot_lrf(self.neighbors_for_lrf, vertices)
         normals = lrfs[:, :, 0]
-        covs = tfp.stats.covariance(tf.gather(normals, neighborhoods_indices), sample_axis=1)
+        normals = tf.gather(normals, neighborhoods_indices)
+        covs = tf.einsum("nvi,nvj->nij", normals, normals)
         covs = tf.reshape(covs, (tf.shape(covs)[0], 9))
         return covs / tf.linalg.norm(covs, axis=-1, keepdims=True)
