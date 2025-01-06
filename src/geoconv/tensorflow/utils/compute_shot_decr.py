69f145b3be28c8169bd601c8731faadc43840cb1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-06 13:40:27 +0100

    Added documentation

--- src/geoconv/tensorflow/utils/compute_shot_decr.py
+++ src/geoconv/tensorflow/utils/compute_shot_decr.py
@@ -2,9 +2,26 @@ import tensorflow as tf
 import numpy as np
 
 
-def determine_central_values(start, stop, divisions):
-    # a range of n + 1 values has n bins
-    central_values = tf.linspace(start=start, stop=stop, num=divisions + 1)[:-1]
+@tf.function(jit_compile=True)
+def determine_central_values(start, stop, n_bins):
+    """Determines the central values within the bins.
+
+    Parameters
+    ----------
+    start: float
+        The start x-value of the histogram.
+    stop: float
+        The stop x-value of the histogram.
+    n_bins: int
+        The amount of bins.
+
+    Returns
+    -------
+    (tf.Tensor, float):
+        The central values of the histogram bins and the step size between any two central values.
+    """
+    # A range of n + 1 values has n bins
+    central_values = tf.linspace(start=start, stop=stop, num=n_bins + 1)[:-1]
     histogram_step_size = tf.math.abs(central_values[0] - central_values[1])
     central_values = central_values + histogram_step_size / 2
     return central_values, histogram_step_size
@@ -15,23 +32,61 @@ def shot_descr(neighborhoods,
                normals,
                neighborhood_indices,
                radius,
-               azimuth_divisions=8,
-               elevation_divisions=2,
-               radial_divisions=2,
+               azimuth_bins=8,
+               elevation_bins=2,
+               radial_bins=2,
                histogram_bins=11):
+    """This function computes SHOT-descriptor.
+
+    SHOT-descriptor have been introduced in:
+    > [SHOT: Unique signatures of histograms for surface and texture
+     description.](https://doi.org/10.1016/j.cviu.2014.04.011)
+    > Salti, Samuele, Federico Tombari, and Luigi Di Stefano.
+
+    Parameters
+    ----------
+    neighborhoods: tf.Tensor
+        A rank-3 tensor of shape (n_vertices, n_neighbors, 3) containing the cartesian coordinates of neighbors.
+    normals: tf.Tensor
+        A rank-2 tensor of shape (n_vertices, 3) containing the normals of the vertices.
+    neighborhood_indices: tf.Tensor
+        A rank-2 tensor of shape (n_vertices, n_neighbors) containing the indices of the neighbors.
+    radius: float
+        The radius for the sphere used to compute the SHOT-descriptor.
+    azimuth_bins: int
+        The amount of bins along the azimuth direction.
+    elevation_bins: int
+        The amount of bins along the elevation direction.
+    radial_bins: int
+        The amount of bins along the radial direction.
+    histogram_bins:
+        The amount of bins in the histogram.
+
+    Returns
+    -------
+    tf.Tensor:
+        A rank-2 tensor of shape
+            (n_vertices, azimuth_bins * elevation_bins * radial_bins * histogram_bins)
+        containing the SHOT-descriptor for each vertex.
+    """
+    ########################################################################
+    # Determine into which spherical- and histogram bins the neighbors fall
+    ########################################################################
     # Omit origin
     neighborhoods = neighborhoods[:, 1:, :]
     neighborhood_indices = neighborhood_indices[:, 1:]
 
     # Compute spherical coordinates of vertices in neighborhoods
     v_radial = tf.linalg.norm(neighborhoods, axis=-1)
-    v_elevation = tf.math.acos(neighborhoods[:, :, 2] / v_radial)
+    v_elevation = tf.math.acos(  # machine accuracy sometimes return slightly larger/smaller values than allowed
+        tf.clip_by_value(neighborhoods[:, :, 2] / v_radial, clip_value_min=-1., clip_value_max=1.)
+    )
     v_azimuth = tf.math.atan2(neighborhoods[:, :, 1], neighborhoods[:, :, 0]) + np.pi
 
     # Bin spherical coordinates of vertices into spherical grid
-    radial_histogram = tf.histogram_fixed_width_bins(v_radial, [0., radius], nbins=radial_divisions)
-    elevation_histogram = tf.histogram_fixed_width_bins(v_elevation, [0., np.pi], nbins=elevation_divisions)
-    azimuth_histogram = tf.histogram_fixed_width_bins(v_azimuth, [0., 2 * np.pi], nbins=azimuth_divisions)
+    radial_histogram = tf.histogram_fixed_width_bins(v_radial, [0., radius], nbins=radial_bins)
+    elevation_histogram = tf.histogram_fixed_width_bins(v_elevation, [0., np.pi], nbins=elevation_bins)
+    azimuth_histogram = tf.histogram_fixed_width_bins(v_azimuth, [0., 2 * np.pi], nbins=azimuth_bins)
     binned_vertices = tf.stack([azimuth_histogram, elevation_histogram, radial_histogram], axis=-1)
 
     # Compute inner product of vertex-normals from vertices in same bins with z-axis of lrf
@@ -54,7 +109,7 @@ def shot_descr(neighborhoods,
 
     # Create histogram tensor and fill it by incrementing indexed bins
     histogram = tf.zeros(
-        shape=(tf.shape(neighborhoods)[0], azimuth_divisions, elevation_divisions, radial_divisions, histogram_bins)
+        shape=(tf.shape(neighborhoods)[0], azimuth_bins, elevation_bins, radial_bins, histogram_bins)
     )
 
     ##############################
@@ -68,7 +123,7 @@ def shot_descr(neighborhoods,
     ###############################
     # Histogram bins interpolation
     ###############################
-    central_values, step_size = determine_central_values(start=-1., stop=1., divisions=histogram_bins)
+    central_values, step_size = determine_central_values(start=-1., stop=1., n_bins=histogram_bins)
     d = tf.abs(cosines - tf.gather(central_values, cosine_bins[:, :, histogram_colon])) / step_size
 
     # Increment histogram bins by 1 - d
@@ -88,9 +143,7 @@ def shot_descr(neighborhoods,
     ################################
     # Azimuth volumes interpolation
     ################################
-    central_values, step_size = determine_central_values(
-        start=0., stop=2 * np.pi, divisions=azimuth_divisions
-    )
+    central_values, step_size = determine_central_values(start=0., stop=2 * np.pi, n_bins=azimuth_bins)
     d = tf.abs(v_azimuth - tf.gather(central_values, cosine_bins[:, :, azimuth_colon])) / step_size
 
     # Increment histogram bins by 1 - d
@@ -116,9 +169,7 @@ def shot_descr(neighborhoods,
     ##################################
     # Elevation volumes interpolation
     ##################################
-    central_values, step_size = determine_central_values(
-        start=0., stop=np.pi, divisions=elevation_divisions
-    )
+    central_values, step_size = determine_central_values(start=0., stop=np.pi, n_bins=elevation_bins)
     d = tf.abs(v_elevation - tf.gather(central_values, cosine_bins[:, :, elevation_colon])) / step_size
 
     # Increment histogram bins by 1 - d
@@ -144,9 +195,7 @@ def shot_descr(neighborhoods,
     ###############################
     # Radial volumes interpolation
     ###############################
-    central_values, step_size = determine_central_values(
-        start=0., stop=radius, divisions=radial_divisions
-    )
+    central_values, step_size = determine_central_values(start=0., stop=radius, n_bins=radial_bins)
     d = tf.abs(v_radial - tf.gather(central_values, cosine_bins[:, :, radial_colon])) / step_size
 
     # Increment histogram bins by 1 - d
@@ -169,9 +218,9 @@ def shot_descr(neighborhoods,
         1 - d
     )
 
-    #######################
-    # Form SHOT-descriptor
-    #######################
+    #########################################
+    # Reshape histogram into SHOT-descriptor
+    #########################################
     # Reshape histogram into vector
     histogram = tf.reshape(histogram, (neighborhood_shape[0], -1))
 
