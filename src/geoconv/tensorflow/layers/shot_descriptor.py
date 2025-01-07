bfda79349f260275fa4f825fafccf4ddb04e4c50
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-07 09:01:29 +0100

    Added radius parameter

--- src/geoconv/tensorflow/layers/shot_descriptor.py
+++ src/geoconv/tensorflow/layers/shot_descriptor.py
@@ -5,13 +5,20 @@ import tensorflow as tf
 
 
 class PointCloudShotDescriptor(tf.keras.layers.Layer):
-    def __init__(self, neighbors_for_lrf=16, azimuth_bins=8, elevation_bins=2, radial_bins=2, histogram_bins=11):
+    def __init__(self,
+                 neighbors_for_lrf=16,
+                 azimuth_bins=8,
+                 elevation_bins=2,
+                 radial_bins=2,
+                 histogram_bins=11,
+                 sphere_radius=0.):
         super().__init__()
         self.neighbors_for_lrf = neighbors_for_lrf
         self.azimuth_bins = azimuth_bins
         self.elevation_bins = elevation_bins
         self.radial_bins = radial_bins
         self.histogram_bins = histogram_bins
+        self.sphere_radius = sphere_radius
 
     @tf.function(jit_compile=True)
     def call(self, vertices):
@@ -20,13 +27,25 @@ class PointCloudShotDescriptor(tf.keras.layers.Layer):
     @tf.function(jit_compile=True)
     def call_helper(self, vertices):
         lrfs, neighborhoods, neighborhoods_indices = knn_shot_lrf(self.neighbors_for_lrf, vertices)
-        return shot_descr(
-            neighborhoods=neighborhoods,
-            normals=lrfs[:, :, 0],
-            neighborhood_indices=neighborhoods_indices,
-            radius=tf.reduce_max(neighborhoods),
-            azimuth_bins=self.azimuth_bins,
-            elevation_bins=self.elevation_bins,
-            radial_bins=self.radial_bins,
-            histogram_bins=self.histogram_bins
-        )
+        if self.sphere_radius > 0.:
+            return shot_descr(
+                neighborhoods=neighborhoods,
+                normals=lrfs[:, :, 0],
+                neighborhood_indices=neighborhoods_indices,
+                radius=self.sphere_radius,
+                azimuth_bins=self.azimuth_bins,
+                elevation_bins=self.elevation_bins,
+                radial_bins=self.radial_bins,
+                histogram_bins=self.histogram_bins
+            )
+        else:
+            return shot_descr(
+                neighborhoods=neighborhoods,
+                normals=lrfs[:, :, 0],
+                neighborhood_indices=neighborhoods_indices,
+                radius=tf.reduce_max(neighborhoods),
+                azimuth_bins=self.azimuth_bins,
+                elevation_bins=self.elevation_bins,
+                radial_bins=self.radial_bins,
+                histogram_bins=self.histogram_bins
+            )
