619e2f5007582a79af7cf72d560dbc4b79fbbe95
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-10 09:48:53 +0100

    Added spatial dropout

---
+++ src/geoconv/tensorflow/layers/spatial_dropout.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+class SpatialDropout(tf.keras.layers.Layer):
+
+    def __init__(self, rate):
+        super().__init__()
+        self.dropout_rate = rate
+
+    # @tf.function(jit_compile=True)
+    def call(self, inputs):
+        """Mask out entire feature maps"""
+        input_shape = tf.shape(inputs)
+        mask = tf.expand_dims(
+            tf.random.categorical(
+                tf.tile(tf.math.log([[self.dropout_rate, 1 - self.dropout_rate]]), (input_shape[0], 1)),
+                input_shape[-1]
+            ), axis=1
+        )
+        return tf.cast(mask, tf.float32) * inputs
