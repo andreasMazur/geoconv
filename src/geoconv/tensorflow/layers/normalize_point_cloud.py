c989cada696d214685594f31ee5684618a3673b4
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-20 16:59:20 +0100

    Fixed bug that occurred due to missing dimension

--- src/geoconv/tensorflow/layers/normalize_point_cloud.py
+++ src/geoconv/tensorflow/layers/normalize_point_cloud.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 class NormalizePointCloud(tf.keras.layers.Layer):
     def call(self, inputs):
         # Move point-cloud into origin
-        inputs = inputs - tf.reduce_mean(inputs, axis=1)
+        inputs = inputs - tf.reduce_mean(inputs, axis=1, keepdims=True)
 
         # Scale maximal axis-variance of point-cloud to one. Scale other axes accordingly.
         return inputs / tf.reduce_max(tf.math.reduce_std(inputs, axis=1))
