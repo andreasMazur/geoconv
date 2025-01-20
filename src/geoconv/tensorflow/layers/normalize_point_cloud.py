63d1cc9ce49c4a1b8afdb396a47b2d618ca643f9
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-20 16:34:48 +0100

    Added point-cloud normalization layer

---
+++ src/geoconv/tensorflow/layers/normalize_point_cloud.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+class NormalizePointCloud(tf.keras.layers.Layer):
+    def call(self, inputs):
+        # Move point-cloud into origin
+        inputs = inputs - tf.reduce_mean(inputs, axis=1)
+
+        # Scale maximal axis-variance of point-cloud to one. Scale other axes accordingly.
+        return inputs / tf.reduce_max(tf.math.reduce_std(inputs, axis=1))
