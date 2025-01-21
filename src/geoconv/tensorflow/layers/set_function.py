172d182f41e07dc7710277b7d0e519fe818c07cf
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-21 11:27:15 +0100

    Added permutation-invariant set function layer

---
+++ src/geoconv/tensorflow/layers/set_function.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+class SetFunction(tf.keras.layers.Layer):
+    """A layer that implements the permutation-invariant function from the Deep Sets paper
+
+    Compare:
+    > [Deep Sets](https://arxiv.org/abs/1703.06114)
+    > Manzil Zaheer et al.
+    """
+    def __init__(self, phi_units, rho_units, phi_activation="relu", rho_activation="linear"):
+        super().__init__()
+        self.phi = tf.keras.layers.Dense(phi_units, activation=phi_activation)
+        self.rho = tf.keras.layers.Dense(rho_units, activation=rho_activation)
+
+    @tf.function(jit_compile=True)
+    def call(self, inputs):
+        return self.rho(tf.reduce_sum(self.phi(inputs), axis=-2))
