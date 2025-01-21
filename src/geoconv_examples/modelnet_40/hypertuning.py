bc645c4cc1fd505a777bea14a0cce83f84f1853a
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-21 13:46:21 +0100

    Fixed naming of hp

--- src/geoconv_examples/modelnet_40/hypertuning.py
+++ src/geoconv_examples/modelnet_40/hypertuning.py
@@ -49,7 +49,7 @@ def hyper_tuning(dataset_path,
         lr = 0.0030670247193529277
         opt = tf.keras.optimizers.AdamW(
             learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=hp.Float("beta_1", min_value=lr * 0.66, max_value=lr * 1.33),
+                initial_learning_rate=hp.Float("init_learning_rate", min_value=lr * 0.66, max_value=lr * 1.33),
                 decay_steps=1809,
                 decay_rate=0.8003162152855945,
                 staircase=False
