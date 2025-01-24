e591c2dcad6f7225fdcf7f9af5f14bbb22b08eca
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-01-24 13:45:34 +0100

    Changed HP search space

--- src/geoconv_examples/modelnet_40/hypertuning.py
+++ src/geoconv_examples/modelnet_40/hypertuning.py
@@ -33,7 +33,7 @@ def hyper_tuning(dataset_path,
         # Configure classifier
         imcnn = ModelNetClf(
             neighbors_for_lrf=neighbors_for_lrf,
-            projection_neighbors=hp.Int("projection_neighbors", min_value=8, max_value=16),
+            projection_neighbors=projection_neighbors,
             n_radial=n_radial,
             n_angular=n_angular,
             template_radius=template_radius,
@@ -47,20 +47,20 @@ def hyper_tuning(dataset_path,
             histogram_bins=histogram_bins,
             sphere_radius=sphere_radius,
             dropout_rate=0.,
-            exp_lambda=hp.Float("exp_lambda", min_value=1.0, max_value=4.0),
+            exp_lambda=exp_lambda,
             shift_angular=shift_angular
         )
 
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="sum_over_batch_size")
         lr = 0.0030670247193529277
         opt = tf.keras.optimizers.AdamW(
-            # learning_rate=hp.Float("init_learning_rate", min_value=10e-6, max_value=lr * 1.5),
-            learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=lr,  # hp.Float("init_learning_rate", min_value=lr * 1/3, max_value=lr * 5/3),
-                decay_steps=1809,
-                decay_rate=0.8003162152855945,
-                staircase=False
-            ),
+            learning_rate=hp.Float("init_learning_rate", min_value=10e-6, max_value=lr * 1.25),
+            # learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(
+            #     initial_learning_rate=lr,  # hp.Float("init_learning_rate", min_value=lr * 1/3, max_value=lr * 5/3),
+            #     decay_steps=1809,
+            #     decay_rate=0.8003162152855945,
+            #     staircase=False
+            # ),
             weight_decay=0.019081993138727875,
             beta_1=0.9,
             beta_2=0.999
