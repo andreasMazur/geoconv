c6adeef04e3cb1bf94fdb868a6bc83a4ad077a3b
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-02-06 17:06:56 +0100

    Removed redundant part

--- src/geoconv/tensorflow/layers/barycentric_coordinates.py
+++ src/geoconv/tensorflow/layers/barycentric_coordinates.py
@@ -186,14 +186,6 @@ def compute_bc(template, projections):
     projections = tf.cast(projections, tf.float64)
     interpolation_weights, interpolation_indices = compute_interpolation_weights(template, projections)
 
-    # Replace 'nan'-interpolation coefficients with zeros to prevent any contribution of this template vertex
-    to_filter = tf.where(
-        tf.logical_or(tf.math.is_nan(interpolation_weights), tf.math.is_inf(interpolation_weights))
-    )[:, :3]
-    zeros = tf.cast(tf.tile([[0., 0., 0.]], multiples=[tf.shape(to_filter)[0], 1]), tf.float64)
-    interpolation_weights = tf.tensor_scatter_nd_update(interpolation_weights, to_filter, zeros)
-    interpolation_indices = tf.tensor_scatter_nd_update(interpolation_indices, to_filter, tf.cast(zeros, tf.int32))
-
     return interpolation_weights, interpolation_indices
 
 
