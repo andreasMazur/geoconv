171523e47d553118d6cda83490bbd7db104094b9
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-02-24 09:45:29 +0100

    Adjusted init lr and lr decay rate

--- src/geoconv_examples/modelnet_40/training.py
+++ src/geoconv_examples/modelnet_40/training.py
@@ -43,9 +43,9 @@ def model_configuration(neighbors_for_lrf,
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="sum_over_batch_size")
     opt = tf.keras.optimizers.AdamW(
         learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate=0.0015218449319544082,
+            initial_learning_rate=0.0007673139778927575,
             decay_steps=2461,
-            decay_rate=0.76942,
+            decay_rate=0.7080237438158256,
             staircase=False
         ),
         weight_decay=0.019081993138727875
