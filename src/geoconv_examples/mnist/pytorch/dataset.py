dba0a93a0113973db5f406a57e4a6256cfd770d8
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-04-01 16:04:58 +0200

    Added sclicing option to PyTorch-MNIST-dataloader

--- src/geoconv_examples/mnist/pytorch/dataset.py
+++ src/geoconv_examples/mnist/pytorch/dataset.py
@@ -1,9 +1,34 @@
 from geoconv.utils.data_generator import barycentric_coordinates_generator
 
 from torchvision import datasets
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 
+import torch
 import numpy as np
+
+
+class ProcessedMNIST(Dataset):
+    """A dataset containing formatted MNIST-images and labels together with barycentric coordinates.
+
+    Attributes
+    ----------
+    images: torch.Tensor
+        The images of MNIST, formatted into vectors.
+    bc: torch.Tensor
+        One set of barycentric coordinates that can be used for any images of MNIST.
+    labels:
+        The labels for the MNIST-images.
+    """
+    def __init__(self, images, bc, labels):
+        self.images = images.reshape(images.shape[0], -1, 1) / 255.
+        self.bc = torch.from_numpy(bc)
+        self.labels = labels
+
+    def __len__(self):
+        return self.images.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.images[idx], self.bc), self.labels[idx]
 
 
 def load_preprocessed_mnist(dataset_path,
@@ -12,7 +37,8 @@ def load_preprocessed_mnist(dataset_path,
                             template_radius,
                             set_type,
                             batch_size=8,
-                            mnist_folder=None):
+                            mnist_folder=None,
+                            indices=None):
     """Loads MNIST while adding barycentric coordinates to the dataset and reshapes images to vectors.
 
     Parameters
@@ -31,6 +57,8 @@ def load_preprocessed_mnist(dataset_path,
         The batch-size.
     mnist_folder: str | None
         The path to the folder containing the MNIST dataset. If not stored there, the dataset will be downloaded.
+    indices: np.ndarray | None
+        The indices if elements of the dataset to load.
 
     Returns
     -------
@@ -47,12 +75,11 @@ def load_preprocessed_mnist(dataset_path,
 
     # Add barycentric coordinates to MNIST data
     for bc in barycentric_coordinates:
-        def make_compatible(image):
-            image = np.array(image).reshape((-1, 1))
-            return image / 255., bc
-        dataloader = DataLoader(
-            datasets.MNIST(mnist_folder, train=set_type == "train", download=True, transform=make_compatible),
-            batch_size=batch_size,
-            shuffle=True
-        )
-        return dataloader
+
+        dataset = datasets.MNIST(mnist_folder, train=set_type == "train", download=True)
+        if indices is None:
+            dataset = ProcessedMNIST(images=dataset.data, bc=bc, labels=dataset.targets)
+        else:
+            dataset = ProcessedMNIST(images=dataset.data[indices], bc=bc, labels=dataset.targets[indices])
+
+        return DataLoader(dataset, batch_size=batch_size, shuffle=True)
