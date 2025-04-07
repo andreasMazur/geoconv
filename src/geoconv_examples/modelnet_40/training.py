a3f2174f62983a7333d4bc16547fde7a9a805e60
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-04-07 10:39:20 +0200

    Simplified training script and added experiment repetitions

--- src/geoconv_examples/modelnet_40/training.py
+++ src/geoconv_examples/modelnet_40/training.py
@@ -65,95 +65,71 @@ def training(dataset_path,
              neighbors_for_lrf=16,
              projection_neighbors=10,
              template_radius=None,
-             modelnet10=False,
-             gen_info_file=None,
              batch_size=1,
              variant=None,
-             set_mem_growth=False,
-             redirect_output=False,
              rotation_delta=1,
-             epochs=200,
-             debug=False,
              exp_lambda=2.0,
              shift_angular=True,
-             isc_layer_conf=None,
              pooling="avg"):
-    if isc_layer_conf is None:
-        isc_layer_conf = [128, 128, 128, 64]
-
     # Create logging dir
     os.makedirs(logging_dir, exist_ok=True)
 
-    # Redirect output (stdout/stderr)
-    if redirect_output:
-        sys.stdout = open(f"{logging_dir}/stdout.txt", "a")
-        sys.stderr = open(f"{logging_dir}/stderr.txt", "a")
+    for repetition in range(10):
+        logging_dir = f"{logging_dir}_trial_{repetition}"
+        for (n_radial, n_angular, template_scale) in template_configurations:
+            # Get classification model
+            imcnn = model_configuration(
+                neighbors_for_lrf=neighbors_for_lrf,
+                projection_neighbors=projection_neighbors,
+                n_radial=n_radial,
+                n_angular=n_angular,
+                template_radius=template_radius * template_scale,
+                modelnet10=True,
+                variant=variant,
+                rotation_delta=rotation_delta,
+                exp_lambda=exp_lambda,
+                shift_angular=shift_angular,
+                isc_layer_conf=[128, 128],
+                pooling=pooling
+            )
 
-    # Set memory growth, e.g., for training multiple models on one GPU
-    if set_mem_growth:
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        assert len(gpus) > 0, "No GPUs found!"
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+            # Define callbacks
+            exp_number = f"{n_radial}_{n_angular}_{template_scale}"
+            csv_file_name = f"{logging_dir}/training_{exp_number}.log"
+            csv = tf.keras.callbacks.CSVLogger(csv_file_name)
+            stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, min_delta=0.01)
+            tb = tf.keras.callbacks.TensorBoard(
+                log_dir=f"{logging_dir}/tensorboard_{exp_number}",
+                histogram_freq=1,
+                write_graph=False,
+                write_steps_per_second=True,
+                update_freq="epoch",
+                profile_batch=(1, 200)
+            )
 
-    # Set filename for generator
-    if gen_info_file is None:
-        gen_info_file = "generator_info.json"
+            # Load data
+            train_data = load_preprocessed_modelnet(
+                dataset_path,
+                set_type="train",
+                modelnet10=True,
+                gen_info_file=f"{logging_dir}/generator_info.json",
+                batch_size=batch_size,
+                debug_data=False
+            )
+            test_data = load_preprocessed_modelnet(
+                dataset_path,
+                set_type="test",
+                modelnet10=True,
+                gen_info_file=f"{logging_dir}/test_generator_info.json",
+                batch_size=batch_size,
+                debug_data=False
+            )
+            save = tf.keras.callbacks.ModelCheckpoint(
+                filepath=f"{logging_dir}/saved_imcnn_{exp_number}.keras",
+                monitor="val_accuracy",
+                save_best_only=True,
+                save_freq="epoch"
+            )
 
-    for (n_radial, n_angular, template_scale) in template_configurations:
-        # Get classification model
-        imcnn = model_configuration(
-            neighbors_for_lrf=neighbors_for_lrf,
-            projection_neighbors=projection_neighbors,
-            n_radial=n_radial,
-            n_angular=n_angular,
-            template_radius=template_radius * template_scale,
-            modelnet10=modelnet10,
-            variant=variant,
-            rotation_delta=rotation_delta,
-            exp_lambda=exp_lambda,
-            shift_angular=shift_angular,
-            isc_layer_conf=isc_layer_conf,
-            pooling=pooling
-        )
-
-        # Define callbacks
-        exp_number = f"{n_radial}_{n_angular}_{template_scale}"
-        csv_file_name = f"{logging_dir}/training_{exp_number}.log"
-        csv = tf.keras.callbacks.CSVLogger(csv_file_name)
-        stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, min_delta=0.01)
-        tb = tf.keras.callbacks.TensorBoard(
-            log_dir=f"{logging_dir}/tensorboard_{exp_number}",
-            histogram_freq=1,
-            write_graph=False,
-            write_steps_per_second=True,
-            update_freq="epoch",
-            profile_batch=(1, 200)
-        )
-
-        # Load data
-        train_data = load_preprocessed_modelnet(
-            dataset_path,
-            set_type="train",
-            modelnet10=modelnet10,
-            gen_info_file=f"{logging_dir}/{gen_info_file}",
-            batch_size=batch_size,
-            debug_data=debug
-        )
-        test_data = load_preprocessed_modelnet(
-            dataset_path,
-            set_type="test",
-            modelnet10=modelnet10,
-            gen_info_file=f"{logging_dir}/test_{gen_info_file}",
-            batch_size=batch_size,
-            debug_data=debug
-        )
-        save = tf.keras.callbacks.ModelCheckpoint(
-            filepath=f"{logging_dir}/saved_imcnn_{exp_number}.keras",
-            monitor="val_accuracy",
-            save_best_only=True,
-            save_freq="epoch"
-        )
-
-        # Train model
-        imcnn.fit(x=train_data, callbacks=[stop, tb, csv, save], validation_data=test_data, epochs=epochs)
+            # Train model
+            imcnn.fit(x=train_data, callbacks=[stop, tb, csv, save], validation_data=test_data, epochs=200)
