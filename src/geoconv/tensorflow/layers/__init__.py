d6ab36f5ab561c598f6cae777231bfa69a72a27c
David Leins <dleins@techfak.uni-bielefeld.de>
2025-04-08 15:35:49 +0200

    fixes torch.layers import order

--- src/geoconv/tensorflow/layers/__init__.py
+++ src/geoconv/tensorflow/layers/__init__.py
@@ -5,11 +5,13 @@ from .conv_exp import ConvExp
 from .conv_geodesic import ConvGeodesic
 from .conv_student_t import ConvStudentT
 from .conv_zero import ConvZero
-from .barycentric_coordinates import BarycentricCoordinates
-
-from .pooling import AngularAvgPooling, AngularMaxPooling, AngularMinPooling, GravityPooling
 
 from .normalize_point_cloud import NormalizePointCloud
+
+from .barycentric_coordinates import BarycentricCoordinates
+
+from .pooling import AngularAvgPooling, AngularMaxPooling, AngularMinPooling
+
 
 from .set_function import SetFunction
 from .shot_descriptor import PointCloudShotDescriptor
