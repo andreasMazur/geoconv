ae2ac35f1fa80fb5eabf0a87e6f0d35f4e913614
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-06-03 09:41:59 +0200

    Fixed bug caused by float-conversion

--- src/geoconv_examples/modelnet_40/classifier.py
+++ src/geoconv_examples/modelnet_40/classifier.py
@@ -236,13 +236,11 @@ class ModelNetClf(tf.keras.Model):
     def adapt(self, template_radius=None, adapt_data=None):
         # Adapt the BC-layer
         if adapt_data is not None:
-            self.template_radius = float(
-                self.bc_layer.adapt(
-                    data=adapt_data,
-                    template_scale=self.template_scale,
-                    exp_lambda=self.exp_lambda,
-                    shift_angular=self.shift_angular
-                ).numpy()
+            self.template_radius = self.bc_layer.adapt(
+                data=adapt_data,
+                template_scale=self.template_scale,
+                exp_lambda=self.exp_lambda,
+                shift_angular=self.shift_angular
             )
         elif template_radius is not None:
             self.template_radius = self.bc_layer.adapt(template_radius=template_radius)
