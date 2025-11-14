fd388c6bfd79c2eb1b6a90c29004127e776379bf
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-11-14 14:14:42 +0100

    Added center conv

--- src/geoconv/tensorflow/layers/conv_gauge_equiv.py
+++ src/geoconv/tensorflow/layers/conv_gauge_equiv.py
@@ -4,6 +4,37 @@ import tensorflow as tf
 
 
 @tf.function
+def compute_amplitude_and_normalize(A):
+    """Computes the amplitude complex numbers stored in a tensor and normalizes those.
+
+    This function assumes that the innermost dimension contains stacked 2-dimensional vectors, each of which contains
+    one entry for the real- and one entry for the imaginary part of a complex number. E.g:
+
+    A = [
+        [1, 2, 3, 4], => Imaginary numbers: 1 + 2i AND 3 + 4i
+        [3, 4, 1, 2], => Imaginary numbers: 3 + 4i AND 1 + 2i
+        [5, 6, 10, 11], => Imaginary numbers: 5 + 6i AND 10 + 11i
+    ] whereby 'A' with 'shape(A) = (a = 3, b = 4)' thus contains a * (b / 2) complex numbers.
+
+    Parameters
+    ----------
+    A: tf.Tensor
+        The tensor that contains stacked complex numbers in its last dimension.
+
+    Returns
+    -------
+    (tf.Tensor, tf:tensor):
+        Two tensors: (i) a tensor containing complex numbers with amplitude 1 in its last two dimensions and (ii) a
+        tensor containing the original amplitudes of the complex numbers in its last dimension.
+    """
+    A_shape = tf.shape(A)
+    A = tf.reshape(A, tf.concat([A_shape[:-1], [-1], [2]], axis=-1))
+    A_amplitudes = tf.linalg.norm(A, ord=2, axis=-1)
+    A = tf.math.divide_no_nan(A, A_amplitudes[..., None])
+    return A, A_amplitudes
+
+
+@tf.function
 def complex_multiplication(A, B):
     """Computes the complex multiplication between the innermost dimension of two tensors.
 
@@ -57,6 +88,8 @@ class ConvGaugeEquiv(ConvBase):
         # Set in build
         self.feature_dim = None
         self.all_angular_coordinates = None
+        self._amplitude_weights_center = None
+        self._phase_b_center = None
         self._amplitude_weights = None
         self._phase_m = None
         self._phase_b = None
@@ -70,6 +103,19 @@ class ConvGaugeEquiv(ConvBase):
         # Require template vertices from super().build()
         self.all_angular_coordinates = tf.cast(self.template_vertices[0, :, 1], tf.float32)
 
+        ### Center weights ###
+        self._amplitude_weights_center = self.add_weight(
+            name="center_radial_weights",
+            shape=(self.n_complex_numbers, int(self.feature_dim / 2)),
+            trainable=True,
+        )
+        self._phase_b_center = self.add_weight(
+            name="phase_b",
+            shape=(self.n_complex_numbers,),
+            trainable=True
+        )
+
+        ### Neighbor weights ###
         # Initialize amplitude weights
         self._amplitude_weights = self.add_weight(
             name="radial_weights",
@@ -97,6 +143,33 @@ class ConvGaugeEquiv(ConvBase):
         # input_rotation_orders : (batch, input_dim / 2)
         signals, bc, angles, input_rotation_orders = inputs
 
+        # center_result : (batch, n_vertices, output_dim / 2, 2)
+        center_result = self.conv_center(signals)
+
+        # neighbor_result : (batch, n_vertices, output_dim / 2, 2)
+        neighbor_result = self.conv_neighbor(signals, bc, angles, input_rotation_orders)
+
+        ### Add results ###
+        result = center_result + neighbor_result
+        result_shape = tf.shape(result)
+        result = tf.reshape(result, (result_shape[0], result_shape[1], self.output_dim))
+
+        return result, tf.tile(self._phase_m[None, ...], multiples=[result_shape[0], 1])
+
+    @tf.function
+    def conv_center(self, signals):
+        # Create phase weight tensor (bias only, assume 'theta = 0' in origin
+        P_w_center = self.create_phase_weight_tensor_center()
+
+        # Get normalized signals and their amplitudes
+        signals, signal_amplitudes = compute_amplitude_and_normalize(signals)
+        center_conv = tf.einsum(
+            "qf,bkf,qij,bkfj->bkqi", self._amplitude_weights_center, signal_amplitudes, P_w_center, signals
+        )
+        return center_conv
+
+    @tf.function
+    def conv_neighbor(self, signals, bc, angles, input_rotation_orders):
         # Gather the signals
         # signals   : (batch, n_vertices, n_radial, n_angular, 3, input_dim)
         # bc_values : (batch, n_vertices, n_radial, n_angular, 3)
@@ -117,20 +190,24 @@ class ConvGaugeEquiv(ConvBase):
         # Compute amplitudes
         # signals          : (batch, n_vertices, n_radial, n_angular, input_dim / 2, 2)
         # signal_amplitudes: (batch, n_vertices, n_radial, n_angular, input_dim / 2)
-        signals = tf.reshape(signals, tf.concat([tf.shape(signals)[:4], [-1], [2]], axis=-1))
-        signal_amplitudes = tf.linalg.norm(signals, ord=2, axis=-1)
-        signals = tf.math.divide_no_nan(signals, signal_amplitudes[..., None])
+        signals, signal_amplitudes = compute_amplitude_and_normalize(signals)
 
         # Create phase weight tensor
         P_w = self.create_phase_weight_tensor()
 
         # Compute convolution
-        result = tf.einsum(
+        neighbor_conv = tf.einsum(
             "qrf,bkraf,qaij,bkrafj->bkqi", self._amplitude_weights, signal_amplitudes, P_w, signals
         )
-        result_shape = tf.shape(result)
-        result = tf.reshape(result, (result_shape[0], result_shape[1], self.output_dim))
-        return result, tf.tile(self._phase_m[None, ...], multiples=[result_shape[0], 1])
+        return neighbor_conv
+
+    @tf.function
+    def create_phase_weight_tensor_center(self):
+        cos_matrix = tf.cos(self._phase_b_center)
+        sin_matrix = tf.sin(self._phase_b_center)
+        I = tf.constant([[1., 0.], [0., 1.]])
+        K = tf.constant([[0., -1.], [1., 0.]])
+        return I * cos_matrix[..., None, None] + K * sin_matrix[..., None, None]
 
     @tf.function
     def create_phase_weight_tensor(self):
