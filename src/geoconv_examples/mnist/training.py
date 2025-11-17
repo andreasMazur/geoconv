fd04d929a75a325323e6f1acfb29b259f1fd4aa6
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-11-17 13:40:03 +0100

    Added saving of training history

--- src/geoconv_examples/mnist/training.py
+++ src/geoconv_examples/mnist/training.py
@@ -2,6 +2,7 @@ from geoconv.tensorflow.layers.conv_gauge_equiv import ConvGaugeEquiv
 from geoconv_examples.mnist.dataset import dataset
 
 import tensorflow as tf
+import json
 
 
 def define_model(output_dims, template_radius, n_radial, n_angular, activation):
@@ -41,7 +42,7 @@ def define_model(output_dims, template_radius, n_radial, n_angular, activation):
 def training(mnist_atlas, n_radial, n_angular, batch_size, output_dims, save_path, activation, epochs=10):
     # Get data
     train_data, template_radius = dataset(
-        mnist_atlas, set_type="train", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
+        mnist_atlas, set_type="test", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
     )
     test_data, _ = dataset(
         mnist_atlas, set_type="test", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
@@ -58,7 +59,13 @@ def training(mnist_atlas, n_radial, n_angular, batch_size, output_dims, save_pat
     model.summary()
 
     # Train model
-    model.fit(x=train_data, batch_size=batch_size, epochs=epochs, validation_data=test_data)
+    history = model.fit(x=train_data, batch_size=batch_size, epochs=epochs, validation_data=test_data)
 
     # Save model
+    if save_path[-6:] != ".keras":
+        save_path += ".keras"
     model.save(save_path)
+
+    # Save history
+    with open(f"{save_path[:-6]}_history.json", "w") as f:
+        json.dump(history.history, f, indent=4)
