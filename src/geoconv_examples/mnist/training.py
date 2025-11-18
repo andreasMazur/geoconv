eb16e78addb7ffef7f4c3fbfc896267f870e98b4
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-11-18 10:36:16 +0100

    Moved hypertuning to training file

--- src/geoconv_examples/mnist/training.py
+++ src/geoconv_examples/mnist/training.py
@@ -3,6 +3,7 @@ from geoconv.tensorflow.layers.pooling.global_complex_max_pooling import GlobalC
 from geoconv_examples.mnist.dataset import dataset
 
 import tensorflow as tf
+import keras_tuner as kt
 import json
 
 
@@ -38,6 +39,47 @@ def define_model(output_dims, template_radius, n_radial, n_angular, activation, 
         metrics=["sparse_categorical_accuracy"]
     )
     return imcnn
+
+
+def hypertuning(mnist_atlas, n_radial, n_angular, batch_size, activation, save_path, epochs=5):
+    # Get data
+    train_data, template_radius = dataset(
+        mnist_atlas, set_type="train", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
+    )
+    test_data, _ = dataset(
+        mnist_atlas, set_type="test", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
+    )
+
+    # Define hypermodel function
+    def get_hypermodel(hp):
+        model = define_model(
+            output_dims=[
+                hp.Int("units", min_value=8, max_value=64) for _ in range(hp.Int("n_layers", min_value=4, max_value=16))
+            ],
+            template_radius=template_radius,
+            n_radial=n_radial,
+            n_angular=n_angular,
+            activation=activation,
+            learning_rate=hp.Float("learning_rate", min_value=1e-6, max_value=0.1),
+        )
+        model.summary()
+        return model
+
+    # Hyperparameter search
+    tuner = kt.BayesianOptimization(
+        hypermodel=get_hypermodel,
+        objective=kt.Objective("val_sparse_categorical_accuracy", direction="max"),
+        max_trials=10_000,
+        num_initial_points=10,
+        seed=42
+    )
+    tuner.search(train_data, epochs=epochs, validation_data=test_data)
+
+    # Save best model
+    best_model = tuner.get_best_models()[0]
+    if save_path[-6:] != ".keras":
+        save_path += ".keras"
+    best_model.save(save_path)
 
 
 def training(mnist_atlas, n_radial, n_angular, batch_size, output_dims, save_path, activation, epochs=10):
