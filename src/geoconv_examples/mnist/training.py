37f5beecca7b78364b7d9c62b0fd5b2b9edd026e
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-01-16 15:25:49 +0100

    Adjusted network architecture

--- src/geoconv_examples/mnist/training.py
+++ src/geoconv_examples/mnist/training.py
@@ -1,5 +1,6 @@
-from geoconv.tensorflow.layers.conv_gauge_equiv import ConvGaugeEquiv
+from geoconv.tensorflow.layers.conv_harmonic import ConvHarmonic
 from geoconv.tensorflow.layers.pooling.global_complex_max_pooling import GlobalComplexPooling
+from geoconv.tensorflow.layers.activation_beta_relu import BetaRelu
 from geoconv_examples.mnist.dataset import dataset
 
 import tensorflow as tf
@@ -14,24 +15,24 @@ def define_model(output_dims, template_radius, n_radial, n_angular, activation, 
     image_input = tf.keras.Input(shape=(image_size, 2), name="image_input", dtype=tf.float32)
     bc_input = tf.keras.Input(shape=(image_size, n_radial, n_angular, 3, 2), name="bc_input", dtype=tf.float32)
     rotations_input = tf.keras.Input(shape=(image_size, image_size), name="rotations_input", dtype=tf.float32)
-    rotation_orders_input = tf.keras.Input(shape=(1,), name="orders_input", dtype=tf.float32)
 
     # Initialize variables for forward pass
     signal = image_input
-    rotation_orders = rotation_orders_input
 
     # Forward pass
     for od in output_dims:
-        signal, rotation_orders = ConvGaugeEquiv(
+        signal = ConvHarmonic(
             output_dim=od,
             template_radius=template_radius,
-            activation=activation
-        )([signal, bc_input, rotations_input, rotation_orders])
+            activation=activation,
+            rotation_order=1
+        )([signal, bc_input, rotations_input])
+        signal = BetaRelu()(signal)
     signal = GlobalComplexPooling()(signal)
     output = tf.keras.layers.Dense(10, activation="linear")(signal)
 
     imcnn = tf.keras.Model(
-        inputs=[image_input, bc_input, rotations_input, rotation_orders_input], outputs=output, name="mnist_model"
+        inputs=[image_input, bc_input, rotations_input], outputs=output, name="mnist_model"
     )
     imcnn.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
