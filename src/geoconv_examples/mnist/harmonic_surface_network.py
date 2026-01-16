a8f0ff6e5eaec02fa3c2422dd8ef551b5008df34
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-01-16 16:02:46 +0100

    Moved model definition outside of training function

---
+++ src/geoconv_examples/mnist/harmonic_surface_network.py
@@ -0,0 +1,39 @@
+from geoconv.tensorflow.layers.activation_beta_relu import BetaRelu
+from geoconv.tensorflow.layers.conv_harmonic import ConvHarmonic
+from geoconv.tensorflow.layers.pooling.global_complex_max_pooling import GlobalComplexPooling
+
+import tensorflow as tf
+
+
+def define_model(output_dims, template_radius, n_radial, n_angular, learning_rate=0.001):
+    image_size = 28 * 28
+
+    # Define input layers
+    image_input = tf.keras.Input(shape=(image_size, 2), name="image_input", dtype=tf.float32)
+    bc_input = tf.keras.Input(shape=(image_size, n_radial, n_angular, 3, 2), name="bc_input", dtype=tf.float32)
+    rotations_input = tf.keras.Input(shape=(image_size, image_size), name="rotations_input", dtype=tf.float32)
+
+    # Initialize variables for forward pass
+    signal = image_input
+
+    # Forward pass
+    for od in output_dims:
+        signal = ConvHarmonic(
+            output_dim=od,
+            template_radius=template_radius,
+            activation="linear",
+            rotation_order=1
+        )([signal, bc_input, rotations_input])
+        signal = BetaRelu()(signal)
+    signal = GlobalComplexPooling()(signal)
+    output = tf.keras.layers.Dense(10, activation="linear")(signal)
+
+    imcnn = tf.keras.Model(
+        inputs=[image_input, bc_input, rotations_input], outputs=output, name="mnist_model"
+    )
+    imcnn.compile(
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        metrics=["sparse_categorical_accuracy"]
+    )
+    return imcnn
--- src/geoconv_examples/mnist/training.py
+++ src/geoconv_examples/mnist/training.py
@@ -1,45 +1,7 @@
-from geoconv.tensorflow.layers.conv_harmonic import ConvHarmonic
-from geoconv.tensorflow.layers.pooling.global_complex_max_pooling import GlobalComplexPooling
-from geoconv.tensorflow.layers.activation_beta_relu import BetaRelu
 from geoconv_examples.mnist.dataset import dataset
 
-import tensorflow as tf
 import keras_tuner as kt
 import json
-
-
-def define_model(output_dims, template_radius, n_radial, n_angular, learning_rate=0.001):
-    image_size = 28 * 28
-
-    # Define input layers
-    image_input = tf.keras.Input(shape=(image_size, 2), name="image_input", dtype=tf.float32)
-    bc_input = tf.keras.Input(shape=(image_size, n_radial, n_angular, 3, 2), name="bc_input", dtype=tf.float32)
-    rotations_input = tf.keras.Input(shape=(image_size, image_size), name="rotations_input", dtype=tf.float32)
-
-    # Initialize variables for forward pass
-    signal = image_input
-
-    # Forward pass
-    for od in output_dims:
-        signal = ConvHarmonic(
-            output_dim=od,
-            template_radius=template_radius,
-            activation="linear",
-            rotation_order=1
-        )([signal, bc_input, rotations_input])
-        signal = BetaRelu()(signal)
-    signal = GlobalComplexPooling()(signal)
-    output = tf.keras.layers.Dense(10, activation="linear")(signal)
-
-    imcnn = tf.keras.Model(
-        inputs=[image_input, bc_input, rotations_input], outputs=output, name="mnist_model"
-    )
-    imcnn.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-        metrics=["sparse_categorical_accuracy"]
-    )
-    return imcnn
 
 
 def hypertuning(mnist_atlas, n_radial, n_angular, batch_size, save_path, epochs=5):
@@ -82,7 +44,7 @@ def hypertuning(mnist_atlas, n_radial, n_angular, batch_size, save_path, epochs=
     best_model.save(save_path)
 
 
-def training(mnist_atlas, n_radial, n_angular, batch_size, output_dims, save_path, epochs=10):
+def training(mnist_atlas, n_radial, n_angular, batch_size, model, save_path, epochs=10):
     # Get data
     train_data, template_radius = dataset(
         mnist_atlas, set_type="train", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
@@ -91,13 +53,7 @@ def training(mnist_atlas, n_radial, n_angular, batch_size, output_dims, save_pat
         mnist_atlas, set_type="test", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
     )
 
-    # Get model
-    model = define_model(
-        output_dims=output_dims,
-        template_radius=template_radius,
-        n_radial=n_radial,
-        n_angular=n_angular,
-    )
+    # Show model summary
     model.summary()
 
     # Train model
