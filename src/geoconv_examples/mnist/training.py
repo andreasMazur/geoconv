109a00c60796f82b4b195dfe19945d6d778064de
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-01-23 15:44:52 +0100

    Added termination on nan

--- src/geoconv_examples/mnist/training.py
+++ src/geoconv_examples/mnist/training.py
@@ -1,47 +1,7 @@
 from geoconv_examples.mnist.dataset import dataset
 
-import keras_tuner as kt
+import tensorflow as tf
 import json
-
-
-def hypertuning(mnist_atlas, n_radial, n_angular, batch_size, save_path, epochs=5):
-    # Get data
-    train_data, template_radius = dataset(
-        mnist_atlas, set_type="train", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
-    )
-    test_data, _ = dataset(
-        mnist_atlas, set_type="test", n_radial=n_radial, n_angular=n_angular, batch_size=batch_size
-    )
-
-    # Define hypermodel function
-    def get_hypermodel(hp):
-        model = define_model(
-            output_dims=[
-                hp.Int("units", min_value=8, max_value=64, step=2) for _ in range(hp.Int("n_layers", min_value=4, max_value=16))
-            ],
-            template_radius=template_radius,
-            n_radial=n_radial,
-            n_angular=n_angular,
-            learning_rate=hp.Float("learning_rate", min_value=1e-6, max_value=0.1),
-        )
-        model.summary()
-        return model
-
-    # Hyperparameter search
-    tuner = kt.BayesianOptimization(
-        hypermodel=get_hypermodel,
-        objective=kt.Objective("val_sparse_categorical_accuracy", direction="max"),
-        max_trials=10_000,
-        num_initial_points=10,
-        seed=42
-    )
-    tuner.search(train_data, epochs=epochs, validation_data=test_data)
-
-    # Save best model
-    best_model = tuner.get_best_models()[0]
-    if save_path[-6:] != ".keras":
-        save_path += ".keras"
-    best_model.save(save_path)
 
 
 def training(mnist_atlas, n_radial, n_angular, batch_size, model, save_path, epochs=10):
@@ -57,7 +17,8 @@ def training(mnist_atlas, n_radial, n_angular, batch_size, model, save_path, epo
     model.summary()
 
     # Train model
-    history = model.fit(x=train_data, batch_size=batch_size, epochs=epochs, validation_data=test_data)
+    term = tf.keras.callbacks.TerminateOnNaN()
+    history = model.fit(x=train_data, batch_size=batch_size, epochs=epochs, validation_data=test_data, callbacks=[term])
 
     # Save model
     if save_path[-6:] != ".keras":
