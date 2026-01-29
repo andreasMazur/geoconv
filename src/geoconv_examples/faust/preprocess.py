efdd3e182c51f8d5a60359e73a71868c097bec1e
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-01-29 10:43:07 +0100

    Refactored FAUST preprocessing

--- src/geoconv_examples/faust/preprocess.py
+++ src/geoconv_examples/faust/preprocess.py
@@ -6,6 +6,25 @@ import shutil
 import numpy as np
 
 
+def load_or_repair(mesh_load_path, mesh_save_path, max_chart_radius, method, normalization_method, processes):
+    try:
+        # Remember chart radii for BC computation
+        atlas = load_atlas(mesh_save_path)
+        print(f"[load or repair] Atlas loaded from: '{mesh_save_path}'.")
+    except KeyError:
+        print(f"[load or repair] Repairing atlas for: '{mesh_load_path}'")
+        mesh = trimesh.load(mesh_load_path)
+        atlas = Atlas(
+            triangle_mesh=mesh,
+            max_radius=max_chart_radius,
+            method=method,
+            normalization_method=normalization_method,
+            processes=processes
+        )
+        atlas.save(mesh_save_path)
+    return atlas
+
+
 def preprocess_faust(registration_dir,
                      output_path,
                      template_resolutions=None,
@@ -28,7 +47,7 @@ def preprocess_faust(registration_dir,
         for ply_filename in registrations:
             mesh_save_path = f"{radius_output_path}/{ply_filename.split('.')[0]}.hdf5"
             if not os.path.isfile(mesh_save_path):
-                print(f"Currently computing GPC-systems for: '{ply_filename}'")
+                print(f"[GPC system] Preprocessing '{ply_filename}']")
                 mesh = trimesh.load(f"{registration_dir}/{ply_filename}")
                 atlas = Atlas(
                     triangle_mesh=mesh,
@@ -37,13 +56,21 @@ def preprocess_faust(registration_dir,
                     normalization_method=normalization_method,
                     processes=processes
                 )
+                atlas.save(mesh_save_path)
+
                 # Remember chart radii for BC computation
                 gpc_system_radii.extend(atlas.chart_radii.tolist())
-                atlas.save(mesh_save_path)
             else:
-                print(f"Preprocessed dataset already exists at {mesh_save_path}.")
+                atlas = load_or_repair(
+                    mesh_load_path=f"{registration_dir}/{ply_filename}",
+                    mesh_save_path=mesh_save_path,
+                    max_chart_radius=max_chart_radius,
+                    method=method,
+                    normalization_method=normalization_method,
+                    processes=processes
+                )
+
                 # Remember chart radii for BC computation
-                atlas = load_atlas(mesh_save_path)
                 gpc_system_radii.extend(atlas.chart_radii.tolist())
 
         # 4.) Compute barycentric coordinates
@@ -53,8 +80,16 @@ def preprocess_faust(registration_dir,
             for (n_radial, n_angular) in template_resolutions:
                 for ply_filename in registrations:
                     mesh_save_path = f"{radius_output_path}/{ply_filename.split('.')[0]}.hdf5"
-                    print(f"Currently computing barycentric coordinates for: '{ply_filename}'")
-                    atlas = load_atlas(mesh_save_path)
+                    atlas = load_or_repair(
+                        mesh_load_path=f"{registration_dir}/{ply_filename}",
+                        mesh_save_path=mesh_save_path,
+                        max_chart_radius=max_chart_radius,
+                        method=method,
+                        normalization_method=normalization_method,
+                        processes=processes
+                    )
+
+                    print(f"[BC computation] Calculating BC for '{ply_filename}']")
                     atlas.determine_barycentric_coordinates(
                         n_radial=n_radial,
                         n_angular=n_angular,
