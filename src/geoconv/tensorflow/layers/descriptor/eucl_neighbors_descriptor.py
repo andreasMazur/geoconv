b7a4ae522e6676ae53ee98d564574811f1bca766
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-02 17:32:19 +0100

    fixup! Refactored layer folder structure and added eucl-neighborhoods descriptor layer

--- src/geoconv/tensorflow/layers/descriptor/eucl_neighbors_descriptor.py
+++ src/geoconv/tensorflow/layers/descriptor/eucl_neighbors_descriptor.py
@@ -14,4 +14,6 @@ class EuclNeighborsDescriptor(tf.keras.layers.Layer):
         # 'neighborhoods' : (batch, vertices, n_neighbors, 3)
         neighborhoods, _, _ = compute_neighborhood(inputs, self.n_neighbors)
         neighborhoods_shape = tf.shape(neighborhoods)
-        return tf.reshape(neighborhoods, (neighborhoods_shape[0], neighborhoods_shape[1], self.n_neighbors * 3))
+        return tf.reshape(
+            neighborhoods, (neighborhoods_shape[0], neighborhoods_shape[1], self.n_neighbors * 3)
+        )[..., 3:]  # Cut away the origin-zero vectors
