c259acfd19cacc1c28c61d4d5cefc6fd4fa182d3
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-02 17:56:15 +0100

    Replaced nn input with shot-descriptors

--- src/geoconv_examples/faust/architectures/intrinsic_cnn.py
+++ src/geoconv_examples/faust/architectures/intrinsic_cnn.py
@@ -1,4 +1,4 @@
-from geoconv.tensorflow.layers import AngularMaxPooling, ConvDirac
+from geoconv.tensorflow.layers import AngularMaxPooling, ConvDirac, PointCloudShotDescriptor
 from geoconv.tensorflow.layers import ConvGeodesic
 from geoconv.tensorflow.layers.descriptor.eucl_neighbors_descriptor import EuclNeighborsDescriptor
 
@@ -31,8 +31,7 @@ def define_model(output_dims, template_radius, n_radial, n_angular, kernel, lear
         raise ValueError("The 'kernel' must be either 'geodesic' or 'dirac'.")
 
     # Forward pass
-    signal = EuclNeighborsDescriptor(n_radial, n_angular)(vertices_input)
-    signal = tf.keras.layers.Dense(64, activation="relu")(signal)
+    signal = PointCloudShotDescriptor()(vertices_input)
     for od in output_dims:
         signal = layer_type(
             output_dim=od,
