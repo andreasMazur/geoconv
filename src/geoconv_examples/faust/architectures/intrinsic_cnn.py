650b60a1ea7532d3e2cf3da2768b99326f89dcdd
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-03 17:56:06 +0100

    Adjusted architecture

--- src/geoconv_examples/faust/architectures/intrinsic_cnn.py
+++ src/geoconv_examples/faust/architectures/intrinsic_cnn.py
@@ -32,6 +32,9 @@ def define_model(output_dims, template_radius, n_radial, n_angular, kernel, lear
 
     # Forward pass
     signal = EuclNeighborsDescriptor(n_radial, n_angular)(vertices_input)
+    signal = tf.keras.layers.Normalization(axis=-1)(signal)
+    signal = tf.keras.layers.Dense(64, activation="relu")(signal)
+    signal = tf.keras.layers.LayerNormalization(axis=-1)(signal)
     for od in output_dims:
         signal = layer_type(
             output_dim=od,
