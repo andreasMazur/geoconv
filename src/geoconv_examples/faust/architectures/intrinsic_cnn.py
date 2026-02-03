7711cf496938f10ad80b48335c2e16a2b71b60a2
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-03 16:41:58 +0100

    Changed loss

--- src/geoconv_examples/faust/architectures/intrinsic_cnn.py
+++ src/geoconv_examples/faust/architectures/intrinsic_cnn.py
@@ -18,6 +18,14 @@ def define_hypermodel(hp, output_dims, template_radius, n_radial, n_angular, ker
     return model
 
 
+def custom_act(x, const=6890.):
+    return tf.nn.sigmoid(x) * tf.constant(const)
+
+
+def custom_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.math.squared_difference(y_pred, y_true[..., None]), axis=-1)
+
+
 def define_model(output_dims, template_radius, n_radial, n_angular, kernel, learning_rate=0.001):
     # Define input layers
     vertices_input = tf.keras.Input(shape=(6890, 3), name="vertices_input", dtype=tf.float32)
@@ -39,14 +47,15 @@ def define_model(output_dims, template_radius, n_radial, n_angular, kernel, lear
             activation="relu",
             rotation_delta=1
         )([signal, bc_input])
-        signal = tf.keras.layers.BatchNormalization(axis=-1)(signal)
+        # signal = tf.keras.layers.BatchNormalization(axis=-1)(signal)
         signal = AngularMaxPooling()(signal)
-    output = tf.keras.layers.Dense(6890, activation="linear")(signal)
+    output = tf.keras.layers.Dense(1, activation=custom_act)(signal)
 
     imcnn = tf.keras.Model(inputs=[vertices_input, bc_input], outputs=output, name="faust_model")
     imcnn.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        loss=custom_loss,
         optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-        metrics=["sparse_categorical_accuracy"]
+        # metrics=["sparse_categorical_accuracy"]
     )
     return imcnn
