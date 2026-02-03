c4d11733e052ded1eaf8601b72eaf89f4a0fc753
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-03 16:43:46 +0100

    Changed hyperparameter tuning objective

--- src/geoconv_examples/faust/training.py
+++ src/geoconv_examples/faust/training.py
@@ -35,7 +35,8 @@ def hypertuning(faust_path,
     # Hyperparameter search
     tuner = kt.BayesianOptimization(
         hypermodel=get_hypermodel,
-        objective=kt.Objective("val_sparse_categorical_accuracy", direction="max"),
+        # objective=kt.Objective("val_sparse_categorical_accuracy", direction="max"),
+        objective=kt.Objective("val_loss", direction="min"),
         max_trials=1_000,
         num_initial_points=10,
         seed=42,
