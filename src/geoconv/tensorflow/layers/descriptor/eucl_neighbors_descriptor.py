dc1f63e6c292b9fa94910b7df4a55588756691a2
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-04 14:43:25 +0100

    Added sphere normalization

--- src/geoconv/tensorflow/layers/descriptor/eucl_neighbors_descriptor.py
+++ src/geoconv/tensorflow/layers/descriptor/eucl_neighbors_descriptor.py
@@ -13,6 +13,9 @@ class EuclNeighborsDescriptor(tf.keras.layers.Layer):
     def call(self, inputs, **kwargs):
         # 'neighborhoods' : (batch, vertices, n_neighbors, 3)
         neighborhoods, _, _ = compute_neighborhood(inputs, self.n_neighbors)
+
+        # Scale max sphere radius to 1
+        neighborhoods = neighborhoods / tf.reduce_max(tf.linalg.norm(neighborhoods, axis=-1), axis=-1)[..., None, None]
         neighborhoods_shape = tf.shape(neighborhoods)
         return tf.reshape(
             neighborhoods, (neighborhoods_shape[0], neighborhoods_shape[1], self.n_neighbors * 3)
