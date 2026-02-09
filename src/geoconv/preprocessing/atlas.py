b573c63f560b877d0c05033997c2aecf715ad4e1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-09 10:33:40 +0100

    Fixed a bug in atlas loading

--- src/geoconv/preprocessing/atlas.py
+++ src/geoconv/preprocessing/atlas.py
@@ -137,7 +137,11 @@ def load_atlas(filepath):
         barycentric_coordinates = {}
         for template_res in f["barycentric_coordinates"].keys():
             template_res_key = tuple([int(x) for x in template_res.split("_")])
-            barycentric_coordinates[template_res_key] = np.array(f["barycentric_coordinates"][template_res])
+            n_radial, n_angular = template_res_key[0], template_res_key[1]
+            radius = float(".".join([f"{int(x)}" for x in template_res.split("_")][2:]))
+            barycentric_coordinates[(n_radial, n_angular, radius)] = np.array(
+                f["barycentric_coordinates"][template_res]
+            )
 
         # Load parallel transport angles
         parallel_transport = np.array(f["parallel_transport/transport_angles"])
