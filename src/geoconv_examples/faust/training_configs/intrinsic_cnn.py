673073fc0e63abc2ad00e220518d6e7e6d65ab26
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-12 17:03:51 +0100

    Adjusted training config to new dictionaries

--- src/geoconv_examples/faust/training_configs/intrinsic_cnn.py
+++ src/geoconv_examples/faust/training_configs/intrinsic_cnn.py
@@ -6,34 +6,33 @@ from geoconv_examples.faust.training_configs.dictionaries import FAUST
 def start_training_run(faust_path, preprocessing, kernel, save_path):
     learning_rate = 0.001
     for (n_radial, n_angular) in [(2, 4), (4, 8)]:
-        for gpc_system_radius in [0.05, 0.1, 0.15, 0.2]:
-            for template_radius_str in ["min", "median", "max"]:
-                # Define model
-                template_radius = FAUST[preprocessing][gpc_system_radius][template_radius_str]
-                model = define_model(
-                    output_dims=[32, 64, 96, 64],
-                    preprocess_method=preprocessing,
-                    gpc_radius=gpc_system_radius,
-                    template_radius=template_radius,
-                    n_radial=n_radial,
-                    n_angular=n_angular,
-                    kernel=kernel,
-                    learning_rate=learning_rate,
-                    faust_path=faust_path,
-                )
+        for gpc_system_radius in [0.01, 0.02, 0.03, 0.04, 0.05]:
+            # Define model
+            template_radius = FAUST[preprocessing][gpc_system_radius]
+            model = define_model(
+                output_dims=[32, 64, 96, 64],
+                preprocess_method=preprocessing,
+                gpc_radius=gpc_system_radius,
+                template_radius=template_radius,
+                n_radial=n_radial,
+                n_angular=n_angular,
+                kernel=kernel,
+                learning_rate=learning_rate,
+                faust_path=faust_path,
+            )
 
-                # Start training
-                training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{gpc_system_radius}'.split('.'))}_{template_radius}"
-                print(f"Currently running: {training_run_save_path}")
-                training(
-                    faust_path=faust_path,
-                    n_radial=n_radial,
-                    n_angular=n_angular,
-                    preprocess_method=preprocessing,
-                    gpc_radius=gpc_system_radius,
-                    template_radius=template_radius,
-                    model=model,
-                    save_path=training_run_save_path,
-                    epochs=200,
-                    return_rotations=False
-                )
+            # Start training
+            training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{gpc_system_radius}'.split('.'))}_{template_radius}"
+            print(f"Currently running: {training_run_save_path}")
+            training(
+                faust_path=faust_path,
+                n_radial=n_radial,
+                n_angular=n_angular,
+                preprocess_method=preprocessing,
+                gpc_radius=gpc_system_radius,
+                template_radius=template_radius,
+                model=model,
+                save_path=training_run_save_path,
+                epochs=200,
+                return_rotations=False
+            )
