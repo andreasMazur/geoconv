827e6ed913dc7db1062edf92198cbc4be3514064
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-16 11:30:05 +0100

    Updated MN10 dataset

--- src/geoconv_examples/modelnet/dataset.py
+++ src/geoconv_examples/modelnet/dataset.py
@@ -1,40 +1,8 @@
-import io
 import numpy as np
-import h5py
 import tensorflow as tf
 
 
-def get_content(h5_file, kernel_size):
-    """Given an HDF5 file, return its content required for training.
-
-    Parameters
-    ----------
-    h5_file: h5py.File
-        The HDF5 file that needs to be loaded.
-    kernel_size: tuple
-        The number radial- and angular coordinates of the template that shall be loaded.
-
-    Returns
-    -------
-    (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
-        The vertices, barycentric coordinates, angles for the parallel transport and the ground truth label for the
-        associated shape, in that order.
-    """
-    # Load mesh
-    vertices = np.array(h5_file["triangle_mesh/vertices"])
-
-    # Load barycentric coordinates
-    barycentric_coordinates = np.array(h5_file["barycentric_coordinates"][f"{kernel_size[0]}_{kernel_size[1]}"])
-
-    # Load parallel transport angles
-    parallel_transport = np.array(h5_file["parallel_transport/transport_angles"])
-
-    # Load custom arrays
-    gt = np.array(h5_file["custom_arrays"]["ground_truth"])
-    return vertices, barycentric_coordinates, parallel_transport, gt
-
-
-def generator(path, set_type, n_radial, n_angular, return_rotations=True):
+def generator(path, set_type, n_radial, n_angular, template_radius, method, return_rotations=True):
     """Returns a 'generator'-object for the ModelNet dataset.
 
     Parameters
@@ -42,7 +10,7 @@ def generator(path, set_type, n_radial, n_angular, return_rotations=True):
     path: str
         The path to the preprocessed zip-file of ModelNet.
     set_type: str
-        The set type. Either: 'train', 'validation', 'test' or 'all'.
+        The set type. Either: 'train', 'test' or 'all'.
     n_radial: int
         The number of radial coordinates of the template.
     n_angular: int
@@ -56,31 +24,41 @@ def generator(path, set_type, n_radial, n_angular, return_rotations=True):
         A ModelNet generator.
     """
     if isinstance(path, bytes):
+        path = path.decode("utf-8")
+    if isinstance(set_type, bytes):
         set_type = set_type.decode("utf-8")
 
     zip_file = np.load(path, allow_pickle=True)
 
     # Load zip content
-    zip_content = [f for f in zip_file.files if f.endswith("hdf5")]
-    zip_content.sort()
+    zip_content = []
+    for f in zip_file.files:
+        if "barycentric_coordinates" in f:
+            if f"{n_radial}_{n_angular}" in f and "_".join(f"{template_radius}".split(".")) in f:
+                zip_content.append(f)
 
     # Get desired set type
     if set_type == "train":
-        zip_content = [f for f in zip_content if f.split("/")[2] == "train"]
-    elif set_type == "validation":
-        zip_content = [f for f in zip_content if f.split("/")[2] == "validation"]
+        zip_content = [f for f in zip_content if "train" in f]
     elif set_type == "test":
-        zip_content = [f for f in zip_content if f.split("/")[2] == "test"]
+        zip_content = [f for f in zip_content if "test" in f]
     elif set_type == "all":
         pass
     else:
         raise RuntimeError(f"Invalid set_type: '{set_type}'. Select either 'train', 'validation', 'test' or 'all'.")
 
-    # Yield dataset elements
     for filepath in zip_content:
-        h5_file = h5py.File(io.BytesIO(zip_file[filepath]))
-        vertices, barycentric_coordinates, parallel_transport, gt = get_content(h5_file, (n_radial, n_angular))
+        # Load barycentric coordinates
+        barycentric_coordinates = zip_file[filepath]
+
+        # Load related shape info
+        file_dir = "/".join([f"mn10_{method}_0_01"] + filepath.split("/")[1:-1])
+        vertices = zip_file[f"{file_dir}/vertices"]
+        gt = zip_file[f"{file_dir}/ground_truth"]
+
+        # Yield dataset elements
         if return_rotations:
+            parallel_transport = zip_file[f"{file_dir}/parallel_transport"]
             yield (vertices, barycentric_coordinates, parallel_transport), gt
         else:
             yield (vertices, barycentric_coordinates), gt
