bab6961da1bd90db63bb76308e5a9196d182c9f1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-02-25 12:46:47 +0100

    Added file to compute geodesic errors

--- src/geoconv_examples/faust/geodesic_errors.py
+++ src/geoconv_examples/faust/geodesic_errors.py
@@ -5,8 +5,9 @@ from tqdm import tqdm
 import numpy as np
 
 
-def avg_geodesic_error(model, ref_shape, dataset, distance_calc_method="hdm", processes=1):
+def get_geodesic_errors(model, ref_shape, dataset, distance_calc_method="hdm", processes=1):
     # Prepare geodesic distances
+    # 'geo_distances': (n_vertices, n_vertices)
     geo_distances = calculate_local_charts(
         ref_shape,
         method=distance_calc_method,
@@ -19,13 +20,16 @@ def avg_geodesic_error(model, ref_shape, dataset, distance_calc_method="hdm", pr
     geodesic_errors = np.zeros((0,))
     for inputs, ground_truth in tqdm(dataset, postfix="Calculating geodesic errors.."):
         # Sort distances according to permutation
+        # 'query_order_gd': (n_vertices, n_vertices)
         query_order_gd = geo_distances[ground_truth[0].numpy().astype(np.int32)]
 
-        # Compute prediction
+        # Compute prediction for permuted input
+        # 'y_pred': (n_vertices,)
         y_pred = model(inputs)[0]
         y_pred = y_pred.numpy().argmax(axis=-1)
 
-        # Get geodesic errors
+        # Get geodesic errors (query-order in same order as prediction for permuted input)
+        # 'query_order_gd': (n_vertices,)
         query_order_gd = query_order_gd[np.arange(y_pred.shape[0]), y_pred]
 
         # Append to prior retrieved errors
