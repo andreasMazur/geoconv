49dfa27a805a93d681c4aebf85ce07fc77c762b8
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-03-17 14:13:57 +0100

    Adapted mn10 dataloader to new format

--- src/geoconv_examples/modelnet/dataset.py
+++ src/geoconv_examples/modelnet/dataset.py
@@ -1,14 +1,12 @@
 import numpy as np
 import tensorflow as tf
+import os
 
 
 def generator(path,
               set_type,
-              n_radial,
-              n_angular,
-              template_radius,
+              chart_max_radius,
               method,
-              smallest_gpc_radius=0.01,
               return_rotations=True):
     """Returns a 'generator'-object for the ModelNet dataset.
 
@@ -18,17 +16,10 @@ def generator(path,
         The path to the preprocessed zip-file of ModelNet.
     set_type: str
         The set type. Either: 'train', 'test' or 'all'.
-    n_radial: int
-        The number of radial coordinates of the template.
-    n_angular: int
-        The number of angular coordinates of the template.
-    template_radius: float
-        The radius of the template.
+    chart_max_radius: float
+        The upper bound radius for the local charts.
     method: str
         The used preprocessing method.
-    smallest_gpc_radius: float
-        The smallest maximum radius for GPC-systems. Only this one stores vertices, gt and parallel transports, to save
-        memory usage.
     return_rotations: bool
         Whether to return the rotation angles for the parallel transport.
 
@@ -41,15 +32,17 @@ def generator(path,
         path = path.decode("utf-8")
     if isinstance(set_type, bytes):
         set_type = set_type.decode("utf-8")
+    if isinstance(method, bytes):
+        method = method.decode("utf-8")
 
     zip_file = np.load(path, allow_pickle=True)
 
     # Load zip content
-    zip_content = []
-    for f in zip_file.files:
-        if "barycentric_coordinates" in f:
-            if f"{n_radial}_{n_angular}" in f and "_".join(f"{template_radius}".split(".")) in f:
-                zip_content.append(f)
+    zip_content = [
+        f for f in zip_file.files
+        if f"mn10_{method}_{'_'.join(f'{chart_max_radius}'.split('.'))}" in f and "barycentric_coordinates" in f
+    ]
+    print(f"mn10_{method}_{'_'.join(f'{chart_max_radius}'.split('.'))}")
 
     # Get desired set type
     if set_type == "train":
@@ -61,25 +54,23 @@ def generator(path,
     else:
         raise RuntimeError(f"Invalid set_type: '{set_type}'. Select either 'train', 'validation', 'test' or 'all'.")
 
-    smallest_gpc_radius = "_".join(f"{smallest_gpc_radius}".split('.'))
     for filepath in zip_content:
         # Load barycentric coordinates
         barycentric_coordinates = zip_file[filepath]
 
         # Load related shape info
-        file_dir = "/".join([f"mn10_{method}_{smallest_gpc_radius}"] + filepath.split("/")[1:-1])
+        file_dir = os.path.dirname(filepath)
         vertices = zip_file[f"{file_dir}/vertices"]
         gt = zip_file[f"{file_dir}/ground_truth"]
 
         # Yield dataset elements
         if return_rotations:
-            parallel_transport = zip_file[f"{file_dir}/parallel_transport"]
-            yield (vertices, barycentric_coordinates, parallel_transport), gt
-        else:
             yield (vertices, barycentric_coordinates), gt
+        else:
+            yield (vertices, barycentric_coordinates[..., :2]), gt
 
 
-def dataset(path, set_type, n_radial, n_angular, return_rotations=True):
+def dataset(path, set_type, n_radial, n_angular, chart_max_radius, method, return_rotations=True):
     """Returns a 'tensorflow dataset'-object for the ModelNet dataset.
 
     Parameters
@@ -87,11 +78,15 @@ def dataset(path, set_type, n_radial, n_angular, return_rotations=True):
     path: str
         The path to the preprocessed zip-file of ModelNet.
     set_type: str
-        The set type. Either: 'train', 'validation', 'test' or 'all'.
+        The set type. Either: 'train', 'test' or 'all'.
     n_radial: int
         The number of radial coordinates of the template.
     n_angular: int
         The number of angular coordinates of the template.
+    chart_max_radius: tf.Tensor
+        The upper bound radius for the local charts.
+    method: str
+        The used preprocessing method.
     return_rotations: bool
         Whether to return the rotation angles for the parallel transport.
 
@@ -100,15 +95,20 @@ def dataset(path, set_type, n_radial, n_angular, return_rotations=True):
     tf.data.Dataset:
         A ModelNet dataset.
     """
+    if return_rotations:
+        bc_shape = (3, 3)
+    else:
+        bc_shape = (3, 2)
+    output_signature = (
+        (
+            tf.TensorSpec(shape=(None, 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(None,) + (n_radial, n_angular) + bc_shape, dtype=tf.float32)
+        ),
+        tf.TensorSpec(shape=(None,), dtype=tf.float32),
+    )
+
     return tf.data.Dataset.from_generator(
         generator,
-        args=(path, set_type, n_radial, n_angular, return_rotations),
-        output_signature=(
-            (
-                tf.TensorSpec(shape=(None, 3), dtype=tf.float32),
-                tf.TensorSpec(shape=(None,) + (n_radial, n_angular) + (3, 2), dtype=tf.float32),
-                tf.TensorSpec(shape=(None, None), dtype=tf.float32),
-            ),
-            tf.TensorSpec(shape=(None,), dtype=tf.float32),
-        )
-    ).prefetch(tf.data.AUTOTUNE)
+        args=(path, set_type, chart_max_radius, method, return_rotations),
+        output_signature=output_signature
+    ).prefetch(tf.data.AUTOTUNE).batch(1)
