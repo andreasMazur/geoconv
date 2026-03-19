ed82841f9c4772c04a7e9eccecd98c91ba0dbc38
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-03-19 18:19:38 +0100

    Added temporary skip to nan angles

--- src/geoconv_examples/modelnet/dataset.py
+++ src/geoconv_examples/modelnet/dataset.py
@@ -126,6 +126,10 @@ def generator(path,
         # Load barycentric coordinates
         barycentric_coordinates = zip_file[filepath]
 
+        # TODO: Find what's causing the NaN-angles.
+        if np.isnan(barycentric_coordinates).any():
+            continue
+
         # Load related shape info
         file_dir = os.path.dirname(filepath)
         vertices = zip_file[f"{file_dir}/vertices"]
