e02c600e7746e421b942d44470aa3937dc4260fb
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-03-25 13:23:22 +0100

    fixup! Refunctioned deep sets layer to mask before aggregation

--- src/geoconv_examples/modelnet/architectures/intrinsic_cnn.py
+++ src/geoconv_examples/modelnet/architectures/intrinsic_cnn.py
@@ -1,7 +1,7 @@
 from geoconv.tensorflow.layers import AngularMaxPooling, ConvDirac
 from geoconv.tensorflow.layers import ConvGeodesic
 from geoconv.tensorflow.layers.descriptor.eucl_neighbors_descriptor import EuclNeighborsDescriptor
-from geoconv.tensorflow.layers.pooling.deep_sets import DeepSet
+from geoconv_examples.modelnet.architectures.deep_sets import DeepSet
 from geoconv_examples.modelnet.dataset import MAX_N_VERTICES
 from geoconv_examples.modelnet.training_configs.dictionaries import NORM_FACTORS_EUCL_DESCR_MN10
 
