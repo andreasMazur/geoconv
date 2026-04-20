ac4793b244d5e18646d302b93d0e3aaba405c854
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-04-20 10:52:06 +0200

    Added unlift features layer

---
+++ src/geoconv/tensorflow/layers/unlift_features.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+class UnLiftFeatures2D(tf.keras.layers.Layer):
+    """Lifts features into 2-dimensional space by concatenating zeros after each given scalar."""
+    # @tf.function
+    def call(self, inputs):
+        input_shape = tf.shape(inputs)
+        return tf.reshape(
+            inputs, (input_shape[0], input_shape[1], tf.math.floordiv(input_shape[-1], 2), 2)
+        )[..., 0]
