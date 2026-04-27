3e60a61952a15f6ce05191fa136e49f81599ad9f
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-04-27 11:05:33 +0200

    DGPC and TP charts are now truncated to max-radius

--- src/geoconv/preprocessing/distance_computation.py
+++ src/geoconv/preprocessing/distance_computation.py
@@ -85,6 +85,10 @@ def calculate_local_charts(triangle_mesh,
     distances = np.concatenate(distances, axis=0)
 
     if method in ["dgpc", "tp"]:
+        # Set distances > max_radius to np.inf and their angles to -1.
+        distances[distances[..., 0] > max_radius, 0] = np.inf
+        distances[distances[..., 0] > max_radius, 1] = -1
+
         # The DGPC/TP-algorithm returns both radial- and angular coordinates.
         # Thus, if user expects only distances, we extract them here.
         if not calculate_angle:
