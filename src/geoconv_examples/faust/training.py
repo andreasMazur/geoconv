342083979eb88f139b1a5635bf0298a69bb2f753
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-05-15 14:19:16 +0200

    Added option for tensorboard logging

--- src/geoconv_examples/faust/training.py
+++ src/geoconv_examples/faust/training.py
@@ -83,7 +83,8 @@ def training(faust_path,
              save_path,
              epochs=10,
              return_rotations=True,
-             random_seed=42):
+             random_seed=42,
+             tensorboard_cb=False):
     # Check if model already exists
     test_saving_path = f"{save_path}_test_history.json"
     if os.path.isfile(test_saving_path):
@@ -134,8 +135,21 @@ def training(faust_path,
         verbose=True
     )
     stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=10, min_delta=0.001)
+    callbacks = [term, cp_callback, stop]
+
+    if tensorboard_cb:
+        tb_cb = tf.keras.callbacks.TensorBoard(
+            log_dir=f"{os.path.dirname(save_path)}/tensorboard",
+            histogram_freq=1,
+            write_graph=False,
+            write_steps_per_second=True,
+            update_freq="epoch",
+            profile_batch=(1, 70)
+        )
+        callbacks.append(tb_cb)
+
     train_history = model.fit(
-        x=train_data, batch_size=1, epochs=epochs, validation_data=val_data, callbacks=[term, cp_callback, stop]
+        x=train_data, batch_size=1, epochs=epochs, validation_data=val_data, callbacks=callbacks
     )
 
     # Test best performing model
