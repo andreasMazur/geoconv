fea0c81805d1c74c9d354de245ae09bd4ea9d1f1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-05-31 20:48:45 +0200

    Adjusted saving and chunking

--- src/geoconv_examples/planetswe/preprocess.py
+++ src/geoconv_examples/planetswe/preprocess.py
@@ -1,7 +1,21 @@
-from geoconv.preprocessing.atlas import Atlas
+from geoconv.preprocessing.atlas import Atlas, load_atlas
 from geoconv_examples.planetswe.dataset import create_planetswe_sphere
 
 import numpy as np
+import time
+
+
+def save_atlas(atlas, atlas_save_path):
+    is_saved = False
+    tries = 0
+    while not is_saved:
+        try:
+            atlas.save(atlas_save_path)
+            is_saved = True
+        except BlockingIOError:
+            print(f"Trying to save atlas.. {tries}")
+            tries += 1
+            time.sleep(1)
 
 
 def preprocess(path,
@@ -17,7 +31,10 @@ def preprocess(path,
     sphere_mesh = create_planetswe_sphere(path)
 
     # Compute the atlas
-    for chart_indices in np.split(np.arange(sphere_mesh.vertices.shape[0]), chunks):
+    chart_indices_chunked = np.split(np.arange(sphere_mesh.vertices.shape[0]), chunks)[:2]
+    chart_radii = []
+    for chunk_idx, chart_indices in enumerate(chart_indices_chunked):
+        print(f"Currently computing the atlas for chunk {chunk_idx}: vertices {chart_indices[0]} - {chart_indices[-1]}")
         atlas = Atlas(
             triangle_mesh=sphere_mesh,
             max_radius=max_chart_radius,
@@ -26,13 +43,17 @@ def preprocess(path,
             processes=processes,
             chart_indices=chart_indices
         )
+        chart_radii.extend(atlas.chart_radii.tolist())
+        save_atlas(atlas, atlas_save_path=f"{save_path}_{method}_{chart_indices[0]}_{chart_indices[-1]}")
 
-        # Compute barycentric coordinates
+    # Compute barycentric coordinates
+    for chart_indices in chart_indices_chunked:
         for (n_radial, n_angular) in template_resolutions:
+            atlas = load_atlas(f"{save_path}_{method}_{chart_indices[0]}_{chart_indices[-1]}")
             atlas.determine_barycentric_coordinates(
                 n_radial=n_radial,
                 n_angular=n_angular,
-                radius=np.median(atlas.chart_radii.tolist())
+                radius=np.median(chart_radii),
+                processes=processes
             )
-            # Save atlas
             atlas.save_training_data(f"{save_path}_{method}_{chart_indices[0]}_{chart_indices[-1]}")
