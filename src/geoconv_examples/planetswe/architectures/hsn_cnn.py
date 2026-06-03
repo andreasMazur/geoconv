57a8f026f2daea53acdbd26a2e0cf4a498b778a1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-06-03 12:02:08 +0200

    Adjusted input feature shape

--- src/geoconv_examples/planetswe/architectures/hsn_cnn.py
+++ src/geoconv_examples/planetswe/architectures/hsn_cnn.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def define_model(n_radial, n_angular, template_radius, output_dims, learning_rate, lr_decay_rate):
     # Define input layers
-    features_input = tf.keras.Input(shape=(131_072, 3), name="features_input", dtype=tf.float32)
+    features_input = tf.keras.Input(shape=(131_072, 4), name="features_input", dtype=tf.float32)
     bc_input = tf.keras.Input(shape=(131_072, n_radial, n_angular, 3, 3), name="bc_input", dtype=tf.float32)
 
     # Forward pass
