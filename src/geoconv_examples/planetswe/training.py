d2d1f06ba7307e6092735af16f49b404e502fcd3
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-06-04 13:04:37 +0200

    fixup! Removed redundant custom layer from model loading

--- src/geoconv_examples/planetswe/training.py
+++ src/geoconv_examples/planetswe/training.py
@@ -1,7 +1,6 @@
 from geoconv.tensorflow.layers import ConvDirac, ConvGeodesic, AngularMaxPooling
 from geoconv.tensorflow.layers.activation_beta_relu import BetaRelu
 from geoconv.tensorflow.layers.convolutions.conv_harmonic import ConvHarmonic
-from geoconv.tensorflow.layers.lift_features import LiftFeatures2D
 from geoconv_gem.tensorflow.layers.convolutions.conv_gem import ConvGEM
 from geoconv_gem.tensorflow.layers.convolutions.conv_eman import ConvEMAN
 from geoconv_gem.tensorflow.layers.convolutions.conv_gem_p import ConvGEMP
