391084c393e8cc375a5ed295176783bed4589b7d
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-06-06 10:47:16 +0200

    fixup! Adjusted training skip condition

--- src/geoconv_examples/planetswe/training.py
+++ src/geoconv_examples/planetswe/training.py
@@ -68,9 +68,7 @@ def training(model,
 
     # Check if metric rollout already has been computed - last benchmark in training function
     if os.path.isfile(rollout_statistics_metric_path):
-        print(
-            f"{rollout_statistics_metric_path} already exists! Skipping training..."
-        )
+        print(f"{rollout_statistics_metric_path} already exists! Skipping training...")
         return
 
     # Set seeds
