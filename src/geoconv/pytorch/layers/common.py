7282526715b7fdd6b76418e04f71628180cd5e22
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-06-22 10:38:01 +0200

    Removed old PyTorch files

--- src/geoconv/pytorch/layers/common.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-
-def scipy_softmax(x):
-    """Softmax like in scipy"""
-    e_x = torch.exp(x)
-    return e_x / torch.sum(e_x)
--- src/geoconv/pytorch/layers/pooling/__init__.py
+++
@@ -1,3 +0,0 @@
-from .angular_avg_pooling import AngularAvgPooling
-from .angular_max_pooling import AngularMaxPooling
-from .angular_min_pooling import AngularMinPooling
