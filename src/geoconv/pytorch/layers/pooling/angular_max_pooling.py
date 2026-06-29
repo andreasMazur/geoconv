0f7f568936b96eab4f4c50ba20ff66ea8c91a8b2
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-06-29 16:54:28 +0200

    Added pytorch port for angular max-pooling

--- src/geoconv/pytorch/layers/pooling/angular_max_pooling.py
+++ src/geoconv/pytorch/layers/pooling/angular_max_pooling.py
@@ -1,24 +1,24 @@
-import tensorflow as tf
-import keras
+from torch import nn
+
+import torch
 
 
-class AngularAvgPooling(keras.layers.Layer):
+class AngularMaxPooling(nn.Module):
     """The implementation for angular max-pooling"""
 
-    @tf.function
-    def call(self, inputs):
-        """Averages over the results of a intrinsic surface convolution.
+    def forward(self, inputs):
+        """Max-pools over the results of a intrinsic surface convolution.
 
         Parameters
         ----------
-        inputs: tensorflow.Tensor
+        inputs: torch.Tensor
             A tensor of size: (batch_shapes, n_vertices, n_rotations, feature_dim), where 'n_vertices' references to the
             total amount of vertices in the triangle mesh, 'n_rotations' to the amount of rotations considered during
             the intrinsic surface convolution and 'feature_dim' to the feature dimensionality.
 
         Returns
         -------
-        tensorflow.Tensor:
+        torch.Tensor:
             A three-dimensional tensor of size (batch_shapes, n_vertices, feature_dim).
         """
-        return tf.reduce_mean(inputs, axis=-2)
+        return torch.amax(inputs, dim=-2)
