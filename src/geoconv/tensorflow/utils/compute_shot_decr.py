1082dd7a5a95a09ed8e626772cec50c41d0a59d0
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-07-01 13:51:15 +0200

    Updated comments

--- src/geoconv/tensorflow/utils/compute_shot_decr.py
+++ src/geoconv/tensorflow/utils/compute_shot_decr.py
@@ -79,56 +79,47 @@ def shot_descr(
     neighborhood_indices = neighborhood_indices[:, 1:]
 
     # Compute spherical coordinates of vertices in neighborhoods
+    # 'v_radial': (n_vertices, n_neighbors)
     v_radial = tf.linalg.norm(neighborhoods, axis=-1)
+
+    # 'v_elevation': (n_vertices, n_neighbors)
     v_elevation = tf.math.acos(  # machine accuracy sometimes return slightly larger/smaller values than allowed
         tf.clip_by_value(
             neighborhoods[:, :, 2] / v_radial, clip_value_min=-1.0, clip_value_max=1.0
         )
     )
+
+    # 'v_azimuth': (n_vertices, n_neighbors)
     v_azimuth = tf.math.atan2(neighborhoods[:, :, 1], neighborhoods[:, :, 0]) + np.pi
 
     # Bin spherical coordinates of vertices into spherical grid
-    # 'x_histogram': (n_vertices, n_neighbors)
-    radial_histogram = tf.histogram_fixed_width_bins(
-        v_radial, [0.0, radius], nbins=radial_bins
-    )
-    elevation_histogram = tf.histogram_fixed_width_bins(
-        v_elevation, [0.0, np.pi], nbins=elevation_bins
-    )
-    azimuth_histogram = tf.histogram_fixed_width_bins(
-        v_azimuth, [0.0, 2 * np.pi], nbins=azimuth_bins
-    )
+    # '?_histogram': (n_vertices, n_neighbors)
+    radial_histogram = tf.histogram_fixed_width_bins(v_radial, [0.0, radius], nbins=radial_bins)
+    elevation_histogram = tf.histogram_fixed_width_bins(v_elevation, [0.0, np.pi], nbins=elevation_bins)
+    azimuth_histogram = tf.histogram_fixed_width_bins(v_azimuth, [0.0, 2 * np.pi], nbins=azimuth_bins)
+
     # 'sphere_bins': (n_vertices, n_neighbors, 3)
-    sphere_bins = tf.stack(
-        [azimuth_histogram, elevation_histogram, radial_histogram], axis=-1
-    )
+    sphere_bins = tf.stack([azimuth_histogram, elevation_histogram, radial_histogram], axis=-1)
 
     # Compute inner product of vertex-normals from vertices in same bins with z-axis of lrf
     # 'neighborhood_normals': (n_vertices, n_neighbors, 3)
     neighborhood_normals = tf.gather(normals, neighborhood_indices)
+
     # 'cosines': (n_vertices, n_neighbors)
     cosines = tf.einsum("vi,vni->vn", normals, neighborhood_normals)
+
     # 'cosine_bins': (n_vertices, n_neighbors)
-    cosine_bins = tf.histogram_fixed_width_bins(
-        cosines, [-1.0, 1.0], nbins=histogram_bins
-    )
+    cosine_bins = tf.histogram_fixed_width_bins(cosines, [-1.0, 1.0], nbins=histogram_bins)
 
     # cosine_bins: '(vertex, neighbor, sphere-bin-index AND histogram-index => 3 + 1 = 4)'
-    neighborhood_shape = tf.shape(neighborhood_indices)
-    cosine_bins = tf.concat(
-        [sphere_bins, tf.expand_dims(cosine_bins, axis=-1)], axis=-1
-    )
+    cosine_bins = tf.concat([sphere_bins, tf.expand_dims(cosine_bins, axis=-1)], axis=-1)
 
     # cosine_bins: '(vertex, neighbor, vertex-index AND sphere-bin-index AND histogram-index => 1 + 4 = 5)'
+    neighborhood_shape = tf.shape(neighborhood_indices)
     cosine_bins = tf.concat(
         [
-            tf.tile(
-                tf.reshape(
-                    tf.range(neighborhood_shape[0]), (neighborhood_shape[0], 1, 1)
-                ),
-                multiples=(1, neighborhood_shape[1], 1),
-            ),
-            cosine_bins,
+            tf.tile(tf.range(neighborhood_shape[0])[:, None, None], multiples=(1, neighborhood_shape[1], 1)),
+            cosine_bins
         ],
         axis=-1,
     )
@@ -147,142 +138,155 @@ def shot_descr(
     ##############################
     # Quadrilateral interpolation
     ##############################
-    azimuth_colon = 1
-    elevation_colon = 2
-    radial_colon = 3
-    histogram_colon = 4
+    azimuth_column = 1
+    elevation_column = 2
+    radial_column = 3
+    histogram_column = 4
 
     ###############################
     # Histogram bins interpolation
     ###############################
-    central_values, step_size = determine_central_values(
-        start=-1.0, stop=1.0, n_bins=histogram_bins
-    )
-    d = (
-        tf.abs(cosines - tf.gather(central_values, cosine_bins[:, :, histogram_colon])) / step_size
-    )
+    # 'central_values': (n_histogram_bins,)
+    # 'step_size'     : ()
+    central_values, step_size = determine_central_values(start=-1.0, stop=1.0, n_bins=histogram_bins)
+
+    # 'd': (n_vertices, n_neighbors)
+    d = tf.abs(cosines - tf.gather(central_values, cosine_bins[:, :, histogram_column])) / step_size
 
     # Increment histogram bins by 1 - d
     histogram = tf.tensor_scatter_nd_add(histogram, cosine_bins, 1 - d)
 
     # Determine the closest other bin
-    closest_neighbor = tf.math.top_k(
-        -tf.square(tf.expand_dims(cosines, axis=-1) - central_values), k=2
-    )[1][:, :, 1]
+    # 'closest_neighbor': (n_vertices, n_neighbors)
+    closest_neighbor = tf.math.top_k(-tf.square(tf.expand_dims(cosines, axis=-1) - central_values), k=2)[1][:, :, 1]
+
+    # 'd': (n_vertices, n_neighbors)
     d = tf.abs(cosines - tf.gather(central_values, closest_neighbor)) / step_size
 
     # Increment neighboring histogram bins by 1 - d
     histogram = tf.tensor_scatter_nd_add(
-        histogram,
-        tf.concat(
+        histogram,  # (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+        tf.concat(   # (n_vertices, n_neighbors, 5)
             [
-                cosine_bins[:, :, :histogram_colon],
-                tf.expand_dims(closest_neighbor, axis=-1),
+                cosine_bins[:, :, :histogram_column],
+                closest_neighbor[..., None]
             ],
-            axis=-1,
+            axis=-1
         ),
-        1 - d,
+        1 - d  # (n_vertices, n_neighbors)
     )
 
     ################################
     # Azimuth volumes interpolation
     ################################
-    central_values, step_size = determine_central_values(
-        start=0.0, stop=2 * np.pi, n_bins=azimuth_bins
-    )
-    d = (
-        tf.abs(v_azimuth - tf.gather(central_values, cosine_bins[:, :, azimuth_colon])) / step_size
-    )
+    # 'central_values': (n_azimuth_bins,)
+    # 'step_size'     : ()
+    central_values, step_size = determine_central_values(start=0.0, stop=2 * np.pi, n_bins=azimuth_bins)
+
+    # 'd': (n_vertices, n_neighbors)
+    d = tf.abs(v_azimuth - tf.gather(central_values, cosine_bins[:, :, azimuth_column])) / step_size
 
     # Increment histogram bins by 1 - d
+    # 'histogram':   (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+    # 'cosine_bins': (n_vertices, n_elevation_bins)
+    # 'd':           (n_vertices, n_neighbors)
     histogram = tf.tensor_scatter_nd_add(histogram, cosine_bins, 1 - d)
 
     # Determine the closest other bin
-    closest_neighbor = tf.math.top_k(
-        -tf.square(tf.expand_dims(v_azimuth, axis=-1) - central_values), k=2
-    )[1][:, :, 1]
+    # 'closest_neighbor': (n_vertices, n_neighbors)
+    closest_neighbor = tf.math.top_k(-tf.square(tf.expand_dims(v_azimuth, axis=-1) - central_values), k=2)[1][:, :, 1]
+
+    # 'd': (n_vertices, n_neighbors)
     d = tf.abs(v_azimuth - tf.gather(central_values, closest_neighbor)) / step_size
 
     # Increment neighboring histogram bins by 1 - d
     histogram = tf.tensor_scatter_nd_add(
-        histogram,
-        tf.concat(
+        histogram,  # (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+        tf.concat(  # (n_vertices, n_neighbors, 5)
             [
-                cosine_bins[:, :, :azimuth_colon],
+                cosine_bins[:, :, :azimuth_column],
                 tf.expand_dims(closest_neighbor, axis=-1),
-                cosine_bins[:, :, azimuth_colon + 1 :],
+                cosine_bins[:, :, azimuth_column + 1 :],
             ],
-            axis=-1,
+            axis=-1
         ),
-        1 - d,
+        1 - d  # (n_vertices, n_neighbors)
     )
 
     ##################################
     # Elevation volumes interpolation
     ##################################
-    central_values, step_size = determine_central_values(
-        start=0.0, stop=np.pi, n_bins=elevation_bins
-    )
-    d = (
-        tf.abs(
-            v_elevation - tf.gather(central_values, cosine_bins[:, :, elevation_colon])
-        ) / step_size
-    )
+    # 'central_values': (n_elevation_bins,)
+    # 'step_size'     : ()
+    central_values, step_size = determine_central_values(start=0.0, stop=np.pi, n_bins=elevation_bins)
+
+    # 'd': (n_vertices, n_neighbors)
+    d = tf.abs( v_elevation - tf.gather(central_values, cosine_bins[:, :, elevation_column])) / step_size
 
     # Increment histogram bins by 1 - d
+    # 'histogram':   (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+    # 'cosine_bins': (n_vertices, n_elevation_bins)
+    # 'd':           (n_vertices, n_neighbors)
     histogram = tf.tensor_scatter_nd_add(histogram, cosine_bins, 1 - d)
 
     # Determine the closest other bin
-    closest_neighbor = tf.math.top_k(
-        -tf.square(tf.expand_dims(v_elevation, axis=-1) - central_values), k=2
-    )[1][:, :, 1]
+    # 'closest_neighbor': (n_vertices, n_neighbors)
+    closest_neighbor = tf.math.top_k(-tf.square(tf.expand_dims(v_elevation, axis=-1) - central_values), k=2)[1][:, :, 1]
+
+    # 'd': (n_vertices, n_neighbors)
     d = tf.abs(v_elevation - tf.gather(central_values, closest_neighbor)) / step_size
 
     # Increment neighboring histogram bins by 1 - d
     histogram = tf.tensor_scatter_nd_add(
-        histogram,
-        tf.concat(
+        histogram,  # (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+        tf.concat(  # (n_vertices, n_neighbors, 5)
             [
-                cosine_bins[:, :, :elevation_colon],
+                cosine_bins[:, :, :elevation_column],
                 tf.expand_dims(closest_neighbor, axis=-1),
-                cosine_bins[:, :, elevation_colon + 1 :],
+                cosine_bins[:, :, elevation_column + 1:],
             ],
-            axis=-1,
+            axis=-1
         ),
-        1 - d,
+        1 - d  # (n_vertices, n_neighbors)
     )
 
     ###############################
     # Radial volumes interpolation
     ###############################
+    # 'central_values': (n_radial_bins,)
+    # 'step_size'     : ()
     central_values, step_size = determine_central_values(
         start=0.0, stop=radius, n_bins=radial_bins
     )
-    d = (
-        tf.abs(v_radial - tf.gather(central_values, cosine_bins[:, :, radial_colon])) / step_size
-    )
+
+    # 'd': (n_vertices, n_neighbors)
+    d = tf.abs(v_radial - tf.gather(central_values, cosine_bins[:, :, radial_column])) / step_size
 
     # Increment histogram bins by 1 - d
+    # 'histogram':   (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+    # 'cosine_bins': (n_vertices, n_radial_bins)
+    # 'd':           (n_vertices, n_neighbors)
     histogram = tf.tensor_scatter_nd_add(histogram, cosine_bins, 1 - d)
 
     # Determine the closest other bin
-    closest_neighbor = tf.math.top_k(
-        -tf.square(tf.expand_dims(v_radial, axis=-1) - central_values), k=2
-    )[1][:, :, 1]
+    # 'closest_neighbor': (n_vertices, n_neighbors)
+    closest_neighbor = tf.math.top_k(-tf.square(tf.expand_dims(v_radial, axis=-1) - central_values), k=2)[1][:, :, 1]
+
+    # 'd': (n_vertices, n_neighbors)
     d = tf.abs(v_radial - tf.gather(central_values, closest_neighbor)) / step_size
 
     # Increment neighboring histogram bins by 1 - d
     histogram = tf.tensor_scatter_nd_add(
-        histogram,
-        tf.concat(
+        histogram,  # (n_vertices, n_azimuth_bins, n_elevation_bins, n_radial_bins, n_histogram_bins)
+        tf.concat(  # (n_vertices, n_neighbors, 5)
             [
-                cosine_bins[:, :, :radial_colon],
+                cosine_bins[:, :, :radial_column],
                 tf.expand_dims(closest_neighbor, axis=-1),
-                cosine_bins[:, :, radial_colon + 1 :],
+                cosine_bins[:, :, radial_column + 1 :],
             ],
-            axis=-1,
+            axis=-1
         ),
-        1 - d,
+        1 - d  # (n_vertices, n_neighbors)
     )
 
     #########################################
