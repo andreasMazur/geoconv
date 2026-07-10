ab763c33ffab12b5ef796f733f2bc6a7ab0cb9e2
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-07-10 12:48:57 +0200

    Updated imports

--- src/geoconv/tensorflow/layers/__init__.py
+++ src/geoconv/tensorflow/layers/__init__.py
@@ -1,9 +1,21 @@
+# Activation functions
+from geoconv.tensorflow.layers.activations.beta_relu import BetaRelu
+
+# Convolution layers
+from geoconv.tensorflow.layers.convolutions.conv_base import ConvBase
 from geoconv.tensorflow.layers.convolutions.conv_intrinsic import ConvIntrinsic
 from geoconv.tensorflow.layers.convolutions.conv_dirac import ConvDirac
 from geoconv.tensorflow.layers.convolutions.conv_geodesic import ConvGeodesic
+from geoconv.tensorflow.layers.convolutions.conv_harmonic import ConvHarmonic
+from geoconv.tensorflow.layers.convolutions.conv_gem import ConvGEM
+from geoconv.tensorflow.layers.convolutions.conv_gem_p import ConvGEMP
+from geoconv.tensorflow.layers.convolutions.conv_eman import ConvEMAN
+from geoconv.tensorflow.layers.convolutions.conv_eman_p import ConvEMANP
 
-from geoconv.tensorflow.layers.experimental.normalize_point_cloud import NormalizePointCloud
-
-from .pooling import AngularMaxPooling
-
+# Descriptor layers
+from geoconv.tensorflow.layers.descriptor.eucl_neighbors_descriptor import EuclNeighborsDescriptor
 from geoconv.tensorflow.layers.descriptor.shot_descriptor import ShotDescriptor
+
+# Pooling layers
+from geoconv.tensorflow.layers.pooling.angular_max_pooling import AngularMaxPooling
+from geoconv.tensorflow.layers.pooling.global_complex_max_pooling import GlobalComplexPooling
