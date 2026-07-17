781e29e5ae7361898a3612f4d574db36b7e8fcf0
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-07-17 14:47:58 +0200

    Replaced 'super()' with 'self.' call

--- src/geoconv/tensorflow/layers/convolutions/conv_eman.py
+++ src/geoconv/tensorflow/layers/convolutions/conv_eman.py
@@ -121,7 +121,7 @@ class ConvEMAN(ConvGEM):
         # Calculate self- and neighbor embeddings
         # conv_self  : (n_batch, n_vertices, output_dim / 2, 2)
         # conv_neigh : (n_batch, n_vertices, n_radial, n_angular, output_dim / 2, 2)
-        conv_self, conv_neigh = super().call_helper(
+        conv_self, conv_neigh = self.call_helper(
             signals, template_vertex_interpolations, return_self_and_neighbor_embeddings=True
         )
 
