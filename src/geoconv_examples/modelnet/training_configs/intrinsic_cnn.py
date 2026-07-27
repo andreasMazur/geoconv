33c9e312685a30d21c07754d9b6d580cbb1e579f
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-07-27 10:46:22 +0200

    Refactored training configuration arguments

--- src/geoconv_examples/modelnet/training_configs/intrinsic_cnn.py
+++ src/geoconv_examples/modelnet/training_configs/intrinsic_cnn.py
@@ -9,9 +9,9 @@ def start_training_run(mn10_path,
                        save_path,
                        n_radial,
                        n_angular,
+                       chart_max_radius,
                        learning_rate=0.001,
-                       lr_decay_rate=1.0,
-                       gpc_system_radii=None):
+                       lr_decay_rate=1.0):
     """Starts the training run of either an ISC or a GCNN model.
 
     Parameters
@@ -28,41 +28,37 @@ def start_training_run(mn10_path,
         The amount of radial coordinates considered by the discretized template.
     n_angular: int
         The amount of angular coordinates considered by the discretized template.
+    chart_max_radius: float
+        The maximum allowed chart radius.
     learning_rate: float
         The learning rate
     lr_decay_rate: float
         The decay rate for the learning rate
-    gpc_system_radii: list
-        A list of maximum chart radii.
     """
-    if gpc_system_radii is None:
-        gpc_system_radii = [0.01, 0.02, 0.03, 0.04, 0.05]
+    # Define model
+    template_radius = MN10[(n_radial, n_angular)][preprocessing][chart_max_radius]
+    model = define_model(
+        output_dims=[32, 64, 96, 64],
+        template_radius=template_radius,
+        n_radial=n_radial,
+        n_angular=n_angular,
+        kernel=kernel,
+        learning_rate=learning_rate,
+        lr_decay_rate=lr_decay_rate,
+    )
 
-    for gpc_system_radius in gpc_system_radii:
-        # Define model
-        template_radius = MN10[(n_radial, n_angular)][preprocessing][gpc_system_radius]
-        model = define_model(
-            output_dims=[32, 64, 96, 64],
-            template_radius=template_radius,
-            n_radial=n_radial,
-            n_angular=n_angular,
-            kernel=kernel,
-            learning_rate=learning_rate,
-            lr_decay_rate=lr_decay_rate,
-        )
-
-        # Start training
-        training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{gpc_system_radius}'.split('.'))}_{template_radius}"
-        print(f"Currently running: {training_run_save_path}")
-        training(
-            mn10_path=mn10_path,
-            n_radial=n_radial,
-            n_angular=n_angular,
-            preprocess_method=preprocessing,
-            gpc_radius=gpc_system_radius,
-            model=model,
-            save_path=training_run_save_path,
-            epochs=200,
-            random_seed=42,
-            return_rotations=False
-        )
+    # Start training
+    training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{chart_max_radius}'.split('.'))}_{template_radius}"
+    print(f"Currently running: {training_run_save_path}")
+    training(
+        mn10_path=mn10_path,
+        n_radial=n_radial,
+        n_angular=n_angular,
+        preprocess_method=preprocessing,
+        gpc_radius=chart_max_radius,
+        model=model,
+        save_path=training_run_save_path,
+        epochs=200,
+        random_seed=42,
+        return_rotations=False
+    )
--- src/geoconv_examples/modelnet/training_configs/training_conf.py
+++ src/geoconv_examples/modelnet/training_configs/training_conf.py
@@ -10,10 +10,11 @@ def start_training_run(mn10_path,
                        preprocessing,
                        save_path,
                        model_type,
-                       template_resolutions=None,
+                       n_radial,
+                       n_angular,
+                       chart_max_radius,
                        learning_rate=0.0004,
-                       lr_decay_rate=1.0,
-                       gpc_system_radii=None):
+                       lr_decay_rate=1.0):
     """Starts the training run for a equivariant mesh convolution architectures.
 
     Parameters
@@ -24,14 +25,18 @@ def start_training_run(mn10_path,
         The used charting algorithm.
     save_path: str
         The path which points to where the model and benchmark statistics will be saved.
-    template_resolutions: list
-        A list of tuples, with each one describing the n_radial and n_angular of one discretized template.
+    model_type: str
+        The gauge-equivariant architecture type. Must be one from: ['gem_cnn', 'eman', 'gem_p_cnn', 'eman_p'].
+    n_radial: int
+        The amount of radial coordinates considered by the discretized template.
+    n_angular: int
+        The amount of angular coordinates considered by the discretized template.
+    chart_max_radius: float
+        The maximum allowed chart radius.
     learning_rate: float
         The learning rate
     lr_decay_rate: float
         The decay rate for the learning rate
-    gpc_system_radii: list
-        A list of maximum chart radii.
     """
     assert model_type in ["gem_cnn", "eman", "gem_p_cnn", "eman_p"], (
         "Select model type from: ['gem_cnn', 'eman', 'gem_p_cnn', 'eman_p']"
@@ -45,48 +50,40 @@ def start_training_run(mn10_path,
     else:
         define_model = eman_p
 
-    if template_resolutions is None:
-        template_resolutions = [(4, 8)]
+    # Define model
+    template_radius = MN10[(n_radial, n_angular)][preprocessing][chart_max_radius]
+    model = define_model(
+        input_types=[
+            [0 for _ in range(int(n_radial * n_angular * 3))],
+            [x for _ in range(8) for x in [1, 2]],
+            [x for _ in range(16) for x in [1, 2]],
+            [x for _ in range(24) for x in [1, 2]],
+        ],
+        output_types=[
+            [x for _ in range(8) for x in [1, 2]],
+            [x for _ in range(16) for x in [1, 2]],
+            [x for _ in range(24) for x in [1, 2]],
+            [0 for _ in range(64)]  # causes 128 dimensions but every second element is zero => 64 dims
+        ],
+        template_radius=template_radius,
+        n_radial=n_radial,
+        n_angular=n_angular,
+        learning_rate=learning_rate,
+        lr_decay_rate=lr_decay_rate
+    )
 
-    if gpc_system_radii is None:
-        gpc_system_radii = [0.01, 0.02, 0.03, 0.04, 0.05]
-
-    for (n_radial, n_angular) in template_resolutions:
-        for gpc_system_radius in gpc_system_radii:
-            # Define model
-            template_radius = MN10[(n_radial, n_angular)][preprocessing][gpc_system_radius]
-            model = define_model(
-                input_types=[
-                    [0 for _ in range(int(n_radial * n_angular * 3))],
-                    [x for _ in range(8) for x in [1, 2]],
-                    [x for _ in range(16) for x in [1, 2]],
-                    [x for _ in range(24) for x in [1, 2]],
-                ],
-                output_types=[
-                    [x for _ in range(8) for x in [1, 2]],
-                    [x for _ in range(16) for x in [1, 2]],
-                    [x for _ in range(24) for x in [1, 2]],
-                    [0 for _ in range(64)]  # causes 128 dimensions but every second element is zero => 64 dims
-                ],
-                template_radius=template_radius,
-                n_radial=n_radial,
-                n_angular=n_angular,
-                learning_rate=learning_rate,
-                lr_decay_rate=lr_decay_rate
-            )
-
-            # Start training
-            training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{gpc_system_radius}'.split('.'))}_{template_radius}"
-            print(f"Currently running: {training_run_save_path}")
-            training(
-                mn10_path=mn10_path,
-                n_radial=n_radial,
-                n_angular=n_angular,
-                preprocess_method=preprocessing,
-                gpc_radius=gpc_system_radius,
-                model=model,
-                save_path=training_run_save_path,
-                epochs=200,
-                random_seed=42,
-                return_rotations=True
-            )
+    # Start training
+    training_run_save_path = f"{save_path}/{n_radial}_{n_angular}_{'_'.join(f'{chart_max_radius}'.split('.'))}_{template_radius}"
+    print(f"Currently running: {training_run_save_path}")
+    training(
+        mn10_path=mn10_path,
+        n_radial=n_radial,
+        n_angular=n_angular,
+        preprocess_method=preprocessing,
+        gpc_radius=chart_max_radius,
+        model=model,
+        save_path=training_run_save_path,
+        epochs=200,
+        random_seed=42,
+        return_rotations=True
+    )
