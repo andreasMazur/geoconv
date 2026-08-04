aa3c1e5a5c33284dabf9ff6af641d92a7f055f03
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-08-04 17:01:07 +0200

    Adjusted normalization values such that velocity vectors remain untouched

--- src/geoconv_examples/planetswe/training_configs/dictionaries.py
+++ src/geoconv_examples/planetswe/training_configs/dictionaries.py
@@ -1,8 +1,8 @@
 
 # Compute channel-wise for entire training split: [velocity_x, velocity_y, height]
 PLANETSWE_NORM_VALUES = {
-    "channel_means": [0.0011791672235348196, -0.05589736121891466, -56.605416268502616],
-    "channel_stds": [7.224222325437175, 13.895557044977567, 296.6626617552545]
+    "channel_means": [0., 0., -56.605416268502616],
+    "channel_stds": [1.0, 1., 296.6626617552545]
 }
 
 PLANETSWE = {
