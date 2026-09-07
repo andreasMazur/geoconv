97a38eaca6d6ece2ac2ab8a68ad43efdac9906bf
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-07 14:51:42 +0200

    Added doc-string

--- src/geoconv/tensorflow/layers/experimental/unlift_features.py
+++ src/geoconv/tensorflow/layers/experimental/unlift_features.py
@@ -5,6 +5,19 @@ class UnLiftFeatures2D(tf.keras.layers.Layer):
     """Lifts features into 2-dimensional space by concatenating zeros after each given scalar."""
     @tf.function
     def call(self, inputs):
+        """Applies the layer to the inputs.
+            
+        Parameters
+        ----------
+        inputs: tf.Tensor
+            The signal tensor which has shape 'b x n x 2i', whereby 'b' refers to the number of shapes, 'n' to the number
+            of vertices per shape and 'i' to the feature dimensionality.
+
+        Returns
+        -------
+        tf.Tensor
+            A tensor of shape 'b x n x i'.
+        """
         input_shape = tf.shape(inputs)
         return tf.reshape(
             inputs, (input_shape[0], input_shape[1], tf.math.floordiv(input_shape[-1], 2), 2)
