8be45a8fa7746935f86f2b71036a57e8f1a8b6d8
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-07 11:19:18 +0200

    Updated the doc-string

--- src/geoconv/pytorch/layers/activations/beta_relu.py
+++ src/geoconv/pytorch/layers/activations/beta_relu.py
@@ -5,11 +5,36 @@ import torch
 
 class BetaRelu(nn.Module):
     def __init__(self, feature_input_dim, min_norm=1e-6, *args, **kwargs):
+        """Initializes the object.
+
+        Parameters
+        ----------
+        feature_input_dim: torch.Tensor
+            The feature input dim.
+        min_norm: float
+            The min norm.
+        *args: tuple
+            The args.
+        **kwargs: dict
+            The kwargs.
+        """
         super().__init__(*args, **kwargs)
         self.min_norm = min_norm
         self.beta = nn.Parameter(torch.empty(feature_input_dim // 2, 1))
 
     def forward(self, inputs):
+        """Applies the layer to the inputs.
+
+        Parameters
+        ----------
+        inputs: torch.Tensor
+            The inputs tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor containing the beta-relu altered inputs.
+        """
         input_shape = inputs.size()
         inputs = torch.reshape(inputs, (input_shape[0], input_shape[1], input_shape[2] // 2, 2))
         inputs_norm = torch.maximum(torch.linalg.norm(inputs, axis=-1), torch.tensor(self.min_norm))
