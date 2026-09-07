50572b089c72b71a38065c6ae45e7b9d15289047
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-07 13:49:35 +0200

    Added doc-string

--- src/geoconv/pytorch/layers/descriptor/shot_descriptor.py
+++ src/geoconv/pytorch/layers/descriptor/shot_descriptor.py
@@ -24,17 +24,36 @@ class ShotDescriptor(nn.Module):
     sphere_radius: float
         The radius of the sphere for the SHOT-descriptor.
     """
-    def __init__(
-        self,
-        neighbors_for_lrf=16,
-        azimuth_bins=8,
-        elevation_bins=2,
-        radial_bins=2,
-        histogram_bins=11,
-        sphere_radius=0.0,
-        *args,
-        **kwargs,
-    ):
+    def __init__(self,
+                 neighbors_for_lrf=16,
+                 azimuth_bins=8,
+                 elevation_bins=2,
+                 radial_bins=2,
+                 histogram_bins=11,
+                 sphere_radius=0.0,
+                 *args,
+                 **kwargs):
+        """Initializes the object.
+
+        Parameters
+        ----------
+        neighbors_for_lrf: int
+            The number of neighbors used to construct local reference frames.
+        azimuth_bins: int
+            The number of azimuth bins.
+        elevation_bins: int
+            The number of elevation bins.
+        radial_bins: int
+            The number of radial bins.
+        histogram_bins: int
+            The number of histogram bins.
+        sphere_radius: float
+            The sphere radius.
+        *args: tuple
+            The args.
+        **kwargs: dict
+            The kwargs.
+        """
         super().__init__(*args, **kwargs)
         self.neighbors_for_lrf = neighbors_for_lrf
         self.azimuth_bins = azimuth_bins
@@ -44,9 +63,34 @@ class ShotDescriptor(nn.Module):
         self.sphere_radius = sphere_radius
 
     def forward(self, vertices):
+        """Applies the layer to the inputs.
+
+        Parameters
+        ----------
+        vertices: torch.Tensor
+            A tensor of shape 'b x n x 3' containing the 3D vertex coordinates, whereby 'b' represents the number of
+            shapes, 'n' the number of vertices per shape.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor of shape 'b x n x d' containing the SHOT descriptors.
+        """
         return torch.stack([self.forward_helper(shape) for shape in vertices], dim=0)
 
     def forward_helper(self, vertices):
+        """Computes a SHOT descriptor for one point cloud.
+
+        Parameters
+        ----------
+        vertices: torch.Tensor
+            A tensor of shape 'n x 3' containing the 3D vertex coordinates.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor of shape 'n x d' containing the SHOT descriptors.
+        """
         lrfs, neighborhoods, neighborhoods_indices = knn_shot_lrf(self.neighbors_for_lrf, vertices[None, ...])
         lrfs, neighborhoods, neighborhoods_indices = lrfs[0], neighborhoods[0], neighborhoods_indices[0]
         if self.sphere_radius > 0.0:
