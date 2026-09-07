7436d9fa7ca9bdee28b8a5d007221440695cf8ec
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-07 14:48:31 +0200

    Added doc-string

--- src/geoconv/tensorflow/layers/experimental/lift_features.py
+++ src/geoconv/tensorflow/layers/experimental/lift_features.py
@@ -5,6 +5,19 @@ class LiftFeatures2D(tf.keras.layers.Layer):
     """Lifts features into 2-dimensional space by concatenating zeros after each given scalar."""
     @tf.function
     def call(self, inputs):
+        """Applies the layer to the inputs.
+
+        Parameters
+        ----------
+        inputs: tf.Tensor
+            A tensor having a shape 'b x n x i', whereby 'b' refers to the number of shapes, 'n' to the number of
+            vertices per shape and 'i' to the feature dimensionality.
+
+        Returns
+        -------
+        tf.Tensor
+            A tensor of shape 'b x n x 2i'.
+        """
         input_shape = tf.shape(inputs)
         return tf.reshape(
             tf.stack([inputs, tf.zeros_like(inputs)], axis=-1),
