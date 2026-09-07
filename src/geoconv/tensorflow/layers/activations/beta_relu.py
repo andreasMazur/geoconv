c88129e2db86b34fa6759daa4d281d2d38dc6599
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-07 14:11:28 +0200

    Added doc-string

--- src/geoconv/tensorflow/layers/activations/beta_relu.py
+++ src/geoconv/tensorflow/layers/activations/beta_relu.py
@@ -9,11 +9,29 @@ class BetaRelu(tf.keras.layers.Layer):
     > Maurice Weiler, Mario Geiger, Max Welling, Wouter Boomsma and Taco Cohen.
     """
     def __init__(self, min_norm=1e-6, *args, **kwargs):
+        """Initializes the object.
+
+        Parameters
+        ----------
+        min_norm: float
+            The min norm.
+        *args: tuple
+            The args.
+        **kwargs: dict
+            The kwargs.
+        """
         super().__init__(*args, **kwargs)
         self.min_norm = min_norm
         self.beta = None
 
     def build(self, input_shape):
+        """Builds the layer weights.
+
+        Parameters
+        ----------
+        input_shape: list
+            The input shape.
+        """
         self.beta = self.add_weight(
             name="beta",
             shape=(input_shape[-1] // 2,),
@@ -22,9 +40,20 @@ class BetaRelu(tf.keras.layers.Layer):
 
     @tf.function
     def call(self, inputs):
+        """Applies the layer to the inputs.
+
+        Parameters
+        ----------
+        inputs: tf.Tensor
+            The inputs tensor.
+
+        Returns
+        -------
+        tf.Tensor
+            A tensor containing the beta-relu altered inputs.
+        """
         input_shape = tf.shape(inputs)
         inputs = tf.reshape(inputs, (input_shape[0], input_shape[1], input_shape[2] // 2, 2))
         inputs_norm = tf.maximum(tf.linalg.norm(inputs, axis=-1), self.min_norm)
         inputs = tf.nn.relu(inputs_norm - self.beta)[..., None] * tf.math.divide_no_nan(inputs, inputs_norm[..., None])
         return tf.reshape(inputs, (input_shape[0], input_shape[1], input_shape[2]))
-
