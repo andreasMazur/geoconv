8cfd2693c4a6f82e99b419c6910b488826fac250
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-08 13:36:05 +0200

    Adjusted doc-string

--- src/geoconv_examples/faust/architectures/intrinsic_cnn.py
+++ src/geoconv_examples/faust/architectures/intrinsic_cnn.py
@@ -15,6 +15,34 @@ def define_hypermodel(hp,
                       n_angular,
                       kernel,
                       faust_path):
+    """Builds a KerasTuner hypermodel for the architecture.
+
+    Parameters
+    ----------
+    hp: kt.HyperParameters
+        The hyperparameter object.
+    output_dims: list
+        The output dims.
+    preprocess_method: str
+        The preprocess method.
+    gpc_radius: float
+        The gpc radius.
+    template_radius: float
+        The template radius.
+    n_radial: int
+        The amount of radial coordinates used by the discretized template.
+    n_angular: int
+        The amount of angular coordinates used by the discretized template.
+    kernel: str
+        The kernel.
+    faust_path: str
+        The path to the preprocessed FAUST dataset.
+
+    Returns
+    -------
+    tf.keras.Model
+        The constructed model.
+    """
     model = define_model(
         output_dims=output_dims,
         preprocess_method=preprocess_method,
