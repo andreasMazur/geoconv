2f3c2e3c15d28cc3075791c7395c33068792625b
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-08 13:32:47 +0200

    Added doc-string

--- src/geoconv_examples/faust/architectures/gem_p_cnn.py
+++ src/geoconv_examples/faust/architectures/gem_p_cnn.py
@@ -17,6 +17,34 @@ def define_hypermodel(hp,
                       n_radial,
                       n_angular,
                       faust_path):
+    """Builds a KerasTuner hypermodel for the architecture.
+        
+    Parameters
+    ----------
+    hp: kt.HyperParameters
+        The hyperparameter object.
+    input_types: list
+        The input types.
+    output_types: list
+        The output types.
+    preprocess_method: str
+        The preprocess method.
+    gpc_radius: float
+        The gpc radius.
+    template_radius: float
+        The template radius.
+    n_radial: int
+        The amount of radial coordinates used by the discretized template.
+    n_angular: int
+        The amount of angular coordinates used by the discretized template.
+    faust_path: str
+        The path to the preprocessed FAUST dataset.
+
+    Returns
+    -------
+    tf.keras.Model
+        The constructed model.
+    """
     model = define_model(
         input_types=input_types,
         output_types=output_types,
