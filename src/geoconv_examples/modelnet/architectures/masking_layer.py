ee392e25f39f3a72c1e2795b6c3f5db3b61305fd
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2026-09-09 09:59:47 +0200

    Added doc-string

--- src/geoconv_examples/modelnet/architectures/masking_layer.py
+++ src/geoconv_examples/modelnet/architectures/masking_layer.py
@@ -4,5 +4,17 @@ import tensorflow as tf
 class MaskingLayer(tf.keras.layers.Layer):
     @tf.function
     def call(self, inputs):
+        """Applies the vertex mask and expands the batch dimension.
+            
+        Parameters
+        ----------
+        inputs: (tf.Tensor, tf.Tensor)
+            The signal tensor and a mask tensor.
+
+        Returns
+        -------
+        tf.Tensor:
+            The masked signal tensor.
+        """
         signal, mask = inputs
         return tf.expand_dims(signal[mask], axis=0)
